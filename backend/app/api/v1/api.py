96d668fa4b08724ed8b7d5c1f925e1c8bd542c44
Yannic Kilcher <yk@users.noreply.github.com>
2022-12-14 23:08:08 +0100

    Rough draft of story summarization interaction

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-from app.api.v1 import labelers, prompts
+from app.api.v1 import labelers, prompts, tasks
 from fastapi import APIRouter
 
 api_router = APIRouter()
 api_router.include_router(labelers.router, prefix="/labelers", tags=["labelers"])
 api_router.include_router(prompts.router, prefix="/prompts", tags=["prompts"])
+api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
