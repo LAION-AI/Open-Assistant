edf123e451f7b869e0b019f8be6b19b9b3b7e476
Andreas Köpf <andreas.koepf@provisio.com>
2022-12-17 12:06:24 +0100

    add 'laion-ai/ocgp-' prefix to backend docker image names

--- backend/scripts/docker-build-backend.sh
+++ backend/scripts/docker-build-backend.sh
@@ -2,6 +2,6 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 pushd $parent_path
 
-docker build ../../backend -f ../backend.dockerfile -t ocgpt-backend
+docker build ../../backend -f ../backend.dockerfile -t laion-ai/ocgpt-backend
 
 popd
