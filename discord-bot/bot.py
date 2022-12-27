2207b0a16d3458b500fe4a5af195dd9700b7970d
ottojoash <louisjoshbricks@gmail.com>
2022-12-27 04:47:14 -0500

    import

--- discord-bot/bot.py
+++ discord-bot/bot.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 import asyncio
 from datetime import timedelta
 from pathlib import Path
-from tkinter import BOTH
 from typing import Optional, Union
 
 import discord
