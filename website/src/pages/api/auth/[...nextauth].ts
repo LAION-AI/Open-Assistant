2a814b5b0b766b17ad7f8309625a90d006a00d82
Yannic Kilcher <yk@users.noreply.github.com>
2022-12-27 13:57:55 +0100

    Update website/src/pages/api/auth/[...nextauth].ts
    Co-authored-by: Ville Lahdenvuo <tuhoojabotti@gmail.com>

--- website/src/pages/api/auth/[...nextauth].ts
+++ website/src/pages/api/auth/[...nextauth].ts
@@ -34,7 +34,7 @@ if (process.env.DISCORD_CLIENT_ID) {
   );
 }
 
-if (boolean(process.env.NODE_ENV === "development")) {
+if (process.env.NODE_ENV === "development") {
   providers.push(
     CredentialsProvider({
       name: "Debug Credentials",
