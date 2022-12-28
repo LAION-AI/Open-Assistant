64206895ff9deebcd72700dfac58ebfc683bb75a
croumegous <cyril.roumegous@gmail.com>
2022-12-28 13:00:53 +0100

    Unified multiple Docker Compose files into one for easier usage

--- scripts/frontend-development/run-bot-local.sh
+++ scripts/frontend-development/run-bot-local.sh
@@ -2,7 +2,7 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 # switch to bot directory
-pushd "$parent_path/../../bot"
+pushd "$parent_path/../../discord-bot"
 
 python3 __main__.py
 
