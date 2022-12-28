b11dc6219a8c04c62e9cadcb51f6ea7aeff270bb
jojopirker <j.pirker@gmail.com>
2022-12-28 15:03:45 +0100

    Prettier main.js

--- website/.storybook/main.js
+++ website/.storybook/main.js
@@ -1,12 +1,15 @@
 const path = require("path");
 
 module.exports = {
-  stories: ["../src/components/**/*.stories.mdx", "../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/components/**/*.stories.mdx",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@chakra-ui/storybook-addon"
+    "@chakra-ui/storybook-addon",
   ],
   framework: "@storybook/react",
   core: {
