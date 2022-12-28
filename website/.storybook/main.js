265d6aa595673d55d6656be8364d736aee421f58
jojopirker <j.pirker@gmail.com>
2022-12-28 14:54:28 +0100

    Storybook update for chakra ui to properly work

--- website/.storybook/main.js
+++ website/.storybook/main.js
@@ -1,11 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.mdx", "../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@chakra-ui/storybook-addon"
   ],
   framework: "@storybook/react",
   core: {
@@ -17,5 +18,8 @@ module.exports = {
     config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
 
     return config;
+  },
+  features: {
+    emotionAlias: false,
   },
 };
