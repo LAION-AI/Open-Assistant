9bb4b9778d3ab254e8f6e3305dd91d6c398c82a3
Jac-Zac <jacopozac@icloud.com>
2022-12-28 19:45:20 +0100

    new config file

---
+++ website/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        allowSeparatedGroups: false
+      }
+    ]
+  },
+  root: true,
+};
