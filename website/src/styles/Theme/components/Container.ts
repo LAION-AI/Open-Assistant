a537a0ffa06e7ae7713523d4e9d55f3cade38309
Lucian Petri <petri.lucian@outlook.com>
2022-12-30 01:55:27 +0200

    adding default text Color to Container

--- website/src/styles/Theme/components/Container.ts
+++ website/src/styles/Theme/components/Container.ts
@@ -4,7 +4,8 @@ import { colors } from "../colors"
 const baseStyle = defineStyle(({colorMode}) => ({
   minWidth: "100%",
   bg: colorMode === "light" ? colors.light.bg : colors.dark.bg,
-  transition: "background-color 250ms ease-in"
+  transition: "background-color 250ms ease-in",
+  color: colorMode === "light" ? colors.light.text : colors.dark.text,
 }))
 
 export const containerTheme = defineStyleConfig({
