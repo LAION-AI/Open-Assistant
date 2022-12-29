721012ab2708a674413cb2b9cafc7a3cf90dc016
Lucian Petri <petri.lucian@outlook.com>
2022-12-29 18:40:09 +0200

    Add Color Mode

--- website/src/pages/_document.tsx
+++ website/src/pages/_document.tsx
@@ -1,4 +1,6 @@
+import { ColorModeScript } from "@chakra-ui/react";
 import { Head, Html, Main, NextScript } from "next/document";
+import theme from "../styles/Theme";
 
 export default function Document() {
   return (
@@ -7,6 +9,7 @@ export default function Document() {
         <link rel="shortcut icon" type="image/png" href="/images/logos/favicon.png" />
       </Head>
       <body className="flex h-full flex-col bg-gray-50">
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
---
+++ website/src/styles/Theme.tsx
@@ -0,0 +1,10 @@
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config });
+
+export default theme;
