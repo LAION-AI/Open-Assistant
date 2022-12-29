f9bb5b2bf1e8416a3aa296dd87042dd169b5325e
Martin H. Normark <m@martinnormark.com>
2022-12-29 18:04:06 +0100

    Install cypress for e2e and component tests

---
+++ website/cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+      viewportWidth: 500,
+      viewportHeight: 500,
+    },
+  },
+});
