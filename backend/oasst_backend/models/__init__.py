42e9a7745615449e4861d6f1919a1bf056549ed6
Andreas Köpf <andreas.koepf@provisio.com>
2022-12-31 00:06:22 +0100

    further pre-commit fixes

--- backend/oasst_backend/models/__init__.py
+++ backend/oasst_backend/models/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from .api_client import ApiClient
 from .journal import Journal, JournalIntegration
-from .user import User
-from .user_stats import UserStats
 from .message import Message
 from .message_reaction import MessageReaction
-from .text_labels import TextLabels
 from .task import Task
+from .text_labels import TextLabels
+from .user import User
+from .user_stats import UserStats
 
 __all__ = [
     "ApiClient",
