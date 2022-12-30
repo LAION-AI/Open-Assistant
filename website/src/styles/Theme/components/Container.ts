80d2e02e90dc0ec9b7e6c5edab749744f012a6fb
Lucian Petri <petri.lucian@outlook.com>
2022-12-30 02:18:48 +0200

    adding a cubic bezier for transition

--- website/src/styles/Theme/components/Container.ts
+++ website/src/styles/Theme/components/Container.ts
@@ -4,7 +4,7 @@ import { colors } from "../colors"
 const baseStyle = defineStyle(({colorMode}) => ({
   minWidth: "100%",
   bg: colorMode === "light" ? colors.light.bg : colors.dark.bg,
-  transition: "background-color 250ms ease-in",
+  transition: "background-color 200ms cubic-bezier(0.4, 0, 1, 1)",
   color: colorMode === "light" ? colors.light.text : colors.dark.text,
 }))
 
