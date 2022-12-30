b78bdfb3a9f071d2c6d127416dc6e0aecb349609
Igor Miagkov <myagkov@physics.msu.ru>
2022-12-31 00:34:00 +0400

    stats api method

--- backend/oasst_backend/api/v1/api.py
+++ backend/oasst_backend/api/v1/api.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from fastapi import APIRouter
-from oasst_backend.api.v1 import frontend_messages, frontend_users, messages, tasks, text_labels, users
+from oasst_backend.api.v1 import frontend_messages, frontend_users, messages, stats, tasks, text_labels, users
 
 api_router = APIRouter()
 api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
@@ -9,3 +9,4 @@ api_router.include_router(messages.router, prefix="/messages", tags=["messages"]
 api_router.include_router(frontend_messages.router, prefix="/frontend_messages", tags=["frontend_messages"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(frontend_users.router, prefix="/frontend_users", tags=["frontend_users"])
+api_router.include_router(stats.router, prefix="/stats", tags=["stats"])
---
+++ backend/oasst_backend/api/v1/stats.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from fastapi import APIRouter, Depends
+from oasst_backend.api import deps
+from oasst_backend.models import ApiClient
+from oasst_backend.prompt_repository import PromptRepository
+from oasst_shared.schemas import protocol
+from sqlmodel import Session
+
+router = APIRouter()
+
+
+@router.get("/")
+def get_message_stats(
+    db: Session = Depends(deps.get_db),
+    api_client: ApiClient = Depends(deps.get_trusted_api_client),
+):
+    pr = PromptRepository(db, api_client, None)
+    return protocol.SystemStats(**pr.get_stats())
