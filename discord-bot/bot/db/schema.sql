a7b7487611eb22dd4da89447e85e3f3275b2bf80
Alex Ott <66271487+AlexanderHOtt@users.noreply.github.com>
2022-12-30 22:55:24 -0800

    remove example table from sql schema

--- discord-bot/bot/db/schema.sql
+++ discord-bot/bot/db/schema.sql
@@ -3,8 +3,3 @@ CREATE TABLE IF NOT EXISTS guild_settings (
     guild_id BIGINT NOT NULL PRIMARY KEY,
     log_channel_id BIGINT
 );
-
-CREATE TABLE IF NOT EXISTS example (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255) NOT NULL
-);
