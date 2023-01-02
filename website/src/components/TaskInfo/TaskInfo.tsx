32e212b5a52391df2d78fccaa2dd1d9db218ff0e
Adrian Cowan <othrayte@gmail.com>
2023-01-02 17:59:03 +1100

    website: Add e2e tests of creating an assistant/prompter reply
    These are intended to function as a minimal smoke test that the API interaction with the backend is functional.

--- website/src/components/TaskInfo/TaskInfo.tsx
+++ website/src/components/TaskInfo/TaskInfo.tsx
@@ -2,7 +2,7 @@ export const TaskInfo = ({ id, output }: { id: string; output: string }) => {
   return (
     <div className="grid grid-cols-[min-content_auto] gap-x-2 text-gray-700">
       <b>Prompt</b>
-      <span>{id}</span>
+      <span data-cy="task-id">{id}</span>
       <b>Output</b>
       <span>{output}</span>
     </div>
