6dd12107367ab83d0717b8b7570d8fb250c59e74
Andrew Maguire <andrewm4894@gmail.com>
2023-01-03 21:39:54 +0000

    docusarus init

--- docs/open-assistant/docusaurus.config.js
+++ docs/open-assistant/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://andrewm4894.github.io/Open-Assistant/",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
