c68bbe75d09368d6f1a156769c1092a8da8872ac
Kostia <kkaploon@gmail.com>
2023-01-03 03:42:36 +0200

    Ran pre-commit to make prettier happy.

--- website/src/components/CollapsableText.tsx
+++ website/src/components/CollapsableText.tsx
@@ -1,37 +1,30 @@
-import { Button, Container, useDisclosure } from "@chakra-ui/react"
-import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    ModalCloseButton,
-  } from '@chakra-ui/react'
+import { Button, Container, useDisclosure } from "@chakra-ui/react";
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton } from "@chakra-ui/react";
 import React from "react";
 
-export const CollapsableText = ({text, maxLength=220}) => {
-        const { isOpen, onOpen, onClose } = useDisclosure()
-        if (typeof(text) != 'string' || text.length <= maxLength) {
-            return text;
-        } else {
-            return (
-                <>
-                    {text.substring(0, maxLength-3)}
-                    <Button style={{display: 'contents'}} onClick={onOpen}>...</Button>
-                    <Container maxW='full'>
-                        <Modal isOpen={isOpen} onClose={onClose} size="xl" scrollBehavior={'inside'} > 
-                            <ModalOverlay>
-                                <ModalContent maxH="400">
-                                    <ModalHeader>Full Text</ModalHeader>
-                                    <ModalCloseButton />
-                                    <ModalBody>
-                                        {text}
-                                    </ModalBody>
-                                </ModalContent>
-                            </ModalOverlay>
-                        </Modal>
-                    </Container>
-                </>
-            );
-        }
-    }
+export const CollapsableText = ({ text, maxLength = 220 }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  if (typeof text != "string" || text.length <= maxLength) {
+    return text;
+  } else {
+    return (
+      <>
+        {text.substring(0, maxLength - 3)}
+        <Button style={{ display: "contents" }} onClick={onOpen}>
+          ...
+        </Button>
+        <Container maxW="full">
+          <Modal isOpen={isOpen} onClose={onClose} size="xl" scrollBehavior={"inside"}>
+            <ModalOverlay>
+              <ModalContent maxH="400">
+                <ModalHeader>Full Text</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>{text}</ModalBody>
+              </ModalContent>
+            </ModalOverlay>
+          </Modal>
+        </Container>
+      </>
+    );
+  }
+};
