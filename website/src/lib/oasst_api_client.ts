91099657feb90e96bc5f7216b21306bd5c60952f
Jack Michaud <jack@lomz.me>
2023-01-03 17:11:28 -0500

    refactor: move new task's oasst api fetching into OasstApiClient

---
+++ website/src/lib/oasst_api_client.ts
@@ -0,0 +1,63 @@
+import { JWT } from "next-auth/jwt";
+
+class OasstError {
+  message: string;
+  errorCode: number;
+  httpStatusCode: number;
+
+  constructor(message: string, errorCode: number, httpStatusCode: number) {
+    this.message = message;
+    this.errorCode = errorCode;
+    this.httpStatusCode = httpStatusCode;
+  }
+}
+
+export default class OasstApiClient {
+  constructor(private readonly oasstApiUrl: string, private readonly oasstApiKey: string) {}
+
+  private async post(path: string, body: any): Promise<any> {
+    const resp = await fetch(`${this.oasstApiUrl}${path}`, {
+      method: "POST",
+      headers: {
+        "X-API-Key": this.oasstApiKey,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (resp.status == 204) {
+      return null;
+    }
+
+    if (resp.status >= 300) {
+      try {
+        const error = await resp.clone().json();
+        throw new OasstError(error.message, error.error_code, resp.status);
+      } catch (e) {
+        throw new OasstError(await resp.text(), 0, resp.status);
+      }
+    }
+
+    return await resp.json();
+  }
+
+  // TODO return a strongly typed Task?
+  // This method is used to store a task in RegisteredTask.task.
+  // This is a raw Json type, so we can't use it to strongly type the task.
+  async fetchTask(taskType: string, userToken: JWT): Promise<any> {
+    return this.post("/api/v1/tasks/", {
+      type: taskType,
+      user: {
+        id: userToken.sub,
+        display_name: userToken.name || userToken.email,
+        auth_method: "local",
+      },
+    });
+  }
+
+  async ackTask(taskId: string, messageId: string): Promise<void> {
+    return this.post(`/api/v1/tasks/${taskId}/ack`, {
+      message_id: messageId,
+    });
+  }
+}
