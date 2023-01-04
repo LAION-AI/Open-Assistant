e77a154a2a581b244caec0f78f8da6e0bd7393ba
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2023-01-04 09:29:50 +0100

    reformatted to pass pre-commit

--- backend/oasst_backend/utils.py
+++ backend/oasst_backend/utils.py
@@ -1,8 +1,9 @@
 import requests
 
+
 def get_detoxify_classification(inputs, hf_token):
-  API_URL = "https://api-inference.huggingface.co/models/unitary/multilingual-toxic-xlm-roberta"
-  headers = {"Authorization": f'Bearer {hf_token}'}
-  payload = {"inputs": inputs}
-  response = requests.post(API_URL, headers=headers, json=payload)
-  return response.json()
+    API_URL = "https://api-inference.huggingface.co/models/unitary/multilingual-toxic-xlm-roberta"
+    headers = {"Authorization": f"Bearer {hf_token}"}
+    payload = {"inputs": inputs}
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
