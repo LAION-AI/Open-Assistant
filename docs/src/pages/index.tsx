10c41fd84cec112301deeb43c0b4b36c42cb0fb0
Andrew Maguire <andrewm4894@gmail.com>
2023-01-04 00:00:22 +0000

    homepage small update

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
-            Docusaurus Tutorial - 5min ⏱️
+            Open Assistant Introduction - 5min ⏱️
           </Link>
         </div>
       </div>
