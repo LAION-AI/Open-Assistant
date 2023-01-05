b63439b16b9989033f8444295abaa7f01c1b569d
Alex Ott <66271487+AlexanderHOtt@users.noreply.github.com>
2023-01-04 23:58:01 -0800

    fix help message

--- discord-bot/bot/extensions/help.py
+++ discord-bot/bot/extensions/help.py
@@ -2,6 +2,8 @@
 import lightbulb
 from bot.messages import help_message
 from bot.settings import Settings
+from hikari.permissions import Permissions
+from lightbulb.utils import permissions_for
 
 plugin = lightbulb.Plugin("HelpPlugin")
 
@@ -13,7 +15,14 @@ settings = Settings()
 @lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
 async def help_command(ctx: lightbulb.Context) -> None:
     """Help for the bot."""
-    await ctx.respond(help_message(True, True))
+    is_admin = False
+    if ctx.guild_id:
+        member = ctx.bot.cache.get_member(ctx.guild_id, ctx.author.id) or await ctx.bot.rest.fetch_member(
+            ctx.guild_id, ctx.author.id
+        )
+        is_admin = bool(permissions_for(member) & Permissions.MANAGE_GUILD)
+
+    await ctx.respond(help_message(is_admin, ctx.author.id in settings.owner_ids))
 
 
 def load(bot: lightbulb.BotApp):
