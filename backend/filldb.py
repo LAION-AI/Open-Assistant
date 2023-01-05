7ba20e62d573915c6b70310846d11f8ef977aede
Nil-Andreu <nilandreug@gmail.com>
2023-01-05 20:57:37 +0100

    [NEW] Fill API client

--- backend/filldb.py
+++ backend/filldb.py
@@ -1,14 +1,86 @@
-from oasst_backend.api.deps import api_auth
+import random
+import string
+import uuid
+from random import choice
+from typing import List
+
 from oasst_backend.database import engine
-from oasst_backend.prompt_repository import PromptRepository
-from oasst_shared.schemas.protocol import User
+from oasst_backend.models import ApiClient
 from sqlmodel import Session
 
+
+class FillDb:
+    """Class object which fills the database: Api Client, Users & Messages."""
+
+    def __init__(self, db_engine, seed: int = 0):
+        # The database engine
+        self.db_engine = db_engine
+
+        # Store the generated api keys
+        self.api_keys: List[str] = []
+
+        # Seed to make sure values are reproducible
+        random.seed(seed)
+
+    def fill_api_client(
+        self,
+    ):
+
+        with Session(self.db_engine) as db:
+            random_api_client = self._create_random_api_client()
+
+            # Store the API key created
+            self.api_keys.append(random_api_client.api_key)
+
+            db.add(random_api_client)
+            db.commit()
+            db.refresh(random_api_client)
+
+            pass
+
+    def fill_users(self):
+        pass
+
+    def _create_random_api_client(
+        self, api_key_length: str = 512, description_length: str = 256, admin_email_length: str = 256
+    ):
+        """Create Random Api Client values"""
+
+        enabled, trusted = self._create_random_bool(2)
+        api_key = self._create_random_str(api_key_length)
+        description = self._create_random_str(description_length)
+        admin_email = self._create_random_str(admin_email_length) + "@example.com"
+
+        api_client = ApiClient(
+            id=uuid.uuid4(),
+            api_key=api_key,
+            description=description,
+            admin_email=admin_email,
+            enabled=enabled,
+            trusted=trusted,
+        )
+
+        return api_client
+
+    @staticmethod
+    def _create_random_str(length: str):
+        return "".join(choice(string.ascii_letters + string.digits) for _ in range(length))
+
+    @staticmethod
+    def _create_random_bool(length: int) -> List[bool]:
+        """Create a list of booleans for a certain length.
+
+        Args:
+            length (int): the length of the list that we want in response
+
+        Returns:
+            Generator: generator of the list of booleans
+        """
+
+        return (choice([True, False]) for idx in range(length))
+
+
 if __name__ == "__main__":
-    db = Session(engine)
-    api_client = api_auth("", db)
-
-    new_user = User(id="randaom123", display_name="display", auth_method="discord")
-
-    # First for that API Client, should create some random users
-    pr = PromptRepository(db, api_client, user=new_user)
+    fill_db = FillDb(engine)
+    fill_db.fill_api_client()
+    print(fill_db.api_keys)
