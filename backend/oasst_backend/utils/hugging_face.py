ee50b573e1d6ad3d7dc66a7e827584e9936b5f4d
Nil Andreu <65730003+Nil-Andreu@users.noreply.github.com>
2023-01-05 16:18:04 +0100

    Detoxify Rest API Client (#404)
    * [NEW] utils: Endpoint Toxic Roberta
    
    * [NEW] Constants API URL
    
    * [NEW] Git ignore venv
    
    * [NEW] Lint
    
    * [NEW] Backend default args
    
    * [NEW] HUGGINGFACE_API_ERROR
    
    * [NEW] Requests package
    
    * [NEW] Get Toxicity Endpoint
    
    * [NEW] Schema:  ToxicityClassification
    
    [NEW] Constants module
    
    [FIX] Module
    
    * [FIX] Test Key HF
    
    * [NEW] settings: HUGGING_FACE_API_KEY
    
    * [NEW] Remove requests
    
    * [NEW] HuggingFace client
    
    * [NEW] Cleaning code

---
+++ backend/oasst_backend/utils/hugging_face.py
@@ -0,0 +1,49 @@
+from typing import Any, Dict
+
+import aiohttp
+from oasst_backend.config import settings
+from oasst_shared.exceptions import OasstError, OasstErrorCode
+
+
+class HuggingFaceAPI:
+    """Class Object to make post calls to endpoints for inference in models hosted in HuggingFace"""
+
+    def __init__(
+        self,
+        api_url: str,
+    ):
+
+        # The API endpoint we want to access
+        self.api_url: str = api_url
+
+        # Access token for the api
+        self.api_key: str = settings.HUGGING_FACE_API_KEY
+
+        # Headers going to be used
+        self.headers: Dict[str, str] = {"Authorization": f"Bearer {self.api_key}"}
+
+    async def post(self, input: str) -> Any:
+        """Post request to the endpoint to get an inference
+
+        Args:
+            input (str): the input that we will pass to the model
+
+        Raises:
+            OasstError: in the case we get a bad response
+
+        Returns:
+            inference: the inference we obtain from the model in HF
+        """
+
+        session = aiohttp.ClientSession()
+        payload: Dict[str, str] = {"inputs": input}
+        response = await session.post(self.api_url, headers=self.headers, json=payload)
+
+        # If we get a bad response
+        if response.status != 200:
+            raise OasstError("Response Error Detoxify HuggingFace", error_code=OasstErrorCode.HUGGINGFACE_API_ERROR)
+
+        # Get the response from the API call
+        inference = await response.json()
+
+        return inference
