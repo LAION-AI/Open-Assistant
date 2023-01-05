c8ecfc3c78578dc3d7f24e4e983e8815ec015782
Nil-Andreu <nilandreug@gmail.com>
2023-01-05 10:36:03 +0100

    [NEW] Constants API URL

---
+++ backend/oasst_backend/constants.py
@@ -0,0 +1,1 @@
+HUGGINGFACE_TOXIC_ROBERTA_URL: str = "https://api-inference.huggingface.co/models/unitary/multilingual-toxic-xlm-roberta"
--- backend/oasst_backend/utils.py
+++ backend/oasst_backend/utils.py
@@ -1,11 +1,10 @@
 import requests
 from typing import Dict, Any
+from oasst_backend.constants import HUGGINGFACE_TOXIC_ROBERTA_URL
 
 
 def get_detoxify_classification(inputs: Any, hf_token: str) -> Dict[Any]:
-
-    API_URL: str = "https://api-inference.huggingface.co/models/unitary/multilingual-toxic-xlm-roberta"
     headers: Dict[str, str] = {"Authorization": f"Bearer {hf_token}"}
     payload: Dict[str, Any] = {"inputs": inputs}
-    response = requests.post(API_URL, headers=headers, json=payload)
+    response = requests.post(HUGGINGFACE_TOXIC_ROBERTA_URL, headers=headers, json=payload)
     return response.json()
