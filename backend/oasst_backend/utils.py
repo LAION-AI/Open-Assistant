5b286b8a8218ce43c095175a40f413ab7e6aa757
Nil-Andreu <nilandreug@gmail.com>
2023-01-05 12:02:20 +0100

    [NEW] HUGGINGFACE_API_ERROR

--- backend/oasst_backend/utils.py
+++ backend/oasst_backend/utils.py
@@ -2,10 +2,15 @@ from typing import Any, Dict
 
 import requests
 from oasst_backend.constants import HUGGINGFACE_TOXIC_ROBERTA_URL
+from oasst_shared.exceptions import OasstError, OasstErrorCode
 
 
 def get_detoxify_classification(inputs: Any, hf_token: str) -> Dict[Any]:
     headers: Dict[str, str] = {"Authorization": f"Bearer {hf_token}"}
     payload: Dict[str, Any] = {"inputs": inputs}
     response = requests.post(HUGGINGFACE_TOXIC_ROBERTA_URL, headers=headers, json=payload)
+
+    if response.status_code != 200:
+        raise OasstError("Response Error Detoxify HuggingFace", error_code=OasstErrorCode.HUGGINGFACE_API_ERROR)
+
     return response.json()
