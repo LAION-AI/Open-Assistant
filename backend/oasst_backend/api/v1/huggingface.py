228aca0561873d98d5cdeee5eb1ddb6f9579fc82
Nil-Andreu <nilandreug@gmail.com>
2023-01-05 12:18:44 +0100

    [NEW] Get Toxicity Endpoint

---
+++ backend/oasst_backend/api/v1/huggingface.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends
+from oasst_backend.api import deps
+from oasst_backend.models import ApiClient
+from oasst_backend.utils import get_detoxify_classification
+
+router = APIRouter()
+
+
+@router.get("/hf/text_toxicity")
+def get_text_toxicity(
+    msg: str,
+    hf_token: str,
+    api_client: ApiClient = Depends(deps.get_trusted_api_client),
+):
+    """Get the Message Toxicity from HuggingFace Roberta model.
+
+    Args:
+        msg (str): the message that we want to analyze.
+        hf_token (str): the token for the huggingface access.
+        api_client (ApiClient, optional): authentification of the user of the request.
+            Defaults to Depends(deps.get_trusted_api_client).
+
+    Returns:
+        ToxicityClassification: the score of toxicity of the message.
+    """
+
+    return get_detoxify_classification(msg=msg, hf_token=hf_token)
--- backend/oasst_backend/utils.py
+++ backend/oasst_backend/utils.py
@@ -1,16 +1,24 @@
 from typing import Any, Dict
 
 import requests
-from oasst_backend.constants import HUGGINGFACE_TOXIC_ROBERTA_URL
+from constants import HUGGINGFACE_TOXIC_ROBERTA_URL
 from oasst_shared.exceptions import OasstError, OasstErrorCode
+from requests.models import Response
 
 
-def get_detoxify_classification(inputs: Any, hf_token: str) -> Dict[Any]:
+def get_detoxify_classification(msg: str, hf_token: str) -> Dict[str, Any]:
     headers: Dict[str, str] = {"Authorization": f"Bearer {hf_token}"}
-    payload: Dict[str, Any] = {"inputs": inputs}
-    response = requests.post(HUGGINGFACE_TOXIC_ROBERTA_URL, headers=headers, json=payload)
+    payload: Dict[str, Any] = {"inputs": msg}
+    response: Response = requests.post(HUGGINGFACE_TOXIC_ROBERTA_URL, headers=headers, json=payload)
+    print(response)
 
     if response.status_code != 200:
         raise OasstError("Response Error Detoxify HuggingFace", error_code=OasstErrorCode.HUGGINGFACE_API_ERROR)
 
     return response.json()
+
+
+if __name__ == "__main__":
+    res = get_detoxify_classification("Bullshit", "")
+
+    print(res)
