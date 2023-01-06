2d4df06178f2897949210623da22348804776e04
Isabell Reichardt <Bereichardt@gmail.com>
2023-01-06 17:11:47 +0100

    Add CTA to Hero.tsx and redirect redirect user

--- website/src/components/Hero.tsx
+++ website/src/components/Hero.tsx
@@ -1,6 +1,10 @@
 import { useColorMode } from "@chakra-ui/react";
 import Image from "next/image";
 import { useId } from "react";
+import { FaLock, FaComment } from "react-icons/fa";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 import { Container } from "./Container";
 
@@ -55,6 +59,39 @@ function BackgroundIllustration(props) {
   );
 }
 
+function CTA() {
+  const { data: session } = useSession();
+  const router = useRouter();
+  if (session) {
+    router.push("/dashboard");
+  }
+  return (
+    <div>
+      {!session ? (
+        <Link href="/auth/signin">
+          <button
+            type="button"
+            className="mb-2 ml-6 flex items-center rounded-md border border-transparent bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          >
+            <FaLock />
+            <span className="text-lg ml-1"> Login</span>
+          </button>
+        </Link>
+      ) : (
+        <Link href="/dashboard">
+          <button
+            type="button"
+            className="mb-2 ml-6 flex items-center rounded-md border border-transparent bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          >
+            <FaComment />
+            <span className="text-lg ml-1"> Start</span>
+          </button>
+        </Link>
+      )}
+    </div>
+  );
+}
+
 export function Hero() {
   const { colorMode } = useColorMode();
   const pTextColor = colorMode === "light" ? "text-gray-600" : "text-white";
@@ -91,8 +128,13 @@ export function Hero() {
               />
             </div>
           </div>
+          <CTA />
         </div>
       </Container>
     </div>
   );
+}
+
+function componentDidMount() {
+  throw new Error("Function not implemented.");
 }
