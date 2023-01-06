1f421b52fc407b7bece68d995bd55f56bf28e2f3
Daniel Hug <danielpatrickhug@gmail.com>
2023-01-06 09:49:24 -0500

    updated instances of ConversationMessage to take in message_id and frontend_message_idin tasks and utils in v1 api

--- backend/oasst_backend/api/v1/tasks.py
+++ backend/oasst_backend/api/v1/tasks.py
@@ -58,10 +58,7 @@ def generate_task(
             messages = pr.fetch_random_conversation("assistant")
             task_messages = [
                 protocol_schema.ConversationMessage(
-                    text=msg.payload.payload.text,
-                    is_assistant=(msg.role == "assistant"),
-                    message_id=msg.id,
-                    frontend_message_id=msg.frontend_message_id,
+                    text=msg.payload.payload.text, is_assistant=(msg.role == "assistant")
                 )
                 for msg in messages
             ]
@@ -74,10 +71,7 @@ def generate_task(
             messages = pr.fetch_random_conversation("prompter")
             task_messages = [
                 protocol_schema.ConversationMessage(
-                    text=msg.payload.payload.text,
-                    is_assistant=(msg.role == "assistant"),
-                    message_id=msg.id,
-                    frontend_message_id=msg.frontend_message_id,
+                    text=msg.payload.payload.text, is_assistant=(msg.role == "assistant")
                 )
                 for msg in messages
             ]
@@ -98,8 +92,6 @@ def generate_task(
                 protocol_schema.ConversationMessage(
                     text=p.text,
                     is_assistant=(p.role == "assistant"),
-                    message_id=p.id,
-                    frontend_message_id=p.frontend_message_id,
                 )
                 for p in conversation
             ]
@@ -119,8 +111,6 @@ def generate_task(
                 protocol_schema.ConversationMessage(
                     text=p.text,
                     is_assistant=(p.role == "assistant"),
-                    message_id=p.id,
-                    frontend_message_id=p.frontend_message_id,
                 )
                 for p in conversation
             ]
