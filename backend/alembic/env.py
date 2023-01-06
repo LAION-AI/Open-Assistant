1309b5b3394f006feb2fc3550c1bb9954bdaafff
shawticus <shawmakesmagic@gmail.com>
2023-01-05 17:17:59 -0800

    lint env

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,7 +1,8 @@
 import logging
+
 from alembic import context
 from oasst_backend import models  # noqa: F401
-from sqlalchemy import (engine_from_config,pool)
+from sqlalchemy import engine_from_config, pool
 
 # Read in the Alembic config file.
 config = context.config
@@ -20,12 +21,12 @@ target_metadata = models.Base.metadata
 
 def run_migrations_offline():
     """
-        Run migrations in 'offline' mode.
+    Run migrations in 'offline' mode.
 
-        This configures the context with just a URL and not an Engine,
-        though an Engine is acceptable here as well. By skipping the
-        Engine creation we don't even need a DBAPI to be available.
-    
+    This configures the context with just a URL and not an Engine,
+    though an Engine is acceptable here as well. By skipping the
+    Engine creation we don't even need a DBAPI to be available.
+
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -40,11 +41,11 @@ def run_migrations_offline():
 
 def run_migrations_online():
     """
-        Run migrations in 'online' mode.
+    Run migrations in 'online' mode.
 
-        In this scenario we need to create an Engine and associate a
-        connection with the context.
-    
+    In this scenario we need to create an Engine and associate a
+    connection with the context.
+
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
@@ -52,13 +53,12 @@ def run_migrations_online():
         poolclass=pool.NullPool,
     )
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
         with context.begin_transaction():
             context.get_context()._ensure_version_table()
             connection.execute("LOCK TABLE alembic_version IN ACCESS EXCLUSIVE MODE")
             context.run_migrations()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
