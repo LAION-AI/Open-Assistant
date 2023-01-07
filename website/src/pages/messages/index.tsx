d3a0bd43055f6d3a69af30d7345b15877a2831f6
klotske <ilya@mokhov.vg>
2023-01-07 03:38:34 +0300

    Moved SideMenu to component & refactored layout

--- website/src/pages/messages/index.tsx
+++ website/src/pages/messages/index.tsx
@@ -2,15 +2,11 @@ import { Box, CircularProgress, SimpleGrid, Text, useColorModeValue } from "@cha
 import Head from "next/head";
 import { useState } from "react";
 import useSWRImmutable from "swr/immutable";
-
 import fetcher from "src/lib/fetcher";
-import { SideMenu } from "src/components/Dashboard";
 import { MessageTable } from "src/components/Messages/MessageTable";
 import { getDashboardLayout } from "src/components/Layout";
-import { colors } from "styles/Theme/colors";
 
 const MessagesDashboard = () => {
-  const bgColor = useColorModeValue(colors.light.bg, colors.dark.bg);
   const boxBgColor = useColorModeValue("white", "gray.700");
   const boxAccentColor = useColorModeValue("gray.200", "gray.900");
 
@@ -35,44 +31,37 @@ const MessagesDashboard = () => {
         <title>Messages - Open Assistant</title>
         <meta name="description" content="Chat with Open Assistant and provide feedback." />
       </Head>
-      <Box backgroundColor={bgColor} className="sm:overflow-hidden">
-        <Box className="sm:flex h-full gap-6">
-          <Box className="p-6 sm:pr-0">
-            <SideMenu />
+      <Box className="flex flex-col overflow-auto p-6 sm:pl-0 gap-14">
+        <SimpleGrid columns={[1, 1, 1, 2]} gap={4}>
+          <Box>
+            <Text className="text-2xl font-bold" pb="4">
+              Most recent messages
+            </Text>
+            <Box
+              backgroundColor={boxBgColor}
+              boxShadow="base"
+              dropShadow={boxAccentColor}
+              borderRadius="xl"
+              className="p-6 shadow-sm"
+            >
+              {isLoadingAll ? <CircularProgress isIndeterminate /> : <MessageTable messages={messages} />}
+            </Box>
           </Box>
-          <Box className="flex flex-col overflow-auto p-6 sm:pl-0 gap-14">
-            <SimpleGrid columns={[1, 1, 1, 2]} gap={4}>
-              <Box>
-                <Text className="text-2xl font-bold" pb="4">
-                  Most recent messages
-                </Text>
-                <Box
-                  backgroundColor={boxBgColor}
-                  boxShadow="base"
-                  dropShadow={boxAccentColor}
-                  borderRadius="xl"
-                  className="p-6 shadow-sm"
-                >
-                  {isLoadingAll ? <CircularProgress isIndeterminate /> : <MessageTable messages={messages} />}
-                </Box>
-              </Box>
-              <Box>
-                <Text className="text-2xl font-bold" pb="4">
-                  Your most recent messages
-                </Text>
-                <Box
-                  backgroundColor={boxBgColor}
-                  boxShadow="base"
-                  dropShadow={boxAccentColor}
-                  borderRadius="xl"
-                  className="p-6 shadow-sm"
-                >
-                  {isLoadingUser ? <CircularProgress isIndeterminate /> : <MessageTable messages={userMessages} />}
-                </Box>
-              </Box>
-            </SimpleGrid>
+          <Box>
+            <Text className="text-2xl font-bold" pb="4">
+              Your most recent messages
+            </Text>
+            <Box
+              backgroundColor={boxBgColor}
+              boxShadow="base"
+              dropShadow={boxAccentColor}
+              borderRadius="xl"
+              className="p-6 shadow-sm"
+            >
+              {isLoadingUser ? <CircularProgress isIndeterminate /> : <MessageTable messages={userMessages} />}
+            </Box>
           </Box>
-        </Box>
+        </SimpleGrid>
       </Box>
     </>
   );
