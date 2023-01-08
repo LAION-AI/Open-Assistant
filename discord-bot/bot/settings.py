6a3c65a0e99c2fc41b1f51c690f24280efff8d1e
Hemang Joshi <hemangjoshi37a@gmail.com>
2023-01-08 12:26:37 +0530

    added comments for understanding

--- discord-bot/bot/settings.py
+++ discord-bot/bot/settings.py
@@ -5,13 +5,26 @@ from pydantic import BaseSettings, Field
 class Settings(BaseSettings):
     """Settings for the bot."""
 
-    bot_token: str = Field(env="BOT_TOKEN", default="")
-    declare_global_commands: int = Field(env="DECLARE_GLOBAL_COMMANDS", default=0)
-    owner_ids: list[int] = Field(env="OWNER_IDS", default_factory=list)
-    prefix: str = Field(env="PREFIX", default="/")
-    oasst_api_url: str = Field(env="OASST_API_URL", default="http://localhost:8080")
-    oasst_api_key: str = Field(env="OASST_API_KEY", default="")
+    # Bot token obtained from the Discord Developer Portal
+    bot_token: str = Field(env_var="BOT_TOKEN", default="", description="Bot token from Discord Developer Portal")
+
+    # ID of the server where global commands are declared
+    declare_global_commands: int = Field(env_var="DECLARE_GLOBAL_COMMANDS", default=0, description="ID of server where global commands are declared")
+
+    # IDs of the bot owners
+    owner_ids: List[int] = Field(env_var="OWNER_IDS", default_factory=list, description="IDs of bot owners")
+
+    # Prefix for commands
+    prefix: str = Field(env_var="PREFIX", default="/", description="Prefix for commands")
+
+    # URL for the OASST API
+    oasst_api_url: str = Field(env_var="OASST_API_URL", default="http://localhost:8080", description="URL for the OASST API")
+
+    # API key for the OASST API
+    oasst_api_key: str = Field(env_var="OASST_API_KEY", default="", description="API key for the OASST API")
 
     class Config(BaseSettings.Config):
+        # Load environment variables from .env file
         env_file = ".env"
+        # Make the settings case-insensitive
         case_sensitive = False
