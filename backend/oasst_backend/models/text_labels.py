170a1e5fa465978461f260326aaee18889f25221
Hemang Joshi <hemangjoshi37a@gmail.com>
2023-01-08 11:55:24 +0530

    minor bugfix for `TextLabels` class

--- backend/oasst_backend/models/text_labels.py
+++ backend/oasst_backend/models/text_labels.py
@@ -1,27 +1,26 @@
 from datetime import datetime
-from typing import Optional
-from uuid import UUID, uuid4
+from typing import Dict, Optional, UUID
+import uuid
 
 import sqlalchemy as sa
 import sqlalchemy.dialects.postgresql as pg
 from sqlmodel import Field, SQLModel
-
 
 class TextLabels(SQLModel, table=True):
     __tablename__ = "text_labels"
 
     id: Optional[UUID] = Field(
         sa_column=sa.Column(
-            pg.UUID(as_uuid=True), primary_key=True, default=uuid4, server_default=sa.text("gen_random_uuid()")
-        ),
+            pg.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, server_default=sa.text("gen_random_uuid()")
+        )
     )
     user_id: UUID = Field(sa_column=sa.Column(pg.UUID(as_uuid=True), sa.ForeignKey("user.id"), nullable=False))
     created_date: Optional[datetime] = Field(
-        sa_column=sa.Column(sa.DateTime(), nullable=False, server_default=sa.func.current_timestamp()),
+        sa_column=sa.Column(sa.DateTime(), nullable=False, server_default=sa.func.current_timestamp())
     )
     api_client_id: UUID = Field(nullable=False, foreign_key="api_client.id")
     text: str = Field(nullable=False, max_length=2**16)
     message_id: Optional[UUID] = Field(
         sa_column=sa.Column(pg.UUID(as_uuid=True), sa.ForeignKey("message.id"), nullable=True)
     )
-    labels: dict[str, float] = Field(default={}, sa_column=sa.Column(pg.JSONB), nullable=False)
+    labels: Dict[str, float] = Field(default={}, sa_column=sa.Column(pg.JSONB), nullable=False)
