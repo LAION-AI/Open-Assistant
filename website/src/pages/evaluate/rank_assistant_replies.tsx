d0042cebb3ca50c595dc73434ac4cc5d0c2215f8
AbdBarho <ka70911@gmail.com>
2023-01-08 19:50:49 +0100

    Use hooks for "evaluate" tasks

--- website/src/pages/evaluate/rank_assistant_replies.tsx
+++ website/src/pages/evaluate/rank_assistant_replies.tsx
@@ -1,34 +1,12 @@
 import { useColorMode } from "@chakra-ui/react";
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { Container } from "src/components/Container";
 import { LoadingScreen } from "src/components/Loading/LoadingScreen";
 import { Task } from "src/components/Tasks/Task";
-import fetcher from "src/lib/fetcher";
-import poster from "src/lib/poster";
-import useSWRImmutable from "swr/immutable";
-import useSWRMutation from "swr/mutation";
+import { useRankAssistantRepliesTask } from "src/hooks/tasks/evaluate/useRankReplies";
 
 const RankAssistantReplies = () => {
-  const [tasks, setTasks] = useState([]);
-
-  const { isLoading, mutate } = useSWRImmutable("/api/new_task/rank_assistant_replies", fetcher, {
-    onSuccess: (data) => {
-      setTasks([data]);
-    },
-  });
-
-  useEffect(() => {
-    if (tasks.length == 0) {
-      mutate();
-    }
-  }, [tasks]);
-
-  const { trigger } = useSWRMutation("/api/update_task", poster, {
-    onSuccess: async (data) => {
-      const newTask = await data.json();
-      setTasks((oldTasks) => [...oldTasks, newTask]);
-    },
-  });
+  const { tasks, isLoading, reset, trigger } = useRankAssistantRepliesTask();
 
   const { colorMode } = useColorMode();
   const mainBgClasses = colorMode === "light" ? "bg-slate-300 text-gray-800" : "bg-slate-900 text-white";
@@ -37,14 +15,8 @@ const RankAssistantReplies = () => {
     return <LoadingScreen text="Loading..." />;
   }
 
-  if (tasks.length == 0) {
-    return (
-      <div className={`p-12 ${mainBgClasses}`}>
-        <div className="flex h-full">
-          <div className="text-xl font-bold  mx-auto my-auto">No tasks found...</div>
-        </div>
-      </div>
-    );
+  if (tasks.length === 0) {
+    return <Container className="p-6 text-center">No tasks found...</Container>;
   }
 
   return (
@@ -53,7 +25,7 @@ const RankAssistantReplies = () => {
         <title>Rank Assistant Replies</title>
         <meta name="description" content="Rank Assistant Replies." />
       </Head>
-      <Task tasks={tasks} trigger={trigger} mutate={mutate} mainBgClasses={mainBgClasses} />
+      <Task tasks={tasks} trigger={trigger} mutate={reset} mainBgClasses={mainBgClasses} />
     </>
   );
 };
