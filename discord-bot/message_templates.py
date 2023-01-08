088fa860a0c6fd327a9fde3a86d99a2f67a3bb7f
Hemang Joshi <hemangjoshi37a@gmail.com>
2023-01-08 12:12:18 +0530

    added better docstring

--- discord-bot/message_templates.py
+++ discord-bot/message_templates.py
@@ -6,15 +6,26 @@ from loguru import logger
 
 
 class MessageTemplates:
-    """Create message templates for the discord bot."""
+    """
+    The MessageTemplates class is used to create message templates for the Discord bot. It uses Jinja2 template engine to render the templates with the provided arguments.
 
+    Attributes:
+    env (jinja2.Environment): The Jinja2 environment object used to load templates from the specified directory.
+
+    Methods:
+    render(template_name: str, **kwargs) -> str: Renders the specified template with the provided keyword arguments and returns the rendered template as a string.
+    """
     def __init__(self, template_dir: str = "./templates"):
-        self.env = jinja2.Environment(  # noqa: S701
+        self.env = jinja2.Environment(
             loader=jinja2.FileSystemLoader(template_dir),
-            autoescape=jinja2.select_autoescape(disabled_extensions=("msg",), default=False, default_for_string=False),
+            autoescape=jinja2.select_autoescape(
+                disabled_extensions=("msg",),
+                default=False,
+                default_for_string=False,
+            ),
         )
 
-    def render(self, template_name: str, **kwargs: typing.Any):
+    def render(self, template_name: str, **kwargs):
         template = self.env.get_template(template_name)
         txt = template.render(kwargs)
         logger.debug(txt)
