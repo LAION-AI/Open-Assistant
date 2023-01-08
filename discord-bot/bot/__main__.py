6c86de6bbb619fa12ab4fcc80df3bbbeeac3d64a
Hemang Joshi <hemangjoshi37a@gmail.com>
2023-01-08 12:23:04 +0530

    minor bugfix and improvement in the code `main()`

--- discord-bot/bot/__main__.py
+++ discord-bot/bot/__main__.py
@@ -1,24 +1,33 @@
-"""Entry point for the bot."""
 import logging
 import os
+
+import uvloop
 
 from bot.bot import bot
 from hikari.presences import Activity, ActivityType, Status
 
 logger = logging.getLogger(__name__)
 
-if __name__ == "__main__":
+def main():
     if os.name != "nt":
-        import uvloop
-
         uvloop.install()
 
     logger.info("Starting bot")
-    bot.run(
-        check_for_updates=True,
-        activity=Activity(
-            name="/help",
-            type=ActivityType.PLAYING,
-        ),
-        status=Status.ONLINE,
+
+    activity = Activity(
+        name="/help",
+        type=ActivityType.PLAYING,
     )
+    status = Status.ONLINE
+
+    try:
+        bot.run(
+            check_for_updates=True,
+            activity=activity,
+            status=status,
+        )
+    except Exception as e:
+        logger.exception("Error starting bot: %s", e)
+
+if __name__ == "__main__":
+    main()
