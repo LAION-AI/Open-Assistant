0361a0977b65376586d5a489389270b7bbeedb62
Nil-Andreu <nilandreug@gmail.com>
2023-01-10 08:35:15 +0100

    [FIX] Alembic merge heads

---
+++ backend/alembic/versions/2023_01_10_0827-e7f955d6ff1b_.py
@@ -0,0 +1,21 @@
+"""empty message
+
+Revision ID: e7f955d6ff1b
+Revises: aac6b2f66006, bcc2fe18d214
+Create Date: 2023-01-10 08:27:55.439608
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "e7f955d6ff1b"
+down_revision = ("aac6b2f66006", "bcc2fe18d214")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
