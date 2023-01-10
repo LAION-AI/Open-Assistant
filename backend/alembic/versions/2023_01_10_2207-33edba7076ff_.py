34031b3f7e524a7583bcfd2ecd36405e20dbe3f7
Nil-Andreu <nilandreug@gmail.com>
2023-01-10 22:08:35 +0100

    [NEW] Separating score/label

---
+++ backend/alembic/versions/2023_01_10_2207-33edba7076ff_.py
@@ -0,0 +1,21 @@
+"""empty message
+
+Revision ID: 33edba7076ff
+Revises: 2d71d4cd5a1b, e7f955d6ff1b
+Create Date: 2023-01-10 22:07:51.109819
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "33edba7076ff"
+down_revision = ("2d71d4cd5a1b", "e7f955d6ff1b")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
