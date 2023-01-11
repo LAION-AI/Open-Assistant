3d75014fd7f3d0d68ffc1b2c4a3413d05d8c54ff
Daniel Hug <danielpatrickhug@gmail.com>
2023-01-10 22:14:30 -0500

    pre-commit changes

--- backend/oasst_backend/utils/similarity_functions.py
+++ backend/oasst_backend/utils/similarity_functions.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+
 def cosine_similarity(a, b):
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
 
 def euclidean_distance(a, b):
     return np.linalg.norm(a - b)
