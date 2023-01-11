9ebbdab79b8dd07834c94e6826a83ba9c43922c4
Daniel Hug <danielpatrickhug@gmail.com>
2023-01-10 22:10:23 -0500

    added cosime_similarity function and euclidean distance function

---
+++ backend/oasst_backend/utils/similarity_functions.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+def euclidean_distance(a, b):
+    return np.linalg.norm(a - b)
