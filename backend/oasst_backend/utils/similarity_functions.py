7230c9b2972c138501235fcf3dcd2b0d2fc145a2
Daniel Hug <danielpatrickhug@gmail.com>
2023-01-11 12:14:11 -0500

    add typing to similarity functions and added a ZeroDivsionError to cosine similarity

--- backend/oasst_backend/utils/similarity_functions.py
+++ backend/oasst_backend/utils/similarity_functions.py
@@ -1,9 +1,17 @@
+from typing import List
+
 import numpy as np
 
 
-def cosine_similarity(a, b):
-    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+def cosine_similarity(a: List[float], b: List[float]):
+    """Compute cosine similarity (dot product of two vectors divided by the product of their norms.)"""
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    if norm_a == 0 or norm_b == 0:
+        raise ZeroDivisionError("One of the vectors has a norm of zero.")
+    return np.dot(a, b) / (norm_a * norm_b)
 
 
-def euclidean_distance(a, b):
-    return np.linalg.norm(a - b)
+def euclidean_distance(a: List[float], b: List[float]):
+    """Compute euclidean distance (norm of the difference of two vectors.)"""
+    return np.linalg.norm(np.subtract(a, b))
