d5cf4ef81e91c94876d49adfece32eab44d03891
devforfu <developer.z@outlook.com>
2023-01-13 00:06:06 +0100

    Testing without in-memory db

--- backend/oasst_backend/api/v1/text_labels.py
+++ backend/oasst_backend/api/v1/text_labels.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security.api_key import APIKey
 from loguru import logger
@@ -11,23 +13,30 @@ from starlette.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
 router = APIRouter()
 
 
+def prompt_repository_factory(
+    db: Session = Depends(deps.get_db),
+    api_key: APIKey = Depends(deps.get_api_key),
+) -> Callable[[str], PromptRepository]:
+    api_client = deps.api_auth(api_key, db)
+
+    def _factory(user: str) -> PromptRepository | None:
+        return PromptRepository(db, api_client, client_user=user)
+
+    return _factory
+
+
 @router.post("/", status_code=HTTP_204_NO_CONTENT)
 def label_text(
     *,
-    db: Session = Depends(deps.get_db),
-    api_key: APIKey = Depends(deps.get_api_key),
+    factory: Callable[[str], PromptRepository] = Depends(prompt_repository_factory),
     text_labels: protocol_schema.TextLabels,
 ) -> None:
     """
     Label a piece of text.
     """
-    api_client = deps.api_auth(api_key, db)
-
     try:
         logger.info(f"Labeling text {text_labels=}.")
-        pr = PromptRepository(db, api_client, client_user=text_labels.user)
-        pr.store_text_labels(text_labels)
-
+        factory(text_labels.user).store_text_labels(text_labels)
     except Exception:
         logger.exception("Failed to store label.")
         raise HTTPException(
--- backend/tests/unit/oasst_backend/api/v1/test_text_labels.py
+++ backend/tests/unit/oasst_backend/api/v1/test_text_labels.py
@@ -1,23 +1,20 @@
-from typing import Generator
+from unittest import mock
+from uuid import UUID
 
 import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from oasst_backend.api.v1 import text_labels
+from oasst_backend.prompt_repository import PromptRepository
 from oasst_backend.schemas.text_labels import ValidLabelsResponse
-from sqlmodel import Session, create_engine
-
-
-def get_db() -> Generator:
-    with Session(create_engine("sqlite://")) as db:
-        yield db
+from oasst_shared.schemas.protocol import TextLabel, TextLabels, User
+from starlette.status import HTTP_204_NO_CONTENT
 
 
 @pytest.fixture
 def app() -> FastAPI:
     app = FastAPI()
-    app.include_router(text_labels.router, prefix="/text_labels")
-    app.dependency_overrides[text_labels.deps.get_db] = get_db
+    app.include_router(text_labels.router, prefix="")
     return app
 
 
@@ -27,5 +24,39 @@ def client(app: FastAPI) -> TestClient:
 
 
 def test_get_valid_labels(client: TestClient) -> None:
-    response = ValidLabelsResponse.parse_obj(client.get("/text_labels/valid_labels").json())
-    assert response.valid_labels
+    expected_labels = [(l.value, l.display_text, l.help_text) for l in TextLabel]
+
+    actual_labels = [
+        (l.name, l.display_text, l.help_text)
+        for l in ValidLabelsResponse.parse_obj(client.get("/valid_labels").json()).valid_labels
+    ]
+
+    assert expected_labels == actual_labels
+
+
+def test_label_text(client: TestClient) -> None:
+    fake_repo = mock.MagicMock(spec=PromptRepository)
+
+    def make_factory():
+        nonlocal fake_repo
+
+        def _inner(user: str):
+            return fake_repo
+
+        return _inner
+
+    client.app.dependency_overrides[text_labels.prompt_repository_factory] = make_factory
+
+    request = TextLabels(
+        user=User(id="test", display_name="test", auth_method="local"),
+        text="Knock, knock. Who's there? LLM.",
+        labels={TextLabel.humor: 0.8, TextLabel.spam: 0.2},
+        message_id=UUID(int=0),
+        task_id=UUID(int=1),
+    )
+
+    response = client.post("/", content=request.json())
+
+    assert response.status_code == HTTP_204_NO_CONTENT
+    fake_repo.store_text_labels.assert_called_once()
+    assert fake_repo.store_text_labels.call_args[0][0] == request
