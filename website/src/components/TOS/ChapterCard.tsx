e6d8ee7d3e63382c68e71793ccb02cb34a03f33b
rsandb <me@rileysandb.org>
2023-01-12 20:24:18 -0600

    separate cards, section data to page

---
+++ website/src/components/TOS/ChapterCard.tsx
@@ -0,0 +1,35 @@
+import { Box, Flex, Heading, Stack, Text, useColorModeValue } from "@chakra-ui/react";
+
+export interface ChapterInfo {
+  number: string;
+  title: string;
+  desc: string;
+}
+
+export interface ChapterProps {
+  chapter: ChapterInfo;
+  children: React.ReactNode;
+}
+
+export const ChapterCard = ({ chapter, children }: ChapterProps) => {
+  const backgroundColor = useColorModeValue("gray.100", "gray.800");
+
+  return (
+    <Box bg={backgroundColor} p="6" borderRadius="xl" shadow="base">
+      <Stack spacing="4">
+        <Stack>
+          <Flex alignItems="end" gap="2">
+            <Text as="b" fontSize="xl" color="blue.500">
+              {chapter.number}
+            </Text>
+            <Heading as="h3" size="lg">
+              {chapter.title}
+            </Heading>
+          </Flex>
+          <Text>{chapter.desc}</Text>
+        </Stack>
+        {children}
+      </Stack>
+    </Box>
+  );
+};
