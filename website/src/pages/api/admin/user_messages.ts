5282c1182725cc3045ab1fd5dbf080bf0a88ea87
Keith Stevens <fozziethebeat@gmail.com>
2023-01-14 18:14:29 +0900

    Adding an explicit type

--- website/src/pages/api/admin/user_messages.ts
+++ website/src/pages/api/admin/user_messages.ts
@@ -1,12 +1,13 @@
 import { withRole } from "src/lib/auth";
 import { oasstApiClient } from "src/lib/oasst_api_client";
+import type { Message } from "src/types/Conversation";
 
 /**
  * Returns the messages recorded by the backend for a user.
  */
 const handler = withRole("admin", async (req, res) => {
   const { user } = req.query;
-  const messages = await oasstApiClient.fetch_user_messages(user as string);
+  const messages: Message[] = await oasstApiClient.fetch_user_messages(user as string);
   res.status(200).json(messages);
 });
 
