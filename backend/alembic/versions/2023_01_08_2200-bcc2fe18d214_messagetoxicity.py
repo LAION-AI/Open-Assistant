6e47c5089eeae11396d04b16343221ed4b7273eb
Andreas Köpf <andreas.koepf@xamla.com>
2023-01-14 13:20:27 +0100

    Updated down_revision in migration script

--- backend/alembic/versions/2023_01_08_2200-bcc2fe18d214_messagetoxicity.py
+++ backend/alembic/versions/2023_01_08_2200-bcc2fe18d214_messagetoxicity.py
@@ -12,7 +12,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = "bcc2fe18d214"
-down_revision = "20cd871f4ec7"
+down_revision = "846cc08ac79f"
 branch_labels = None
 depends_on = None
 
