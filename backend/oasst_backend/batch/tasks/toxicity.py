dab6e7a3e202a55fc53b21ee4a753fb23d147af4
Nil-Andreu <nilandreug@gmail.com>
2023-01-15 20:52:33 +0100

    [NEW] Get Batch toxic messages

--- backend/oasst_backend/batch/tasks/toxicity.py
+++ backend/oasst_backend/batch/tasks/toxicity.py
@@ -1,6 +1,33 @@
-from oasst_backend.batch.broker.celery import app
+from fastapi_pagination import Params, paginate
+from oasst_backend.database import engine
+from oasst_backend.models import Message, MessageEmbedding
+from sqlmodel import Session
+
+"""from oasst_backend.batch.broker.celery import app
 
 
 @app.task
 def batch_toxicity():
-    return 1
+    return 1"""
+
+
+def get_batch_toxic_messages(pagination: int = 100):
+    class PersonalizedParams(Params):
+        size: int = pagination
+
+    params = PersonalizedParams()
+
+    with Session(engine) as db:
+        res = (
+            db.query(Message.id, Message.payload)
+            .outerjoin(MessageEmbedding)
+            .filter(MessageEmbedding.message_id is None)
+            .order_by(Message.id)
+            .all()
+        )
+
+        return paginate(res, params)
+
+
+if __name__ == "__main__":
+    get_batch_toxic_messages()
