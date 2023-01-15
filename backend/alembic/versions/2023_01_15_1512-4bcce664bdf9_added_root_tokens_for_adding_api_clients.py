89eeb6a4fed897e16f6b75429db841580df45030
Andreas Köpf <andreas.koepf@provisio.com>
2023-01-15 21:26:06 +0100

    Change down revision to current alembic head

--- backend/alembic/versions/2023_01_15_1512-4bcce664bdf9_added_root_tokens_for_adding_api_clients.py
+++ backend/alembic/versions/2023_01_15_1512-4bcce664bdf9_added_root_tokens_for_adding_api_clients.py
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "4bcce664bdf9"
-down_revision = "423557e869e4"
+down_revision = "0964ac95170d"
 branch_labels = None
 depends_on = None
 
