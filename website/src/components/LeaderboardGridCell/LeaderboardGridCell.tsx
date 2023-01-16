425b698d62207cd03093e43e5af236b9e3aaee02
AbdBarho <ka70911@gmail.com>
2023-01-16 07:35:40 +0100

    Remove +1

--- website/src/components/LeaderboardGridCell/LeaderboardGridCell.tsx
+++ website/src/components/LeaderboardGridCell/LeaderboardGridCell.tsx
@@ -8,7 +8,7 @@ import useSWRImmutable from "swr/immutable";
 const columns = [
   {
     Header: "Rank",
-    accessor: (item: LeaderboardEntity, rowIndex: number) => "#" + (item.rank + 1),
+    accessor: (item: LeaderboardEntity, rowIndex: number) => "#" + item.rank,
     style: { width: "90px" },
   },
   {
