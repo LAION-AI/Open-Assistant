16b56e2e23b750d59dce301882a8c5355a63a10f
sir-braz <20201012000256@pucgo.edu.br>
2023-01-19 15:33:17 -0300

    [DRAFT] Updating the GUIDE button on the dashboard

--- website/src/pages/guide.tsx
+++ website/src/pages/guide.tsx
@@ -5,38 +5,36 @@ import { EmptyState } from "src/components/EmptyState";
 import { getDashboardLayout } from "src/components/Layout";
 
 export default function Guide() {
-
-    return (
-        <>
-        <Center flexDirection="column" gap="4" fontSize="lg" className="subpixel-antialiased">
-            <EmptyState
-            text="Sorry, we encountered a server error. We're not sure what went wrong."
-            icon={FiAlertTriangle}
-            />
-            <Box display="flex" flexDirection="column" alignItems="center" gap="2" mt="6">
-            <Text fontSize="sm">If you were trying to contribute data but ended up here, please file a bug.</Text>
-            <Button
-                width="fit-content"
-                leftIcon={<FiAlertTriangle className="text-blue-500" aria-hidden="true" />}
-                variant="solid"
-                size="xs"
+  return (
+    <>
+      <Center flexDirection="column" gap="4" fontSize="lg" className="subpixel-antialiased">
+        <EmptyState
+          text="Sorry, we encountered a server error. We're not sure what went wrong."
+          icon={FiAlertTriangle}
+        />
+        <Box display="flex" flexDirection="column" alignItems="center" gap="2" mt="6">
+          <Text fontSize="sm">If you were trying to contribute data but ended up here, please file a bug.</Text>
+          <Button
+            width="fit-content"
+            leftIcon={<FiAlertTriangle className="text-blue-500" aria-hidden="true" />}
+            variant="solid"
+            size="xs"
+          >
+            <Link
+              key="Report a Bug"
+              href="https://github:com/LAION-AI/Open-Assistant/issues/new/choose"
+              aria-label="Report a Bug"
+              className="flex items-center"
+              _hover={{ textDecoration: "none" }}
+              isExternal
             >
-                <Link
-                key="Report a Bug"
-                href="https://github:com/LAION-AI/Open-Assistant/issues/new/choose"
-                aria-label="Report a Bug"
-                className="flex items-center"
-                _hover={{ textDecoration: "none" }}
-                isExternal
-                >
-                Report a Bug
-                </Link> 
-            </Button>
-            </Box>
-        </Center>
-      </>
-    );
+              Report a Bug
+            </Link>
+          </Button>
+        </Box>
+      </Center>
+    </>
+  );
 }
 
 Guide.getLayout = getDashboardLayout;
-
