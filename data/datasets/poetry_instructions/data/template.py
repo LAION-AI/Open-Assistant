1e8a3ace60c110229f606a6e641eaac84fea57b2
Isaac Rehg <isaacrehg@gmail.com>
2023-01-21 15:07:54 -0600

    add poetry-instructions dataset

---
+++ data/datasets/poetry_instructions/data/template.py
@@ -0,0 +1,16 @@
+CONTINUE_POEM_TEMPLATE = """User: Can you continue this poem for me?
+  {poem_start}
+Assistant: Sure, a continuation for this poem could be:
+  {poem_end}"""
+
+NEW_POEM_IN_STYLE_OF_TEMPLATE = """User: Can you write a poem for me in the style of {author}?
+Assistant: Sure, here's a poem in the style of {author}:
+  {poem}"""
+
+NEW_POEM_ABOUT_TEMPLATE = """User: Can you write me a poem about {about}?
+Assistant: Sure, here's a poem about {about}:
+  {poem}"""
+
+NEW_POEM_ABOUT_IN_STYLE_OF_TEMPLATE = """User: Can you write me a poem about {about} in the style of {author}?
+Assistant: Sure, here's a poem about {about} in the style of {author}:
+  {poem}"""
