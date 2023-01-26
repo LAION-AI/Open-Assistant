220b3cef9974bad6cdffb0ec0d641bc01e0d673e
Oliver <olivergestanley@gmail.com>
2023-01-26 19:27:40 +0000

    Remove outdated check

--- backend/oasst_backend/auth.py
+++ backend/oasst_backend/auth.py
@@ -17,10 +17,7 @@ def authenticate_user(placeholder):  # TODO
 def create_access_token(data: dict):
     expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+    expire = datetime.utcnow() + expires_delta
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET, algorithm=ALGORITHM)
     return encoded_jwt
