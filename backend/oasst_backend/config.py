f420895938b5f3294076723b60de586ae5346ea0
Andreas Köpf <andreas.koepf@provisio.com>
2023-01-26 21:52:29 +0100

    increase lonely child sampling probability

--- backend/oasst_backend/config.py
+++ backend/oasst_backend/config.py
@@ -60,10 +60,10 @@ class TreeManagerConfiguration(BaseModel):
     lonely_children_count: int = 3
     """Number of children below which parents are preferred during sampling for reply tasks."""
 
-    p_lonely_child_extension: float = 0.25
+    p_lonely_child_extension: float = 0.8
     """Probability to select a parent with less than lonely_children_count children."""
 
-    recent_tasks_span_sec: int = 2 * 60
+    recent_tasks_span_sec: int = 3 * 60  # 3 min
     """Time in seconds of recent tasks to consider for exclusion during task selection."""
 
 
