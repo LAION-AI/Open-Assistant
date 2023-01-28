46d20aff4025c79c516b1edbcb3b16c3a3047ab5
Keith Stevens <fozziethebeat@gmail.com>
2023-01-28 21:13:58 +0900

    Final manual change

--- website/public/mockServiceWorker.js
+++ website/public/mockServiceWorker.js
@@ -145,7 +145,7 @@ async function handleRequest(event, requestId) {
   // Ensure MSW is active and ready to handle the message, otherwise
   // this message will pend indefinitely.
   if (client && activeClientIds.has(client.id)) {
-    (async function () {
+    ;(async function () {
       const clonedResponse = response.clone()
       sendToClient(client, {
         type: 'RESPONSE',
