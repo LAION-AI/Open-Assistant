4709223e9334ec729d0a6bac8c2f41e60c30bbc4
Keith Stevens <fozziethebeat@gmail.com>
2023-01-28 20:47:45 +0900

    manually applying the lint fixes that get applied in GitHub actions

--- website/public/mockServiceWorker.js
+++ website/public/mockServiceWorker.js
@@ -8,147 +8,147 @@
  * - Please do NOT serve this file on production.
  */
 
-const INTEGRITY_CHECKSUM = '3d6b9f06410d179a7f7404d4bf4c3c70';
-const activeClientIds = new Set();
+const INTEGRITY_CHECKSUM = '3d6b9f06410d179a7f7404d4bf4c3c70'
+const activeClientIds = new Set()
 
-self.addEventListener("install", function () {
-  self.skipWaiting();
-});
+self.addEventListener('install', function () {
+  self.skipWaiting()
+})
 
-self.addEventListener("activate", function (event) {
-  event.waitUntil(self.clients.claim());
-});
+self.addEventListener('activate', function (event) {
+  event.waitUntil(self.clients.claim())
+})
 
-self.addEventListener("message", async function (event) {
-  const clientId = event.source.id;
+self.addEventListener('message', async function (event) {
+  const clientId = event.source.id
 
   if (!clientId || !self.clients) {
-    return;
+    return
   }
 
-  const client = await self.clients.get(clientId);
+  const client = await self.clients.get(clientId)
 
   if (!client) {
-    return;
+    return
   }
 
   const allClients = await self.clients.matchAll({
-    type: "window",
-  });
+    type: 'window',
+  })
 
   switch (event.data) {
-    case "KEEPALIVE_REQUEST": {
+    case 'KEEPALIVE_REQUEST': {
       sendToClient(client, {
-        type: "KEEPALIVE_RESPONSE",
-      });
-      break;
+        type: 'KEEPALIVE_RESPONSE',
+      })
+      break
     }
 
-    case "INTEGRITY_CHECK_REQUEST": {
+    case 'INTEGRITY_CHECK_REQUEST': {
       sendToClient(client, {
-        type: "INTEGRITY_CHECK_RESPONSE",
+        type: 'INTEGRITY_CHECK_RESPONSE',
         payload: INTEGRITY_CHECKSUM,
-      });
-      break;
+      })
+      break
     }
 
-    case "MOCK_ACTIVATE": {
-      activeClientIds.add(clientId);
+    case 'MOCK_ACTIVATE': {
+      activeClientIds.add(clientId)
 
       sendToClient(client, {
-        type: "MOCKING_ENABLED",
+        type: 'MOCKING_ENABLED',
         payload: true,
-      });
-      break;
+      })
+      break
     }
 
-    case "MOCK_DEACTIVATE": {
-      activeClientIds.delete(clientId);
-      break;
+    case 'MOCK_DEACTIVATE': {
+      activeClientIds.delete(clientId)
+      break
     }
 
-    case "CLIENT_CLOSED": {
-      activeClientIds.delete(clientId);
+    case 'CLIENT_CLOSED': {
+      activeClientIds.delete(clientId)
 
       const remainingClients = allClients.filter((client) => {
-        return client.id !== clientId;
-      });
+        return client.id !== clientId
+      })
 
       // Unregister itself when there are no more clients
       if (remainingClients.length === 0) {
-        self.registration.unregister();
+        self.registration.unregister()
       }
 
-      break;
+      break
     }
   }
-});
+})
 
-self.addEventListener("fetch", function (event) {
-  const { request } = event;
-  const accept = request.headers.get("accept") || "";
+self.addEventListener('fetch', function (event) {
+  const { request } = event
+  const accept = request.headers.get('accept') || ''
 
   // Bypass server-sent events.
-  if (accept.includes("text/event-stream")) {
-    return;
+  if (accept.includes('text/event-stream')) {
+    return
   }
 
   // Bypass navigation requests.
-  if (request.mode === "navigate") {
-    return;
+  if (request.mode === 'navigate') {
+    return
   }
 
-  // Opening the DevTools triggers the "only-if-cached" request
+  // Opening the DevTools triggers the 'only-if-cached' request
   // that cannot be handled by the worker. Bypass such requests.
-  if (request.cache === "only-if-cached" && request.mode !== "same-origin") {
-    return;
+  if (request.cache === 'only-if-cached' && request.mode !== 'same-origin') {
+    return
   }
 
   // Bypass all requests when there are no active clients.
   // Prevents the self-unregistered worked from handling requests
   // after it's been deleted (still remains active until the next reload).
   if (activeClientIds.size === 0) {
-    return;
+    return
   }
 
   // Generate unique request ID.
-  const requestId = Math.random().toString(16).slice(2);
+  const requestId = Math.random().toString(16).slice(2)
 
   event.respondWith(
     handleRequest(event, requestId).catch((error) => {
-      if (error.name === "NetworkError") {
+      if (error.name === 'NetworkError') {
         console.warn(
-          '[MSW] Successfully emulated a network error for the "%s %s" request.',
+          '[MSW] Successfully emulated a network error for the '%s %s' request.',
           request.method,
           request.url
-        );
-        return;
+        )
+        return
       }
 
       // At this point, any exception indicates an issue with the original request/response.
       console.error(
         `\
-[MSW] Caught an exception from the "%s %s" request (%s). This is probably not a problem with Mock Service Worker. There is likely an additional logging output above.`,
+[MSW] Caught an exception from the '%s %s' request (%s). This is probably not a problem with Mock Service Worker. There is likely an additional logging output above.`,
         request.method,
         request.url,
         `${error.name}: ${error.message}`
-      );
+      )
     })
-  );
-});
+  )
+})
 
 async function handleRequest(event, requestId) {
-  const client = await resolveMainClient(event);
-  const response = await getResponse(event, client, requestId);
+  const client = await resolveMainClient(event)
+  const response = await getResponse(event, client, requestId)
 
-  // Send back the response clone for the "response:*" life-cycle events.
+  // Send back the response clone for the 'response:*' life-cycle events.
   // Ensure MSW is active and ready to handle the message, otherwise
   // this message will pend indefinitely.
   if (client && activeClientIds.has(client.id)) {
     (async function () {
-      const clonedResponse = response.clone();
+      const clonedResponse = response.clone()
       sendToClient(client, {
-        type: "RESPONSE",
+        type: 'RESPONSE',
         payload: {
           requestId,
           type: clonedResponse.type,
@@ -159,11 +159,11 @@ async function handleRequest(event, requestId) {
           headers: Object.fromEntries(clonedResponse.headers.entries()),
           redirected: clonedResponse.redirected,
         },
-      });
-    })();
+      })
+    })()
   }
 
-  return response;
+  return response
 }
 
 // Resolve the main client for the given event.
@@ -171,68 +171,68 @@ async function handleRequest(event, requestId) {
 // that registered the worker. It's with the latter the worker should
 // communicate with during the response resolving phase.
 async function resolveMainClient(event) {
-  const client = await self.clients.get(event.clientId);
+  const client = await self.clients.get(event.clientId)
 
-  if (client?.frameType === "top-level") {
-    return client;
+  if (client?.frameType === 'top-level') {
+    return client
   }
 
   const allClients = await self.clients.matchAll({
-    type: "window",
-  });
+    type: 'window',
+  })
 
   return allClients
     .filter((client) => {
       // Get only those clients that are currently visible.
-      return client.visibilityState === "visible";
+      return client.visibilityState === 'visible'
     })
     .find((client) => {
       // Find the client ID that's recorded in the
       // set of clients that have registered the worker.
-      return activeClientIds.has(client.id);
-    });
+      return activeClientIds.has(client.id)
+    })
 }
 
 async function getResponse(event, client, requestId) {
-  const { request } = event;
-  const clonedRequest = request.clone();
+  const { request } = event
+  const clonedRequest = request.clone()
 
   function passthrough() {
     // Clone the request because it might've been already used
     // (i.e. its body has been read and sent to the client).
-    const headers = Object.fromEntries(clonedRequest.headers.entries());
+    const headers = Object.fromEntries(clonedRequest.headers.entries())
 
     // Remove MSW-specific request headers so the bypassed requests
     // comply with the server's CORS preflight check.
-    // Operate with the headers as an object because request "Headers"
+    // Operate with the headers as an object because request 'Headers'
     // are immutable.
-    delete headers["x-msw-bypass"];
+    delete headers['x-msw-bypass']
 
-    return fetch(clonedRequest, { headers });
+    return fetch(clonedRequest, { headers })
   }
 
   // Bypass mocking when the client is not active.
   if (!client) {
-    return passthrough();
+    return passthrough()
   }
 
   // Bypass initial page load requests (i.e. static assets).
   // The absence of the immediate/parent client in the map of the active clients
-  // means that MSW hasn't dispatched the "MOCK_ACTIVATE" event yet
+  // means that MSW hasn't dispatched the 'MOCK_ACTIVATE' event yet
   // and is not ready to handle requests.
   if (!activeClientIds.has(client.id)) {
-    return passthrough();
+    return passthrough()
   }
 
   // Bypass requests with the explicit bypass header.
-  // Such requests can be issued by "ctx.fetch()".
-  if (request.headers.get("x-msw-bypass") === "true") {
-    return passthrough();
+  // Such requests can be issued by 'ctx.fetch()'.
+  if (request.headers.get('x-msw-bypass') === 'true') {
+    return passthrough()
   }
 
   // Notify the client that a request has been intercepted.
   const clientMessage = await sendToClient(client, {
-    type: "REQUEST",
+    type: 'REQUEST',
     payload: {
       id: requestId,
       url: request.url,
@@ -250,53 +250,53 @@ async function getResponse(event, client, requestId) {
       bodyUsed: request.bodyUsed,
       keepalive: request.keepalive,
     },
-  });
+  })
 
   switch (clientMessage.type) {
-    case "MOCK_RESPONSE": {
-      return respondWithMock(clientMessage.data);
+    case 'MOCK_RESPONSE': {
+      return respondWithMock(clientMessage.data)
     }
 
-    case "MOCK_NOT_FOUND": {
-      return passthrough();
+    case 'MOCK_NOT_FOUND': {
+      return passthrough()
     }
 
-    case "NETWORK_ERROR": {
-      const { name, message } = clientMessage.data;
-      const networkError = new Error(message);
-      networkError.name = name;
+    case 'NETWORK_ERROR': {
+      const { name, message } = clientMessage.data
+      const networkError = new Error(message)
+      networkError.name = name
 
-      // Rejecting a "respondWith" promise emulates a network error.
-      throw networkError;
+      // Rejecting a 'respondWith' promise emulates a network error.
+      throw networkError
     }
   }
 
-  return passthrough();
+  return passthrough()
 }
 
 function sendToClient(client, message) {
   return new Promise((resolve, reject) => {
-    const channel = new MessageChannel();
+    const channel = new MessageChannel()
 
     channel.port1.onmessage = (event) => {
       if (event.data && event.data.error) {
-        return reject(event.data.error);
+        return reject(event.data.error)
       }
 
-      resolve(event.data);
-    };
+      resolve(event.data)
+    }
 
-    client.postMessage(message, [channel.port2]);
-  });
+    client.postMessage(message, [channel.port2])
+  })
 }
 
 function sleep(timeMs) {
   return new Promise((resolve) => {
-    setTimeout(resolve, timeMs);
-  });
+    setTimeout(resolve, timeMs)
+  })
 }
 
 async function respondWithMock(response) {
-  await sleep(response.delay);
-  return new Response(response.body, response);
+  await sleep(response.delay)
+  return new Response(response.body, response)
 }
