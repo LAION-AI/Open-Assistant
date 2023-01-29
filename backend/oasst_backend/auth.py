b15a5ccc8a9b87b7b13693fd8e2d6844b2a096be
Oliver <olivergestanley@gmail.com>
2023-01-29 12:38:32 +0000

    Remove now redundant methods

--- backend/oasst_backend/auth.py
+++ backend/oasst_backend/auth.py
@@ -1,12 +1,9 @@
 from datetime import datetime, timedelta
 from typing import Optional
-from uuid import UUID
 
-from jose import JWTError, jwt
+from jose import jwt
 from oasst_backend.models import Account
-from oasst_shared.exceptions.oasst_api_error import OasstError, OasstErrorCode
 from sqlmodel import Session
-from starlette.status import HTTP_401_UNAUTHORIZED
 
 SECRET = "test"  # TODO: read this from configuration. maybe use an auth manager object to make this cleaner
 ALGORITHM = "HS256"
@@ -23,20 +20,6 @@ def create_access_token(data: dict) -> str:
     return encoded_jwt
 
 
-def get_current_user(db: Session, token: str) -> Account:
-    try:
-        payload = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
-        account_id: UUID = payload.get("sub")
-        if account_id is None:
-            raise OasstError("Invalid authentication", OasstErrorCode.INVALID_AUTHENTICATION, HTTP_401_UNAUTHORIZED)
-    except JWTError:
-        raise OasstError("Invalid authentication", OasstErrorCode.INVALID_AUTHENTICATION, HTTP_401_UNAUTHORIZED)
-    user = get_account_by_id(db, account_id)
-    if user is None:
-        raise OasstError("Invalid authentication", OasstErrorCode.INVALID_AUTHENTICATION, HTTP_401_UNAUTHORIZED)
-    return user
-
-
 def get_account_from_discord_user(db: Session, discord_user: dict) -> Optional[Account]:
     account: Account = (
         db.query(Account)
@@ -47,9 +30,4 @@ def get_account_from_discord_user(db: Session, discord_user: dict) -> Optional[A
         .first()
     )
 
-    return account
-
-
-def get_account_by_id(db: Session, id: UUID) -> Optional[Account]:
-    account: Account = db.query(Account).filter(Account.id == id).first()
     return account
