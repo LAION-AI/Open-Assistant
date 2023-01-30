1f4113d8beff0e56f8b97e99af3d43103795dbc3
Oliver Stanley <oliver.stanley@kainos.com>
2023-01-30 10:46:02 +0000

    Cleanup

--- backend/oasst_backend/api/v1/login.py
+++ backend/oasst_backend/api/v1/login.py
@@ -1,5 +1,5 @@
 import requests
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
 from oasst_backend import auth
 from oasst_backend.api import deps
 from oasst_backend.config import Settings
@@ -11,34 +11,31 @@ from starlette.status import HTTP_401_UNAUTHORIZED
 
 router = APIRouter()
 
-# TODO: replace placeholder
-REDIRECT_URI = "http://localhost:8000/api/v1/callback_discord"
 
-
-# TODO: make this not only discord
-@router.get("/login_discord")
-def login_discord():
-    # TODO: remove hardcoded URLs
-    auth_url = f"https://discord.com/api/oauth2/authorize?client_id={Settings.DISCORD_CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code&scope=identify"
+@router.get("/login/discord")
+def login_discord(request: Request):
+    redirect_uri = get_discord_callback_uri(request)
+    auth_url = f"https://discord.com/api/oauth2/authorize?client_id={Settings.AUTH_DISCORD_CLIENT_ID}&redirect_uri={redirect_uri}&response_type=code&scope=identify"
     raise HTTPException(status_code=302, headers={"location": auth_url})
 
 
-@router.get("/callback_discord", response_model=protocol_schema.Token)
+@router.get("/callback/discord", response_model=protocol_schema.Token)
 def callback_discord(
     auth_code: str,
+    request: Request,
     db: Session = Depends(deps.get_db),
 ):
+    redirect_uri = get_discord_callback_uri(request)
+
     # Exchange the auth code for an access token
-    # TODO: remove hardcoded discord URLs
     token_response = requests.post(
         "https://discord.com/api/oauth2/token",
         data={
-            "client_id": Settings.DISCORD_CLIENT_ID,
-            "client_secret": Settings.DISCORD_CLIENT_SECRET,
+            "client_id": Settings.AUTH_DISCORD_CLIENT_ID,
+            "client_secret": Settings.AUTH_DISCORD_CLIENT_SECRET,
             "grant_type": "authorization_code",
             "code": auth_code,
-            # TODO replace this with the URI we want to redirect to, must match the one used when setting up OAuth2 client with Discord
-            "redirect_uri": REDIRECT_URI,
+            "redirect_uri": redirect_uri,
             "scope": "identify",
         },
     )
@@ -47,7 +44,6 @@ def callback_discord(
     access_token = token_response.json()["access_token"]
 
     # Retrieve user's Discord information using access token
-    # TODO: remove hardcoded discord URLs
     user_response = requests.get(
         "https://discord.com/api/users/@me", headers={"Authorization": f"Bearer {access_token}"}
     )
@@ -66,3 +62,11 @@ def callback_discord(
     access_token = auth.create_access_token(account)
 
     return protocol_schema.Token(access_token=access_token, token_type="bearer")
+
+
+def get_discord_callback_uri(request: Request):
+    # This seems ugly, not sure if there is a better way
+    current_url = str(request.url)
+    domain = current_url.split("/api/v1/")[0]
+    redirect_uri = f"{domain}/api/v1/callback/discord"
+    return redirect_uri
--- backend/oasst_backend/auth.py
+++ backend/oasst_backend/auth.py
@@ -2,20 +2,17 @@ from datetime import datetime, timedelta
 from typing import Optional
 
 from jose import jwt
+from oasst_backend.config import Settings
 from oasst_backend.models import Account
 from sqlmodel import Session
 
-SECRET = "test"  # TODO: read this from configuration. maybe use an auth manager object to make this cleaner
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 
 def create_access_token(data: dict) -> str:
-    expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expires_delta = timedelta(minutes=Settings.AUTH_ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode = data.copy()
     expire = datetime.utcnow() + expires_delta
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, Settings.AUTH_SECRET, algorithm=Settings.AUTH_ALGORITHM)
     # TODO store JWT here?
     return encoded_jwt
 
