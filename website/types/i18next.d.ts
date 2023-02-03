86eac87c19512c1a98821a59ec0a8668e96a8287
AbdBarho <ka70911@gmail.com>
2023-02-03 20:53:20 +0100

    Add ToS/License Agreement to Website (#1080)
    * add new apis to oasst client
    
    * add tos handler
    
    * Add ToS to Dashboard
    
    * use Provider for ToS
    
    * simplify provider
    
    * fix error
    
    * Inject into JWT
    
    * primitive error handling
    
    * update comment
    
    * address review

--- website/types/i18next.d.ts
+++ website/types/i18next.d.ts
@@ -1,11 +1,12 @@
 import type common from "public/locales/en/common.json";
 import type dashboard from "public/locales/en/dashboard.json";
 import type index from "public/locales/en/index.json";
+import type labelling from "public/locales/en/labelling.json";
 import type leaderboard from "public/locales/en/leaderboard.json";
 import type message from "public/locales/en/message.json";
-import type labelling from "public/locales/en/labelling.json";
-import type tasks from "public/locales/en/tasks.json";
 import type side_menu from "public/locales/en/side_menu.json";
+import type tasks from "public/locales/en/tasks.json";
+import type tos from "public/locales/en/tos.json";
 
 declare module "i18next" {
   interface CustomTypeOptions {
@@ -18,6 +19,7 @@ declare module "i18next" {
       message: typeof message;
       labelling: typeof labelling;
       side_menu: typeof side_menu;
+      tos: typeof tos;
     };
   }
 }
