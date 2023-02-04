973885e9dba5c6ff05f5ee5eb32fb3c841848b8a
theopfr <teddypeifer@gmail.com>
2023-02-04 22:57:28 +0100

    removed border from color-theme toggle button

--- website/src/components/Header/ColorModeToggler.jsx
+++ website/src/components/Header/ColorModeToggler.jsx
@@ -5,7 +5,7 @@ const ColorModeToggler = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Button size="md" width="70px" justifyContent="center" onClick={toggleColorMode} gap="4" variant="outline">
+    <Button size="md" width="70px" justifyContent="center" onClick={toggleColorMode} gap="4" variant="ghost">
       <Sun size={"2em"} />
     </Button>
   );
