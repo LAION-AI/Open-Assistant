006fe78f721146770fd9516cf1d5fedeb39f4a05
Shroominic <db2002dominic@gmail.com>
2023-02-04 23:15:17 +0100

    init project structure

--- discordpy-bot/clients/data_collection_bot.py
+++
@@ -1,5 +0,0 @@
-""" 
-Data Collection Bot
-Used to collect data for training the model.
-Not intended to be used in production.
-"""
--- discordpy-bot/clients/inference_bot.py
+++
@@ -1,5 +0,0 @@
-""" 
-Inference Bot
-Used to run the model in production.
-The final chatbot like chatgpt.
-"""
