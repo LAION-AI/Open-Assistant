f002c7be938eeac0e23adddadaebbf307aa3153e
letier3110 <letier3110@gmail.com>
2023-02-06 00:09:06 +0400

    fix: rename to valid locale

--- website/next-i18next.config.js
+++ website/next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["bn", "de", "en", "es", "fr", "hu", "ja", "ko", "pt-BR", "ru", "ua", "vi", "zh", "tr"],
+    locales: ["bn", "de", "en", "es", "fr", "hu", "ja", "ko", "pt-BR", "ru", "uk-UA", "vi", "zh", "tr"],
   },
 };
