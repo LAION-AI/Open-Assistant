d02b479744a742610185a6c8f7d76ec2b4ae4726
Saifeddine <aloui.seifeddine@gmail.com>
2023-02-06 00:26:14 +0100

    Added Arabic Locale and fixed text direction broblem

--- website/next-i18next.config.js
+++ website/next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["bn", "de", "en", "es", "fr", "hu", "ja", "pt-BR", "ru", "vi", "zh"],
+    locales: ["ar", "bn", "de", "en", "es", "fr", "hu", "ja", "pt-BR", "ru", "vi", "zh"],
   },
 };
