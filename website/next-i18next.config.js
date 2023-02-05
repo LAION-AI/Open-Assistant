0f730e967fc8fee5a05cefc82441efb0b6c4e692
losnaj <losnaj@users.noreply.github.com>
2023-02-05 22:35:25 +0100

    added "ca" flag

--- website/next-i18next.config.js
+++ website/next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["bn", "de", "en", "es", "fr", "hu", "ja", "ko", "pt-BR", "ru", "uk-UA", "vi", "zh", "tr"],
+    locales: ["bn", "ca", "de", "en", "es", "fr", "hu", "ja", "ko", "pt-BR", "ru", "uk-UA", "vi", "zh", "tr"],
   },
 };
