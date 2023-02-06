81167f69d5cc450fb8d6f106ebead36013bdec05
danivfe <66723629+danivfe@users.noreply.github.com>
2023-02-06 12:42:18 +0100

    Add Catalan Language (#1212)
    * Add Catalan Language

--- website/next-i18next.config.js
+++ website/next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["ar", "bn", "de", "en", "es", "fr", "hu", "ja", "ko", "pt-BR", "ru", "uk-UA", "vi", "zh", "tr"],
+    locales: ["ar", "bn", "ca", "de", "en", "es", "fr", "hu", "ja", "ko", "pt-BR", "ru", "uk-UA", "vi", "zh", "tr"],
   },
 };
