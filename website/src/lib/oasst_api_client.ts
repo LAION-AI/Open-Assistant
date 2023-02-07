faa2d6e1970630b34430dc9633c3958e9233a209
Keith Stevens <fozziethebeat@gmail.com>
2023-02-07 18:01:09 +0900

    update the backend path to manage user status

--- website/src/lib/oasst_api_client.ts
+++ website/src/lib/oasst_api_client.ts
@@ -267,7 +267,7 @@ export class OasstApiClient {
    * Updates the backend's knowledge about the `user_id`.
    */
   async set_user_status(user_id: string, is_enabled: boolean, notes: string): Promise<void> {
-    await this.put(`/api/v1/users/users/${user_id}?enabled=${is_enabled}&notes=${notes}`);
+    await this.put(`/api/v1/users/${user_id}?enabled=${is_enabled}&notes=${notes}`);
   }
 
   /**
