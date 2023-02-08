5f39e7b1d9dbe5fd5112d2448d9260be5bdb78f7
Damien Tanner <damien@tanner.me>
2023-02-08 16:17:46 +0000

    recipes dataset
    An instruction dataset of recipes
    
    Tasty recipes
    
    new folder structure
    
    Convert fraction unicode chars in method

--- data/datasets/__init__.py
+++ data/datasets/__init__.py
@@ -1,3 +1,6 @@
 TEXT_DATASETS = {}
 
-INSTRUCTION_DATASETS = {"grade-school-math-instructions": "qwedsacf/grade-school-math-instructions"}
+INSTRUCTION_DATASETS = {
+    "grade-school-math-instructions": "qwedsacf/grade-school-math-instructions",
+    "recipes": "dctanner/oa_recipes",
+}
