bd6ace52b1bd40a2c7b300d2abcc4c3b181dd8be
notmd <tinhmeo10@gmail.com>
2023-02-09 16:54:59 +0700

    fix message reaction count visibility

--- website/src/components/Messages/MessageEmojiButton.tsx
+++ website/src/components/Messages/MessageEmojiButton.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@chakra-ui/react";
+import { useTaskContext } from "src/context/TaskContext";
+import { useHasRole } from "src/hooks/auth/useHasRole";
 import { MessageEmoji } from "src/types/Conversation";
 import { emojiIcons } from "src/types/Emoji";
 
@@ -6,24 +8,47 @@ interface MessageEmojiButtonProps {
   emoji: MessageEmoji;
   checked?: boolean;
   onClick: () => void;
-  showCount: boolean;
+  userIsAuthor: boolean;
+  disabled?: boolean;
+  userReacted: boolean;
 }
 
-export const MessageEmojiButton = ({ emoji, checked, onClick, showCount }: MessageEmojiButtonProps) => {
+export const MessageEmojiButton = ({
+  emoji,
+  checked,
+  onClick,
+  userIsAuthor,
+  disabled,
+  userReacted,
+}: MessageEmojiButtonProps) => {
   const EmojiIcon = emojiIcons.get(emoji.name);
-  if (!EmojiIcon) return <></>;
+  const isAdmin = useHasRole("admin");
+  const isTaskPage = !!useTaskContext();
+
+  if (!EmojiIcon) return null;
+
+  const isDisabled = !!(userIsAuthor ? true : disabled);
+  const showCount = (emoji.count > 0 && !isTaskPage) || userIsAuthor || isAdmin || userReacted;
+
+  const handleOnClick = () => {
+    if (!isDisabled) {
+      onClick();
+    }
+  };
+
   return (
     <Button
-      onClick={onClick}
+      onClick={handleOnClick}
       variant={checked ? "solid" : "ghost"}
       colorScheme={checked ? "blue" : undefined}
       size="sm"
       height="1.6em"
       minWidth={0}
       padding="0"
+      color={isDisabled ? "gray.500" : undefined}
     >
       <EmojiIcon style={{ height: "1em" }} />
-      {emoji.count > 0 && showCount && <span style={{ marginInlineEnd: "0.25em" }}>{emoji.count}</span>}
+      {showCount && <span style={{ marginInlineEnd: "0.25em" }}>{emoji.count}</span>}
     </Button>
   );
 };
