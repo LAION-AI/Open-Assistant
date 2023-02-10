2fbf2fa4575d1545c1513adf0a369bdc65ea63ef
jack.butler <jack.butler@faculty.ai>
2023-02-10 09:46:58 +0000

    add docstring info about tokenizer matching

--- model/supervised_finetuning/utils.py
+++ model/supervised_finetuning/utils.py
@@ -36,8 +36,11 @@ TOKENIZER_CONFIGS = {
 
 
 def match_tokenizer_name(model_name: str) -> TokenizerConfig:
-    """Match a partial model name to a tokenizer configuration"""
-    tokenizer_config_matches = [config for name, config in TOKENIZER_CONFIGS.items() if model_name in name]
+    """
+    Match a partial model name to a tokenizer configuration
+    i.e. model_name `Salesforce/codegen-2B-multi` has config name `codegen`
+    """
+    tokenizer_config_matches = [config for name, config in TOKENIZER_CONFIGS.items() if name in model_name]
     if not tokenizer_config_matches:
         raise ValueError(f"Cannot find any tokeniser configuration to match {model_name=}")
     elif 1 < len(tokenizer_config_matches):
