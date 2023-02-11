8fc276a0583ffe7201d02fde7010414a7e0513e7
notmd <tinhmeo10@gmail.com>
2023-02-11 13:34:05 +0700

    allow mod to ban user

--- website/src/pages/api/admin/update_user.ts
+++ website/src/pages/api/admin/update_user.ts
@@ -1,3 +1,4 @@
+import { ROLES } from "src/components/RoleSelect";
 import { withRole } from "src/lib/auth";
 import { createApiClient } from "src/lib/oasst_client_factory";
 import prisma from "src/lib/prismadb";
@@ -6,16 +7,18 @@ import prisma from "src/lib/prismadb";
  * Update's the user's data in the database.  Accessible only to admins.
  */
 const handler = withRole("admin", async (req, res, token) => {
-  const { id, auth_method, user_id, notes, role, show_on_leaderboard } = req.body;
+  const { id, user_id, notes, role, show_on_leaderboard } = req.body;
 
-  const oasstApiClient = await createApiClient(token);
-  // If the user is authorized by the web, update their role.
-  if (auth_method === "local") {
-    await prisma.user.update({
-      where: { id },
-      data: { role },
-    });
+  // mod can't update user role to mod or admin
+  if (token.role === ROLES.MODERATOR && (role === ROLES.MODERATOR || role === ROLES.ADMIN)) {
+    return res.status(403);
   }
+  const oasstApiClient = await createApiClient(token);
+  await prisma.user.update({
+    where: { id },
+    data: { role },
+  });
+
   // Tell the backend the user's enabled or not enabled status.
   await oasstApiClient.set_user_status(user_id, role !== "banned", notes, show_on_leaderboard);
 
