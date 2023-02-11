3f679470e18ea8e0f29d04209e09005e2131fb6a
Mehdi Zibout <mehdizibout@gmail.com>
2023-02-11 14:41:54 +0100

    Remove unneeded check

--- website/src/components/CollapsableText.tsx
+++ website/src/components/CollapsableText.tsx
@@ -5,12 +5,6 @@ export const CollapsableText = ({ text, maxLength = 220 }: { text: ReactNode; ma
     return <>{text}</>;
   } else {
     const visibleText = text.substring(0, maxLength - 3);
-    const dots = visibleText[visibleText.length - 1] === "." ? ".." : "...";
-    return (
-      <span>
-        {visibleText}
-        {dots}
-      </span>
-    );
+    return <span>{visibleText}...</span>;
   }
 };
