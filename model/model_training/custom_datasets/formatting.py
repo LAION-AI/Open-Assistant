893da110e4ef212fe957bcde9044a4b820837a7e
Sotirios Anagnostidis <sanagnos@porcino.inf.ethz.ch>
2023-02-12 00:09:40 +0100

    fix bug

--- model/model_training/custom_datasets/formatting.py
+++ model/model_training/custom_datasets/formatting.py
@@ -2,10 +2,9 @@ QA_SPECIAL_TOKENS = {"Question": "<human>", "Answer": "<bot>", "StartPrefix": "<
 
 
 def format_pair(pairs):
-    assert len(pairs) % 2 == 0
     return [
         "{}{}{}".format(QA_SPECIAL_TOKENS["Question"], pairs[i], QA_SPECIAL_TOKENS["Answer"])
         if i % 2 == 0
         else pairs[i]
-        for i in range(0, len(pairs), 2)
+        for i in range(len(pairs))
     ]
