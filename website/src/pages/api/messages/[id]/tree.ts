42609a3096ead3b6a34d8e2017953f3c036ba72a
notmd <tinhmeo10@gmail.com>
2023-02-13 00:42:05 +0700

    remove dup code

--- website/src/pages/api/messages/[id]/tree.ts
+++ website/src/pages/api/messages/[id]/tree.ts
@@ -1,7 +1,7 @@
 import { ROLES } from "src/components/RoleSelect";
 import { withoutRole } from "src/lib/auth";
 import { createApiClient } from "src/lib/oasst_client_factory";
-import { Message, MessageWithChildren } from "src/types/Conversation";
+import { buildTree } from "src/pages/api/admin/messages/[id]/tree";
 
 export default withoutRole("banned", async (req, res, token) => {
   const client = await createApiClient(token);
@@ -20,38 +20,3 @@ export default withoutRole("banned", async (req, res, token) => {
 
   return res.json({ tree, message: response.messages.find((m) => m.id === messageId) });
 });
-
-// https://medium.com/@lizhuohang.selina/building-a-hierarchical-tree-from-a-flat-list-an-easy-to-understand-solution-visualisation-19cb24bdfa33
-const buildTree = (messages: Message[]): MessageWithChildren | null => {
-  const map: Record<string, MessageWithChildren> = {};
-  const tree = [];
-
-  // Build a hash table and map items to objects
-  messages.forEach(function (item) {
-    const id = item.id;
-    if (!map[id]) {
-      map[id] = { ...item, children: [] };
-    }
-  });
-
-  // Loop over hash table
-  let mappedElem: MessageWithChildren;
-  for (const id in map) {
-    if (map[id]) {
-      mappedElem = map[id];
-
-      // If the element is not at the root level, add it to its parent array of children. Note this will continue till we have only root level elements left
-      if (mappedElem.parent_id) {
-        const parentId = mappedElem.parent_id;
-        map[parentId].children.push(mappedElem);
-      }
-
-      // If the element is at the root level, directly push to the tree
-      else {
-        tree.push(mappedElem);
-      }
-    }
-  }
-
-  return tree.shift() || null;
-};
