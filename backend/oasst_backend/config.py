6794f0a4ba28540c8350653b9fff04e81e404da7
Andreas Köpf <andreas.koepf@provisio.com>
2023-02-14 23:31:34 +0100

    remove BACKEND_CORS_ORIGINS validator

--- backend/oasst_backend/config.py
+++ backend/oasst_backend/config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional
 
 from oasst_shared.schemas.protocol import TextLabel
 from pydantic import AnyHttpUrl, BaseModel, BaseSettings, FilePath, PostgresDsn, validator
@@ -214,14 +214,6 @@ class Settings(BaseSettings):
 
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
     UPDATE_ALEMBIC: bool = True
-
-    @validator("BACKEND_CORS_ORIGINS", pre=True)
-    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
-        if isinstance(v, str) and not v.startswith("["):
-            return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
-            return v
-        raise ValueError(v)
 
     tree_manager: Optional[TreeManagerConfiguration] = TreeManagerConfiguration()
 
