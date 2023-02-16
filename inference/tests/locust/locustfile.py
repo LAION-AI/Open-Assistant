27b3fc03b69ebb5be2ec5b5295a01047a4deb206
jack.butler <jack.butler@faculty.ai>
2023-02-16 14:53:27 +0000

    adds locust test for sending chat messages

--- inference/tests/locust/locustfile.py
+++ inference/tests/locust/locustfile.py
@@ -1,4 +1,7 @@
-from locust import HttpUser, between
+import json
+
+import sseclient
+from locust import HttpUser, between, task
 
 
 class ChatUser(HttpUser):
@@ -7,3 +10,17 @@ class ChatUser(HttpUser):
     def on_start(self):
         response = self.client.post("/chat", json={}).json()
         self.chat_id = response["id"]
+
+    @task
+    def send_msg(self):
+        response = self.client.post(
+            f"/chat/{self.chat_id}/message",
+            json={"message": "yo"},
+            stream=True,
+            headers={"Accept": "text/event-stream"},
+        )
+        response.raise_for_status()
+
+        client = sseclient.SSEClient(response)
+        for event in client.events():
+            _ = json.loads(event.data)
