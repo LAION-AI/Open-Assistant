4f34d7cb598eed651521f7a4865b4ce3e7fb9996
Daniel Hug <danielpatrickhug@gmail.com>
2023-02-16 17:25:01 -0500

    initial commit to add topic modeling.

---
+++ backend/oasst_backend/utils/message_tree_topic_modeling.py
@@ -0,0 +1,62 @@
+from exported_tree_loading import load_data
+from similarity_functions import embed_data, compute_cos_sim_kernel, k_hop_message_passing_sparse
+from sentence_transformers import SentenceTransformer
+from bertopic import BERTopic
+from bertopic.representation import MaximalMarginalRelevance
+from sklearn.feature_extraction.text import CountVectorizer
+from bertopic.vectorizers import ClassTfidfTransformer
+
+MODEL_NAME='all-MiniLM-L6-v2'
+
+def load_topic_model():
+    vectorizer_model = CountVectorizer(stop_words="english")
+    ctfidf_model = ClassTfidfTransformer(reduce_frequent_words=False)
+    model = SentenceTransformer(MODEL_NAME)
+    representation_model = MaximalMarginalRelevance(diversity=0.2)
+    topic_model = BERTopic(nr_topics="auto", min_topic_size=10, representation_model=representation_model, vectorizer_model=vectorizer_model, ctfidf_model=ctfidf_model, embedding_model=model)
+    return topic_model
+
+def fit_topic_model(topic_model, data, embeddings, key="query"):
+    topics, probs = topic_model.fit_transform(data[key].to_list(), embeddings)
+    return topics, probs
+
+def get_topic_info(topic_model):
+    return topic_model.get_topic_info()
+
+def reduce_topics(topic_model, data, nr_topics, key="query"):
+    topic_model.reduce_topics(data[key].to_list(), nr_topics)
+    return topic_model
+
+def get_representative_docs(topic_model):
+    return topic_model.get_representative_documents()
+
+def reduce_outliers(topic_model, data, topics, probs, key="query", strategy="c-tf-idf"):
+    if strategy == "c-tf-idf":
+        new_topics = topic_model.reduce_outliers(data[key].to_list(), topics, strategy, threshold=0.1)
+    elif strategy == "embeddings":
+        new_topics = topic_model.reduce_outliers(data[key].to_list(), topics, strategy)
+    elif strategy == "distributions":
+        new_topics = topic_model.reduce_outliers(data[key].to_list(), topics, probabilities=probs, strategy=strategy)
+    else:
+        raise ValueError("Invalid strategy")
+    return new_topics
+
+def compute_heirarchical_topic_tree(topic_model, data, key="query"):
+    hierarchical_topics = topic_model.hierarchical_topics(data[key].to_list())
+    tree = topic_model.get_topic_tree(hierarchical_topics)
+    return hierarchical_topics, tree
+
+
+data, message_list = load_data(['./2023-02-06_oasst_prod.jsonl'])
+embs = embed_data(data, model_name=MODEL_NAME, cores=1, gpu=False)
+adj_matrix = compute_cos_sim_kernel(embs)
+print(adj_matrix.shape)
+print(embs.shape)
+A_k, agg_features = k_hop_message_passing_sparse(adj_matrix, embs, 2)
+print(A_k.shape)
+topic_model = load_topic_model()
+topics, probs = fit_topic_model(topic_model, data, agg_features)
+freq = get_topic_info(topic_model)
+rep_docs = get_representative_docs(topic_model)
+print(freq)
+print(rep_docs)
--- backend/oasst_backend/utils/similarity_functions.py
+++ backend/oasst_backend/utils/similarity_functions.py
@@ -1,17 +1,142 @@
+import math
 from typing import List
 
 import numpy as np
+import scipy.sparse as sp
+import torch
+from scipy.sparse import csr_matrix
+from sentence_transformers import SentenceTransformer
+from torch import Tensor
+from tqdm import tqdm
+
+THRESHOLD = 0.65
 
 
-def cosine_similarity(a: List[float], b: List[float]):
-    """Compute cosine similarity (dot product of two vectors divided by the product of their norms.)"""
-    norm_a = np.linalg.norm(a)
-    norm_b = np.linalg.norm(b)
-    if norm_a == 0 or norm_b == 0:
-        raise ZeroDivisionError("One of the vectors has a norm of zero.")
-    return np.dot(a, b) / (norm_a * norm_b)
+def embed_data(data, key="query", model_name="all-MiniLM-L6-v2", cores=1, gpu=False, batch_size=128):
+    """
+    Embed the sentences/text using the MiniLM language model (which uses mean pooling)
+    """
+    print("Embedding data")
+    model = SentenceTransformer(model_name)
+    print("Model loaded")
+
+    sentences = data[key].tolist()
+    unique_sentences = data[key].unique()
+    print("Unique sentences", len(unique_sentences))
+
+    if cores == 1:
+        embeddings = model.encode(unique_sentences, show_progress_bar=True, batch_size=batch_size)
+    else:
+        devices = ["cpu"] * cores
+        if gpu:
+            devices = None  # use all CUDA devices
+
+        # Start the multi-process pool on multiple devices
+        print("Multi-process pool starting")
+        pool = model.start_multi_process_pool(devices)
+        print("Multi-process pool started")
+
+        chunk_size = math.ceil(len(unique_sentences) / cores)
+
+        # Compute the embeddings using the multi-process pool
+        embeddings = model.encode_multi_process(unique_sentences, pool, batch_size=batch_size, chunk_size=chunk_size)
+        model.stop_multi_process_pool(pool)
+
+    print("Embeddings computed")
+
+    mapping = {sentence: embedding for sentence, embedding in zip(unique_sentences, embeddings)}
+    embeddings = np.array([mapping[sentence] for sentence in sentences])
+
+    return embeddings
 
 
-def euclidean_distance(a: List[float], b: List[float]):
-    """Compute euclidean distance (norm of the difference of two vectors.)"""
-    return np.linalg.norm(np.subtract(a, b))
+def cos_sim(a: Tensor, b: Tensor):
+    """
+    Computes the cosine similarity cos_sim(a[i], b[j]) for all i and j.
+    :return: Matrix with res[i][j]  = cos_sim(a[i], b[j])
+    """
+    if not isinstance(a, torch.Tensor):
+        a = torch.tensor(np.array(a))
+
+    if not isinstance(b, torch.Tensor):
+        b = torch.tensor(np.array(b))
+
+    if len(a.shape) == 1:
+        a = a.unsqueeze(0)
+
+    if len(b.shape) == 1:
+        b = b.unsqueeze(0)
+
+    a_norm = torch.nn.functional.normalize(a, p=2, dim=1)
+    b_norm = torch.nn.functional.normalize(b, p=2, dim=1)
+    return torch.mm(a_norm, b_norm.transpose(0, 1))
+
+
+def compute_cos_sim_kernel(embs):
+    A = cos_sim(embs, embs)
+    adj_matrix = torch.zeros_like(A)
+    adj_matrix[A > THRESHOLD] = 1
+    adj_matrix[A <= THRESHOLD] = 0
+    adj_matrix = adj_matrix.numpy().astype(np.float32)
+    return adj_matrix
+
+
+def k_hop_message_passing(A, node_features, k):
+    """
+    Compute the k-hop adjacency matrix and aggregated features using message passing.
+
+    Parameters:
+    A (numpy array): The adjacency matrix of the graph.
+    node_features (numpy array): The feature matrix of the nodes.
+    k (int): The number of hops for message passing.
+
+    Returns:
+    A_k (numpy array): The k-hop adjacency matrix.
+    agg_features (numpy array): The aggregated feature matrix for each node in the k-hop neighborhood.
+    """
+
+    print("Compute the k-hop adjacency matrix")
+    A_k = np.linalg.matrix_power(A, k)
+
+    print("Aggregate the messages from the k-hop neighborhood:")
+    agg_features = node_features.copy()
+
+    for i in tqdm(range(k)):
+        agg_features += np.matmul(np.linalg.matrix_power(A, i + 1), node_features)
+
+    return A_k, agg_features
+
+
+def k_hop_message_passing_sparse(A, node_features, k):
+    """
+    Compute the k-hop adjacency matrix and aggregated features using message passing.
+
+    Parameters:
+    A (numpy array or scipy sparse matrix): The adjacency matrix of the graph.
+    node_features (numpy array or scipy sparse matrix): The feature matrix of the nodes.
+    k (int): The number of hops for message passing.
+
+    Returns:
+    A_k (numpy array): The k-hop adjacency matrix.
+    agg_features (numpy array): The aggregated feature matrix for each node in the k-hop neighborhood.
+    """
+
+    # Convert input matrices to sparse matrices if they are not already
+    if not sp.issparse(A):
+        A = sp.csr_matrix(A)
+    if not sp.issparse(node_features):
+        node_features = sp.csr_matrix(node_features)
+
+    # Compute the k-hop adjacency matrix and the aggregated features
+    A_k = A.copy()
+    agg_features = node_features.copy()
+
+    for i in tqdm(range(k)):
+        # Compute the message passing for the k-hop neighborhood
+        message = A_k.dot(node_features)
+        # Apply a GCN layer to aggregate the messages
+        agg_features = A_k.dot(agg_features) + message
+        # Update the k-hop adjacency matrix by adding new edges
+        A_k += A_k.dot(A)
+
+    return A_k.toarray(), agg_features.toarray()
