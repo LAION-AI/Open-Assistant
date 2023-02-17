7128b95b884676fc06c480b407b5ddd32c625f89
jack.butler <jack.butler@faculty.ai>
2023-02-17 21:21:40 +0000

    move locust load test inside single task

--- inference/tests/locust/locustfile.py
+++ inference/tests/locust/locustfile.py
@@ -1,4 +1,6 @@
 import json
+import random
+import time
 
 import sseclient
 from locust import HttpUser, between, task
@@ -6,21 +8,24 @@ from locust import HttpUser, between, task
 
 class ChatUser(HttpUser):
     wait_time = between(1, 2)
-
-    def on_start(self):
-        response = self.client.post("/chat", json={}).json()
-        self.chat_id = response["id"]
+    conversation_length = random.randint(3, 20)
+    time_to_respond = random.randint(3, 5)  # for the user
 
     @task
-    def send_msg(self):
-        response = self.client.post(
-            f"/chat/{self.chat_id}/message",
-            json={"message": "yo"},
-            stream=True,
-            headers={"Accept": "text/event-stream"},
-        )
-        response.raise_for_status()
+    def chat(self):
+        chat_id = self.client.post("/chat", json={}).json()["id"]
 
-        client = sseclient.SSEClient(response)
-        for event in client.events():
-            _ = json.loads(event.data)
+        for _ in range(self.conversation_length):
+            response = self.client.post(
+                f"/chat/{chat_id}/message",
+                json={"message": "yo"},
+                stream=True,
+                headers={"Accept": "text/event-stream"},
+            )
+            response.raise_for_status()
+
+            client = sseclient.SSEClient(response)
+            for event in client.events():
+                _ = json.loads(event.data)
+
+            time.sleep(self.time_to_respond)
