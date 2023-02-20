52f0a878945876bea5d975a06d24333ce07a2726
CactiStaccingCrane <121211419+CactiStaccingCrane@users.noreply.github.com>
2023-02-20 20:56:17 +0700

    Add more contrast to the radio button
    took me 30 min to load the docker and 2 min to make this change

--- website/src/components/Buttons/LikertButtons.tsx
+++ website/src/components/Buttons/LikertButtons.tsx
@@ -24,7 +24,7 @@ export const LikertButtons = ({
       style={{ display: "flex", justifyContent: "space-between" }}
     >
       {Object.keys(valueMap).map((value) => {
-        return <Radio key={value} value={value} data-cy="radio-option" size="md" padding="0.6em" />;
+        return <Radio key={value} value={value} borderColor='gray.600' data-cy="radio-option" size="md" padding="0.6em" />;
       })}
     </RadioGroup>
   );
