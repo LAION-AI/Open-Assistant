88de080636470d125b2bbd25763c36eb9a5c5143
Yannic Kilcher <yk@users.noreply.github.com>
2023-02-21 17:26:06 +0100

    tracking inference worker sessions in redis

--- inference/server/oasst_inference_server/models.py
+++ inference/server/oasst_inference_server/models.py
@@ -1,4 +1,5 @@
 import datetime
+import enum
 from uuid import uuid4
 
 import sqlalchemy as sa
@@ -56,6 +57,21 @@ class DbChat(SQLModel, table=True):
         return {m.id: m for m in self.messages}
 
 
+class WorkerEventType(str, enum.Enum):
+    connect = "connect"
+
+
+class DbWorkerEvent(SQLModel, table=True):
+    __tablename__ = "worker_event"
+
+    id: str = Field(default_factory=lambda: str(uuid4()), primary_key=True)
+    worker_id: str = Field(..., foreign_key="worker.id", index=True)
+    worker: "DbWorker" = Relationship(back_populates="events")
+    time: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
+    event_type: WorkerEventType
+    worker_config: inference.WorkerConfig | None = Field(None, sa_column=sa.Column(pg.JSONB))
+
+
 class DbWorker(SQLModel, table=True):
     __tablename__ = "worker"
 
@@ -65,3 +81,5 @@ class DbWorker(SQLModel, table=True):
 
     in_compliance_check: bool = Field(default=False, sa_column=sa.Column(sa.Boolean, server_default=sa.text("false")))
     next_compliance_check: datetime.datetime | None = Field(None)
+
+    events: list[DbWorkerEvent] = Relationship(back_populates="worker")
