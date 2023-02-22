b440c61fb20722aef87cd510c5daee61207f3afb
Lalo <121638912+JoLalo@users.noreply.github.com>
2023-02-21 16:33:52 -0800

    contributors.js
    I have made a new contribution in "contributors.js". It's more of an idea than actual code that will work.

---
+++ contributors.js
@@ -0,0 +1,34 @@
+//Create a contributors / credits page
+
+//We can list and manually put all contributors and creditors who have contributed into a credits page first. 
+
+
+
+//Then we can make a function so that whenever a person contributes (For example a successful pull request/other contributions), they are then added to the page. 
+
+
+//This can take the contribution
+const newContribution = 'pullRequest';
+
+
+//Taking the person's data, for example name, github link, etc.
+function getData() {
+    
+} 
+
+//The function for updating the page
+function updatePage() {
+    if (newContribution) {
+        updatePage();
+    }
+
+
+
+}
+
+
+
+
+
+//There can also be a notification telling the contributor they are on the creditor page
+
