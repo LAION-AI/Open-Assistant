869eb6cbf50b9ba2fb01843de133cee6749a0322
Gareth Davidson <gaz@bitplane.net>
2023-02-22 06:44:56 +0000

    Update tests, move stuff to requirements.txt

--- backend/tests/unit/utils/test_similarity_functions.py
+++ backend/tests/unit/utils/test_similarity_functions.py
@@ -1,31 +1,39 @@
-import pytest
-from oasst_backend.utils.similarity_functions import cosine_similarity, euclidean_distance
+from oasst_backend.utils.similarity_functions import cos_sim, cos_sim_torch
 
 
-def test_cosine_similarity():
+def test_cos_sim():
     """
     Test cosine similarity
     """
-    a = [1, 2, 3]
-    b = [4, 5, 6]
-    assert cosine_similarity(a, b) == 0.9746318461970762
+    a = [1.0, 2.0, 3.0]
+    b = [4.0, 5.0, 6.0]
+    assert cos_sim(a, b) == 0.9746318461970762
 
 
-def test_cosine_similarity_zero():
+def test_cos_sim_zero():
     """
     Test cosine similarity with one of the vectors being zero
     """
-    a = [1, 2, 3]
-    b = [0, 0, 0]
+    a = [1.0, 2.0, 3.0]
+    b = [0.0, 0.0, 0.0]
 
-    with pytest.raises(ZeroDivisionError):
-        cosine_similarity(a, b)
+    assert cos_sim(a, b) == 0
 
 
-def test_euclidean_distance():
+def test_cos_sim_torch():
     """
-    Test euclidean distance
+    Test cosine similarity
     """
-    a = [1, 2, 3]
-    b = [4, 5, 6]
-    assert euclidean_distance(a, b) == 5.196152422706632
+    a = [1.0, 2.0, 3.0]
+    b = [4.0, 5.0, 6.0]
+    assert cos_sim_torch(a, b) == 0.9746318461970762
+
+
+def test_cos_sim_torch_zero():
+    """
+    Test cosine similarity with one of the vectors being zero
+    """
+    a = [1.0, 2.0, 3.0]
+    b = [0.0, 0.0, 0.0]
+
+    assert cos_sim_torch(a, b) == 0
