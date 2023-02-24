047a1caa4bc3aedd5a88024ade7febdd621e0896
Anthony Cueva <hi.cuevantngmail.com>
2023-02-24 14:58:11 -0500

    displaying faq's as collapsable

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -72,14 +72,10 @@ const sidebars = {
       items: ["presentations/list"],
     },
     {
-      type: "category",
+      type: "doc",
       label: "FAQ",
-      link: {
-        type: "doc",
-        id: "faq/README",
-      },
-      items: ["faq/faq"],
-    },
+      id: "faq",
+    }
   ],
 };
 
