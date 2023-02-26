02362af304ddf312e7d013123d54f926718dacca
Oliver <olivergestanley@gmail.com>
2023-02-26 14:26:29 +0000

    Add new datasets to init

--- data/datasets/__init__.py
+++ data/datasets/__init__.py
@@ -1,6 +1,10 @@
 TEXT_DATASETS = {}
 
 INSTRUCTION_DATASETS = {
+    "humaneval_codegen_qa": "OllieStanley/humaneval-codegen-qa",
+    "humaneval_testgen_qa": "OllieStanley/humaneval-testgen-qa",
+    "mbpp_codegen_qa": "OllieStanley/mbpp-codegen-qa",
+    "mbpp_testgen_qa": "OllieStanley/mbpp-testgen-qa",
     "grade_school_math_instructions": "qwedsacf/grade-school-math-instructions",
     "recipes": "dctanner/oa_recipes",
 }
