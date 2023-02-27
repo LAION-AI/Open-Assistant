b3a8c2479b12ea69d66487e2852b836083b7e4db
Richard Nagyfi <sedthh@gmail.com>
2023-02-27 20:47:39 +0100

    added Ubuntu QA dataset - added notebook that loads the ubuntu dialogue dataset then filters Q&A-like chat messages (about <1% of all data) - added README.md based on HF datacard - added requirements.txt to run notebook in collab - updated __init__.py with HF dataset

--- data/datasets/__init__.py
+++ data/datasets/__init__.py
@@ -3,4 +3,5 @@ TEXT_DATASETS = {}
 INSTRUCTION_DATASETS = {
     "grade-school-math-instructions": "qwedsacf/grade-school-math-instructions",
     "recipes": "dctanner/oa_recipes",
+    "ubuntu_dialogue_qa": "sedthh/ubuntu_dialogue_qa",
 }
