88038a8269b5ad86d91f2c0e3cd1b9005f31f620
Richard Nagyfi <sedthh@gmail.com>
2023-02-27 20:31:41 +0100

    added CMU Wiki QA parser - added notebook that loads the CMU Wiki QA dataset then cleans / filters it and saves it in Assistant's prompt format - added README.md based on HF data card - added requirements.txt for notebook collab use - updated __init__.py to include HF dataset

--- data/datasets/__init__.py
+++ data/datasets/__init__.py
@@ -3,4 +3,5 @@ TEXT_DATASETS = {}
 INSTRUCTION_DATASETS = {
     "grade-school-math-instructions": "qwedsacf/grade-school-math-instructions",
     "recipes": "dctanner/oa_recipes",
+    "cmu_wiki_qa": "sedthh/cmu_wiki_qa",
 }
