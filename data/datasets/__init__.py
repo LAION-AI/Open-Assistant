b9efe61942ac306578cd2eae260b8a0118042de7
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2023-03-01 00:22:20 +0700

    Update __init__.py

--- data/datasets/__init__.py
+++ data/datasets/__init__.py
@@ -3,5 +3,5 @@ TEXT_DATASETS = {}
 INSTRUCTION_DATASETS = {
     "grade-school-math-instructions": "qwedsacf/grade-school-math-instructions",
     "recipes": "dctanner/oa_recipes",
-    "iapp_wiki_qa_squad" : "wannaphong/iapp_wiki_qa_squad_oa",
+    "iapp_wiki_qa_squad": "wannaphong/iapp_wiki_qa_squad_oa",
 }
