68c1f495ea7631d8d0f12e59e333083b4e53ca0d
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2023-03-01 00:20:52 +0700

    Update loading_script.py

--- data/datasets/iapp_wiki_qa_squad/loading_script.py
+++ data/datasets/iapp_wiki_qa_squad/loading_script.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
 
 if __name__ == "__main__":
-    ds = load_dataset("iapp_wiki_qa_squad")
-    print(ds["train"])
+    ds = load_dataset("wannaphong/iapp_wiki_qa_squad_oa")
+    print(ds)
