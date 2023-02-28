fb3b1820e72a8242f8dd907f16548a67e23a6695
notmd <tinhmeo10@gmail.com>
2023-02-28 16:50:10 +0700

    update validation

--- website/src/pages/api/chat/vote.ts
+++ website/src/pages/api/chat/vote.ts
@@ -4,8 +4,8 @@ import { OasstInferenceClient } from "src/lib/oasst_inference_client";
 export default withoutRole("banned", async (req, res, token) => {
   const client = new OasstInferenceClient(req, res, token);
   const { chat_id, message_id, score } = req.body as { chat_id: string; message_id: string; score: number };
-  if (score < -1 || score > 1) {
-    return res.status(400).send("");
+  if (![-1, 0, 1].includes(score)) {
+    return res.status(400).send("Invalid score");
   }
 
   const data = await client.vote({ message_id, chat_id, score });
