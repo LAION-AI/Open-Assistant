a2c52778ab7a841163c2370e626bd6a55368db73
Richard Nagyfi <sedthh@gmail.com>
2023-02-28 21:57:13 +0100

    added Ubuntu QA dataset (#1906)
    - added notebook that loads the ubuntu dialogue dataset then filters
    Q&A-like chat messages
    (about <1% of all data)
    - added README.md based on HF datacard
    - added requirements.txt to run notebook in collab
    - updated __init__.py with HF dataset
    
    solves https://github.com/LAION-AI/Open-Assistant/issues/1874

--- data/datasets/__init__.py
+++ data/datasets/__init__.py
@@ -3,5 +3,6 @@ TEXT_DATASETS = {}
 INSTRUCTION_DATASETS = {
     "grade-school-math-instructions": "qwedsacf/grade-school-math-instructions",
     "recipes": "dctanner/oa_recipes",
+    "ubuntu_dialogue_qa": "sedthh/ubuntu_dialogue_qa",
     "cmu_wiki_qa": "sedthh/cmu_wiki_qa",
 }
