9a1c4a88f61e1833e3eec09a9b5c93f287ecf39e
Richard Nagyfi <sedthh@gmail.com>
2023-03-01 21:57:57 +0100

    Gutenberg fix (#1914)
    - updated dataset to match the new schema for both english and
    multilangual Project Gutenberg eBooks
    - added link to HF text datasets to __init__.py
    - moved Gutenberg Crawler from notebooks to datasets folder, removed
    loader script
    - updated README.md and requirements.txt
    - note that the jupyter notebook with the crawler remained unchanged and
    produces the original columns
    
    this solves https://github.com/LAION-AI/Open-Assistant/issues/1899

--- data/datasets/__init__.py
+++ data/datasets/__init__.py
@@ -1,4 +1,7 @@
-TEXT_DATASETS = {}
+TEXT_DATASETS = {
+    "gutenberg_english": "sedthh/gutenberg_english",
+    "gutenberg_multilang": "sedthh/gutenberg_multilang",
+}
 
 INSTRUCTION_DATASETS = {
     "grade-school-math-instructions": "qwedsacf/grade-school-math-instructions",
