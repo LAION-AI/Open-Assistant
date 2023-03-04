6cd8336ac8def6d70e7e6db6ac7f59a6adbb528b
theblackcat102 <theblackcat102@github.com>
2023-03-04 03:47:13 +0000

    [feature] change to new conversation format

--- model/model_training/custom_datasets/formatting.py
+++ model/model_training/custom_datasets/formatting.py
@@ -1,13 +1,35 @@
-QA_SPECIAL_TOKENS = {"Question": "<human>", "Answer": "<bot>", "StartPrefix": "<prefix>", "EndPrefix": "</prefix>"}
+from enum import Enum
+
+QA_SPECIAL_TOKENS = {
+    "Question": "<|startoftext|>",
+    "Answer": "<bot>",
+    "StartPrefix": "<prefix>",
+    "EndPrefix": "</prefix>",
+}
+
+
+class SeqToken(str, Enum):
+    begin = "<|startoftext|>"
+    end = "<|endoftext|>"
+    delimiter = "\n"
+
+
+class ChatRole(str, Enum):
+    system = "system"
+    prompter = "prompter"
+    assistant = "assistant"
 
 
 def format_pair(pairs):
-    return [
-        "{}{}{}".format(QA_SPECIAL_TOKENS["Question"], pairs[i], QA_SPECIAL_TOKENS["Answer"])
-        if i % 2 == 0
-        else pairs[i]
-        for i in range(len(pairs))
-    ]
+    conversations = []
+    for i, text in enumerate(pairs):
+        if i % 2 == 0:
+            text = f"{SeqToken.begin}{ChatRole.prompter}{SeqToken.delimiter}{text}{SeqToken.end}{SeqToken.begin}{ChatRole.assistant}{SeqToken.delimiter}"
+        else:  # label mask = 1
+            text = f"{text}{SeqToken.end}"
+        conversations.append(text)
+
+    return conversations
 
 
 def format_rl_text(pairs):
