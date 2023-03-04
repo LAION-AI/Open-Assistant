8cf86ad13614e867c0390fe8f81feb32ef2d1da7
notmd <tinhmeo10@gmail.com>
2023-03-04 21:27:08 +0700

    remove options

--- website/src/lib/prismadb.ts
+++ website/src/lib/prismadb.ts
@@ -3,11 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
