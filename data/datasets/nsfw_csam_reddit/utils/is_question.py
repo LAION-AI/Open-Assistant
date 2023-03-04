616f62ea3c18e0556cd120893be463da900a09d6
Jithin James <jjmachan@pop-os.localdomain>
2023-03-05 00:17:17 +0530

    pipeline to scrape submissions

--- data/datasets/nsfw_csam_reddit/utils/is_question.py
+++ data/datasets/nsfw_csam_reddit/utils/is_question.py
@@ -3,22 +3,16 @@ from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipe
 
 class IsQuestion:
     def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            "shahrukhx01/question-vs-statement-classifier"
-        )
-        self.model = AutoModelForSequenceClassification.from_pretrained(
-            "shahrukhx01/question-vs-statement-classifier"
-        )
-        self.classifier = pipeline(
-            "sentiment-analysis", model=self.model, tokenizer=self.tokenizer
-        )
+        self.tokenizer = AutoTokenizer.from_pretrained("shahrukhx01/question-vs-statement-classifier")
+        self.model = AutoModelForSequenceClassification.from_pretrained("shahrukhx01/question-vs-statement-classifier")
+        self.classifier = pipeline("sentiment-analysis", model=self.model, tokenizer=self.tokenizer)
         self.labels = {
             "LABEL_0": False,
             "LABEL_1": True,
         }
 
-    def __call__(self, text):
-        return self.classifier(text)
+    def __call__(self, text: str) -> bool:
+        return self.labels[self.classifier(text)[0]["label"]]
 
 
 is_question = IsQuestion()
