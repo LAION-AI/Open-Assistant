b41ad37dfdd364f6d34bf3886faff27cc050ddec
Jithin James <jjmachan@pop-os.localdomain>
2023-03-05 04:17:52 +0530

    cleaned up scrap-submissions pipeline

--- data/datasets/nsfw_csam_reddit/utils/__init__.py
+++ data/datasets/nsfw_csam_reddit/utils/__init__.py
@@ -1,3 +1,4 @@
 from .is_question import is_question
+from .reddit import init_praw_reddit, scrap_subreddit
 
-__all__ = ["is_question"]
+__all__ = ["is_question", "scrap_subreddit", "init_praw_reddit"]
