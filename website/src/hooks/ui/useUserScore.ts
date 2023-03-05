96a2889711d8d99be32b3778e420bdc04ad7c40a
AbdBarho <ka70911@gmail.com>
2023-03-05 08:47:18 +0100

    Show user score in header

---
+++ website/src/hooks/ui/useUserScore.ts
@@ -0,0 +1,15 @@
+import { useSession } from "next-auth/react";
+import { get } from "src/lib/api";
+import { LeaderboardEntity, LeaderboardTimeFrame } from "src/types/Leaderboard";
+import uswSWRImmutable from "swr/immutable";
+
+export const useUserScore = () => {
+  const { status } = useSession();
+  const isLoggedIn = status === "authenticated";
+  const { data: entries } = uswSWRImmutable<Partial<{ [time in LeaderboardTimeFrame]: LeaderboardEntity }>>(
+    isLoggedIn && "/api/user_stats",
+    get
+  );
+  const score: number | undefined = entries?.total?.leader_score;
+  return score;
+};
