515d39554103c6972af8f44a9415d334f09612e7
Jithin James <jjmachan@pop-os.localdomain>
2023-03-05 09:51:01 +0530

    put intermediate as huggingface dataset

--- data/datasets/nsfw_csam_reddit/utils/__init__.py
+++ data/datasets/nsfw_csam_reddit/utils/__init__.py
@@ -6,8 +6,8 @@ from .reddit import init_praw_reddit, scrap_subreddit
 __all__ = ["is_question", "scrap_subreddit", "init_praw_reddit"]
 
 
-def save_to_huggingface(df):
+def save_to_huggingface(df, name):
     TMP_FILE = "/tmp/save_to_huggingface_tmp.json"
     df.to_json(TMP_FILE, orient="records")
     hf_dataset = load_dataset("json", data_files=TMP_FILE)
-    hf_dataset.push_to_hub("jjmachan/NSFW-questions")
+    hf_dataset.push_to_hub(name)
