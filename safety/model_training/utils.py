977f8b8d06600912b352bdb104479050611c2bed
Shahules786 <shahules786@gmail.com>
2023-03-07 11:49:14 +0000

    run pre-commit

--- safety/model_training/utils.py
+++ safety/model_training/utils.py
@@ -1,9 +1,7 @@
-
-
-def add_special_tokens(special_tokens,tokenizer,model):
-    for key,value in special_tokens.items():
-        setattr(tokenizer,key,value)
+def add_special_tokens(special_tokens, tokenizer, model):
+    for key, value in special_tokens.items():
+        setattr(tokenizer, key, value)
         tokenizer.add_tokens([value])
-        setattr(tokenizer,key+"_id",tokenizer.encode(value)[0])
+        setattr(tokenizer, key + "_id", tokenizer.encode(value)[0])
 
     model.resize_token_embeddings(len(tokenizer))
