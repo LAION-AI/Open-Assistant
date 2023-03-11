LAION-AI/Open-Assistant

commit 73f71dedaa8683735dff060e646bc861ffa589a5

fix for invalid avatarUrl error (#1986)
fix for https://github.com/LAION-AI/Open-Assistant/issues/1985

If the avatarUrl ends up being invalid, it will use the same default
value as if the avatarUrl is missing.

I tried to fix it without useState but it caused flickering as Next.js
kept trying to fetch the invalid url

<img width="618" alt="Screenshot 2023-03-05 at 8 57 24 PM"
src="https://user-images.githubusercontent.com/43126781/223002519-51c08086-7c72-419d-812e-78c5193d3b2f.png">

website/src/components/UserAvatar.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
-
+import { useState } from "react";
 export function UserAvatar(options: { displayName: string; avatarUrl?: string }) {
   const { displayName, avatarUrl } = options;
+  const [src, setSrc] = useState(
+    avatarUrl
+      ? avatarUrl
+      : `https://api.dicebear.com/5.x/initials/png?seed=${displayName}&radius=50&backgroundType=gradientLinear`
+  );
   return (
     <>
       <Image
-        src={
-          avatarUrl
-            ? avatarUrl
-            : `https://api.dicebear.com/5.x/initials/png?seed=${displayName}&radius=50&backgroundType=gradientLinear`
+        src={src}
+        onError={() =>
+          setSrc(
+            `https://api.dicebear.com/5.x/initials/png?seed=${displayName}&radius=50&backgroundType=gradientLinear`
+          )
         }
         alt={`${displayName}'s avatar`}
         width={30}

commit 981d1c9b30af2366a2b20662cdea709f2818bd3d

Show correct user avatars on leaderboard (#2039)
Fixes a bug introduced in #1986 where going forwards or backwards in the
in the users list on the leaderboard would not update the avatar.

website/src/components/LeaderboardTable/LeaderboardTable.tsx
@@ -63,18 +63,21 @@ export const LeaderboardTable = ({
       },
       columnHelper.accessor("display_name", {
         header: t("user"),
-        cell: ({ getValue, row }) => (
-          <div className="flex flex-row items-center gap-2">
-            <UserAvatar displayName={getValue()} avatarUrl={row.original.image} />
-            {isAdminOrMod ? (
-              <Link as={NextLink} href={`/admin/manage_user/${row.original.user_id}`}>
-                {getValue()}
-              </Link>
-            ) : (
-              getValue()
-            )}
-          </div>
-        ),
+        cell: ({ getValue, row }) => {
+          const display_name = getValue();
+          return (
+            <div className="flex flex-row items-center gap-2">
+              <UserAvatar displayName={display_name} avatarUrl={row.original.image} />
+              {isAdminOrMod ? (
+                <Link as={NextLink} href={`/admin/manage_user/${row.original.user_id}`}>
+                  {display_name}
+                </Link>
+              ) : (
+                display_name
+              )}
+            </div>
+          );
+        },
       }),
       columnHelper.accessor("leader_score", {
         header: t("score"),
@@ -117,7 +120,7 @@ export const LeaderboardTable = ({
   const rowProps = useBoardRowProps<WindowLeaderboardEntity>();
 
   if (isLoading) {
-    return <CircularProgress isIndeterminate></CircularProgress>;
+    return <CircularProgress isIndeterminate />;
   }
 
   if (error) {
@@ -132,7 +135,7 @@ export const LeaderboardTable = ({
       rowProps={rowProps}
       getSubRows={jsonExpandRowModel.getSubRows}
       {...pagnationProps}
-    ></DataTable>
+    />
   );
 };
 
@@ -140,7 +143,7 @@ const SpaceRow = () => {
   const color = useColorModeValue("gray.600", "gray.400");
   return (
     <Flex justify="center">
-      <Box as={MoreHorizontal} color={color}></Box>
+      <Box as={MoreHorizontal} color={color} />
     </Flex>
   );
 };

website/src/components/UserAvatar.tsx
@@ -1,26 +1,21 @@
 import Image from "next/image";
-import { useState } from "react";
-export function UserAvatar(options: { displayName: string; avatarUrl?: string }) {
-  const { displayName, avatarUrl } = options;
-  const [src, setSrc] = useState(
-    avatarUrl
-      ? avatarUrl
-      : `https://api.dicebear.com/5.x/initials/png?seed=${displayName}&radius=50&backgroundType=gradientLinear`
-  );
+import { useEffect, useState } from "react";
+export function UserAvatar({ displayName, avatarUrl }: { displayName: string; avatarUrl?: string }) {
+  const diceBearURL = `https://api.dicebear.com/5.x/initials/png?seed=${displayName}&radius=50&backgroundType=gradientLinear`;
+
+  const [src, setSrc] = useState(avatarUrl ?? diceBearURL);
+  useEffect(() => {
+    setSrc(avatarUrl ?? diceBearURL);
+  }, [avatarUrl, diceBearURL]);
+
   return (
-    <>
-      <Image
-        src={src}
-        onError={() =>
-          setSrc(
-            `https://api.dicebear.com/5.x/initials/png?seed=${displayName}&radius=50&backgroundType=gradientLinear`
-          )
-        }
-        alt={`${displayName}'s avatar`}
-        width={30}
-        height={30}
-        className="rounded-full"
-      />
-    </>
+    <Image
+      src={src}
+      onError={() => setSrc(diceBearURL)}
+      alt={`${displayName}'s avatar`}
+      width={30}
+      height={30}
+      className="rounded-full"
+    />
   );
 }