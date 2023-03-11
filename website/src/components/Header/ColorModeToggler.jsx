55f88d70adb332faea540a3fd9a36f50a045b6aa
AbdBarho <ka70911@gmail.com>
2023-03-11 16:28:34 +0100

    Swap color toggle icons (#2042)
    Closes #2030

--- website/src/components/Header/ColorModeToggler.jsx
+++ website/src/components/Header/ColorModeToggler.jsx
@@ -1,12 +1,12 @@
 import { Button, useColorMode } from "@chakra-ui/react";
-import { Sun, Moon } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 
 const ColorModeToggler = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <Button size="md" p="0px" justifyContent="center" onClick={toggleColorMode} gap="4" variant="ghost">
-      {colorMode == "dark" ? <Sun size={"1.5em"} /> : <Moon size={"1.5em"} />}
+      {colorMode === "dark" ? <Moon size={"1.5em"} /> : <Sun size={"1.5em"} />}
     </Button>
   );
 };
