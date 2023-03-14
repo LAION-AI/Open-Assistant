2593c792009a93be264ee99685cbf24d4a6d6ab0
James Melvin <melvin@gameface.ai>
2023-03-14 18:33:07 +0530

    fix : incorporated review comments

--- scripts/backend-development/start_worker.sh
+++ scripts/backend-development/start_worker.sh
@@ -5,7 +5,8 @@ then
    kill -9 $(ps aux | grep celery | grep -v grep | awk '{print $2}' | tr '\n'  ' ') > /dev/null 2>&1
 
 fi
+cd ../../backend/oasst_backend
 celery -A celery_worker beat -l info &
 celery -A celery_worker worker -l info &
-# celery -A oasst_backend beat -l info --logfile=celery.beat.log --detach
-# celery -A oasst_backend worker -l info --logfile=celery.log --detach
+# celery -A celery_worker beat -l info --logfile=celery.beat.log --detach
+# celery -A celery_worker worker -l info --logfile=celery.log --detach
