9ccf52587e75fec1cfe83a01585314f84807cfa0
Andreas Koepf <andreas.koepf@provisio.com>
2023-03-16 17:50:10 +0100

    fix metric, add accuracy

--- model/model_training/trainer_rm.py
+++ model/model_training/trainer_rm.py
@@ -23,11 +23,12 @@ from utils import PerDatasetSampler, _strtobool, get_dataset, get_loss, get_mode
 
 def compute_metrics(eval_pred):
     scores = eval_pred.predictions
-    pos_scores, neg_scores = scores[:, 0], scores[:, 1]
+    pos_scores, neg_scores = scores[0::2, :], scores[1::2, :]
     metrics = {
         "pos_score": np.mean(pos_scores),
         "neg_score": np.mean(neg_scores),
         "score_diff": np.mean(pos_scores - neg_scores),
+        "accuracy": np.mean(pos_scores > neg_scores),
     }
     return metrics
 
@@ -84,7 +85,7 @@ class RMTrainer(Trainer):
         out_logits = torch.stack([pos_logits, neg_logits])
         # we can't pass something (not nothing) for `compute_metrics` to be called`
         # also, it has to have the same size as logits, otherwise `_pad_across_processes` hangs
-        labels = torch.ones_like(out_logits[:, 0])
+        labels = torch.zeros_like(out_logits[:, 0])
 
         return (loss, out_logits, labels)
 
