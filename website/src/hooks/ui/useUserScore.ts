b77327ddee55dd774373729d76f23f7a77694920
AbdBarho <ka70911@gmail.com>
2023-03-17 18:38:58 +0100

    Add XP progress (#2093)
    Closes #1957 
    
    
    ![image](https://user-images.githubusercontent.com/24505302/225850374-24fe1a4b-3b3d-4252-9efa-f4b7f6829997.png)
    
    The star is animated, we maybe want to consider
    [prefers-reduced-motion](https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion)
    at some point.

--- website/src/hooks/ui/useUserScore.ts
+++ website/src/hooks/ui/useUserScore.ts
@@ -3,6 +3,18 @@ import { get } from "src/lib/api";
 import { LeaderboardEntity, LeaderboardTimeFrame } from "src/types/Leaderboard";
 import uswSWRImmutable from "swr/immutable";
 
+// https://github.com/LAION-AI/Open-Assistant/issues/1957
+function* generateThresholds(baseline = 3, alpha = 1.1521, maxLevel = 100) {
+  let sum = 0;
+  yield sum;
+  for (let i = 1; i < maxLevel; i++) {
+    sum += i * alpha + baseline;
+    yield Math.round(sum);
+  }
+}
+
+const thresholds = Array.from(generateThresholds());
+
 export const useUserScore = () => {
   const { status } = useSession();
   const isLoggedIn = status === "authenticated";
@@ -14,6 +26,13 @@ export const useUserScore = () => {
       keepPreviousData: true,
     }
   );
-  const score: number | undefined = entries?.total?.leader_score;
-  return score;
+  const score = entries?.total?.leader_score ?? 0;
+  const level = entries?.total?.level ?? 0;
+
+  const currentLevelScore = thresholds[level];
+  const nextLevelScore = thresholds[level + 1] ?? Infinity;
+  const scoreUntilNextLevel = nextLevelScore - score;
+  const reachedMaxLevel = level === 100;
+
+  return { score, level, currentLevelScore, nextLevelScore, scoreUntilNextLevel, reachedMaxLevel };
 };
