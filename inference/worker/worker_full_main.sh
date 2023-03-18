913886e019f37d52e67a22aecae3cd49625e0e68
Yannic Kilcher <yk@users.noreply.github.com>
2023-03-18 15:47:01 +0100

    added OA model to allowed hashes

--- inference/worker/worker_full_main.sh
+++ inference/worker/worker_full_main.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-
-# read PARALLELISM from env or set to 1 if not set
-PARALLELISM=${PARALLELISM:-1}
-
 text-generation-launcher &
 
 export INFERENCE_SERVER_URL="http://localhost:80"
 
-# launch PARALLELISM workers
-for i in $(seq 1 $PARALLELISM); do
-    /opt/miniconda/envs/worker/bin/python /worker &
-done
+/opt/miniconda/envs/worker/bin/python /worker &
 
 wait -n
 
