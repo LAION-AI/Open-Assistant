8d7517dd7fac2863e45cc1e20a2b5906f65a212d
Shahules786 <shahules786@gmail.com>
2023-03-18 18:27:48 +0000

    run pre-commit

--- model/model_training/custom_datasets/rank_datasets.py
+++ model/model_training/custom_datasets/rank_datasets.py
@@ -1,9 +1,11 @@
-from torch.utils.data import Dataset
-from datasets import load_dataset
 from collections import defaultdict
+
 import numpy as np
+from datasets import load_dataset
+from torch.utils.data import Dataset
 
 SEED = 2020
+
 
 class SHPDataset(Dataset):
     """
@@ -11,8 +13,8 @@ class SHPDataset(Dataset):
     """
 
     name = "SHP"
-    def __init__(self, split):
 
+    def __init__(self, split):
         super().__init__()
 
         self.post_dict = defaultdict(dict)
@@ -21,30 +23,29 @@ class SHPDataset(Dataset):
         for item in dataset:
             postid = item.get("post_id")
             self.postids.append(postid)
-            score_A, score_B = [item.get(key) for key in ("score_A","score_B")]
-            answers = [item.get(key) for key in ("human_ref_A","human_ref_B")]
+            score_A, score_B = [item.get(key) for key in ("score_A", "score_B")]
+            answers = [item.get(key) for key in ("human_ref_A", "human_ref_B")]
             if score_B > score_A:
                 answers = list(reversed(answers))
 
-            self.post_dict[postid].update({"post": item.get("history"), "answers":answers})
+            self.post_dict[postid].update({"post": item.get("history"), "answers": answers})
 
     def __len__(self):
         return len(self.postids)
 
-
     def __getitem__(self, idx):
-
         postid = self.postids[idx]
-        post, answers = self.post_dict.get(postid,{}).values()
+        post, answers = self.post_dict.get(postid, {}).values()
         return post, answers
 
 
 class HellaSwagDataset(Dataset):
 
     """
-    Dataset class to use data from https://arxiv.org/pdf/1905.07830.pdf 
+    Dataset class to use data from https://arxiv.org/pdf/1905.07830.pdf
     for Reward modeling
     """
+
     name = "hellaswag"
 
     def __init__(self, split) -> None:
@@ -58,14 +59,11 @@ class HellaSwagDataset(Dataset):
             endings = item.get("endings")
             selected = endings.pop(item.get("label"))
             ordered_ends = [selected, np.random.choice(endings)]
-            self.dataset_list.append({"context":context, "completions":ordered_ends})
+            self.dataset_list.append({"context": context, "completions": ordered_ends})
 
     def __len__(self):
         return len(self.dataset_list)
 
     def __getitem__(self, idx):
-
         context, completions = self.dataset_list[idx].values()
         return context, completions
-
-
