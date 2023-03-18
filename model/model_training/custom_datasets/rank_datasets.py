fdda2591b53f1d17677008d8dae6394601b25b0f
Shahules786 <shahules786@gmail.com>
2023-03-18 17:43:57 +0000

    add SHP dataset

---
+++ model/model_training/custom_datasets/rank_datasets.py
@@ -0,0 +1,37 @@
+from torch.utils.data import Dataset
+from datasets import load_dataset
+from collections import defaultdict
+
+
+class SHPDataset(Dataset):
+    """
+    Dataset class to load stanfordnlp/SHP for Reward Modeling
+    """
+
+    name = "SHP"
+    def __init__(self, split):
+
+        super().__init__()
+
+        self.post_dict = defaultdict(dict)
+        self.postids = []
+        dataset = load_dataset("stanfordnlp/SHP", split=split)
+        for item in dataset:
+            postid = item.get("post_id")
+            self.postids.append(postid)
+            score_A, score_B = [item.get(key) for key in ("score_A","score_B")]
+            answers = [item.get(key) for key in ("human_ref_A","human_ref_B")]
+            if score_B > score_A:
+                answers = list(reversed(answers))
+
+            self.post_dict[postid].update({"post": item.get("history"), "answers":answers})
+
+    def __len__(self):
+        return len(self.postids)
+
+
+    def __getitem__(self, idx):
+
+        postid = self.postids[idx]
+
+        return self.post_dict.get(postid,{}).values()
