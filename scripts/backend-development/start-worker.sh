4474123f9fef0f4f25c83cc3d8dad292dee82e14
Andreas Köpf <andreas.koepf@provisio.com>
2023-03-20 21:59:00 +0100

    Simplify celery worker use (#2131)

---
+++ scripts/backend-development/start-worker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+# switch to backend directory
+pushd "$parent_path/../../backend"
+
+celery -A oasst_backend.celery_worker worker -l INFO -B
+
+popd
