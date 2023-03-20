3e5a8e117681d52d4ce71c3d8a6098ca41f334c1
notmd <33456881+notmd@users.noreply.github.com>
2023-03-20 13:28:14 +0700

    Add Joak to team (#2134)
    He is our ES moderator, id 1032 on our discord. 
    - Only render the GitHub badge when the URL exists 
    - Fix the hydration error in the title.
    - Fallback to name when image not exists.
    
    ![image](https://user-images.githubusercontent.com/33456881/226254603-667c5653-301e-480a-8586-77f21f822a78.png)

--- website/src/pages/team.tsx
+++ website/src/pages/team.tsx
@@ -15,8 +15,8 @@ const Team = () => {
   return (
     <>
       <Head>
-        <title>{t("who_are_we")} - Open Assistant</title>
-        <meta name="description" content="The team behind Open Assistant" />
+        <title>{`${t("who_are_we")} - Open Assistant`}</title>
+        <meta name="description" content="The team begind Open Assistant" />
       </Head>
       <Box fontFamily="Inter" p="6" className="oa-basic-theme">
         <Box className="max-w-6xl mx-auto">
@@ -37,15 +37,17 @@ const Team = () => {
                       const { name, title, githubURL, imageURL } = info;
                       return (
                         <Flex key={id} gap={2}>
-                          <Avatar src={imageURL} loading="lazy" />
+                          <Avatar src={imageURL} loading="lazy" name={name} />
                           <Box ml="3">
                             <Text fontWeight="bold">
                               {name}
-                              <Badge ml="1">
-                                <Link href={githubURL} target="_default" rel="noreferrer" title="github">
-                                  <Github size={12} />
-                                </Link>
-                              </Badge>
+                              {githubURL && (
+                                <Badge ml="1">
+                                  <Link href={githubURL} target="_default" rel="noreferrer" title="github">
+                                    <Github size={12} />
+                                  </Link>
+                                </Badge>
+                              )}
                             </Text>
                             <Text fontSize="sm">{title}</Text>
                           </Box>
