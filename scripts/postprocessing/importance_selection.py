fd23807b801d3ac4750fa22e333f7af0a84515c5
Alexander Mattick <alex.mattick@fau.de>
2023-03-21 20:21:05 +0100

    data analysis suite

--- scripts/postprocessing/importance_selection.py
+++ scripts/postprocessing/importance_selection.py
@@ -4,18 +4,46 @@ import warnings
 import numpy as np
 import pandas as pd
 import psycopg2
-import shap
+from scipy.optimize import LinearConstraint, minimize
+from scipy.sparse import coo_array, csr_array, csr_matrix, hstack
 from scipy.special import softmax
-from scipy.stats import dirichlet
 from tqdm import trange
 from tqdm.contrib.logging import logging_redirect_tqdm
-from xgboost import XGBRegressor
+
+
+def least_squares_fit(features, target, scaling=1):
+    X = features  # (features - np.mean(features, 0)) / np.std(features, 0)
+    # print("feature",X.shape)
+    # get target
+    y = target.reshape(-1)
+    # Use simple imputer for mean to not change the importance of tree split
+    # Create an instance of the ExtraTreesRegressor algorithm
+    zX = X.toarray()
+    summed_target = y  # (y+zX[:,-1])/2
+    vote_matrix = csr_matrix(zX[:, :-1])
+    constraint = LinearConstraint(np.ones(X.shape[-1] - 1), 1 * scaling, 1 * scaling)
+    init = np.ones(X.shape[-1] - 1)  # lsqr(vote_matrix,summed_target)[0]
+    init = init / np.linalg.norm(init)
+    result = minimize(
+        lambda x: np.sum((vote_matrix @ x - summed_target) ** 2),
+        init,
+        jac=lambda x: 2 * vote_matrix.T @ (vote_matrix @ x - summed_target),
+        constraints=constraint,
+        hess=lambda _: 2 * vote_matrix.T @ vote_matrix,
+        method="trust-constr",
+    )
+    # result = least_squares(residual, np.ones(X.shape[-1]-1))
+    # result = least_squares(zX[:,:-1], (y+zX[:,-1])/2,
+    # print(result)
+    return np.concatenate([result.x, np.ones(1)])
 
 
 def get_df(study_label):
     conn = psycopg2.connect("host=0.0.0.0 port=5432 user=postgres password=postgres dbname=postgres")
     # Define the SQL query
-    query = "SELECT message_id, labels, message.user_id FROM text_labels JOIN message ON message_id = message.id;"
+    query = (
+        "SELECT DISTINCT message_id, labels, message.user_id FROM text_labels JOIN message ON message_id = message.id;"
+    )
 
     # Read the query results into a Pandas dataframe
     df = pd.read_sql(query, con=conn)
@@ -23,19 +51,39 @@ def get_df(study_label):
     # Close the database connection
     conn.close()
     users = set()
-    for row in df.itertuples(index=False):
-        row = row._asdict()
-        users.add(str(row["user_id"]))
-    users = list(users)
     messages = set()
     for row in df.itertuples(index=False):
         row = row._asdict()
+        users.add(str(row["user_id"]))
+        # for row in df.itertuples(index=False):
+        # row = row._asdict()
         messages.add(str(row["message_id"]))
+    users = list(users)
     messages = list(messages)
+    print("num users:", len(users), "num messages:", len(messages), "num in df", len(df))
 
-    arr = np.full((len(messages), len(users)), np.NaN, dtype=np.half)
+    # arr = np.full((len(messages), len(users)), np.NaN, dtype=np.half)
+    row_idx = []
+    col_idx = []
+    data = []
 
-    for row in df.itertuples(index=False):
+    def swap(x):
+        return (x[1], x[0])
+
+    dct = dict(map(swap, enumerate(messages)))
+    print("converting messages...")
+    df["message_id"] = df["message_id"].map(dct)
+    print("converting users...")
+    df["user_id"] = df["user_id"].map(dict(map(swap, enumerate(users))))
+    print("converting labels...")
+    df["labels"] = df["labels"].map(lambda x: float(x.get(study_label, 0)))
+    row_idx = df["message_id"].to_numpy()
+    col_idx = df["user_id"].to_numpy()
+    data = df["labels"].to_numpy()
+    print(data)
+    print(row_idx)
+    print(col_idx)
+    """ for row in df.itertuples(index=False):
         row = row._asdict()
         labels = row["labels"]
         value = labels.get(study_label, None)
@@ -45,77 +93,40 @@ def get_df(study_label):
             # tmp[users.index(row["user_id"])] = value
             # out[str(row["message_id"])] = np.array(tmp)
             # print(out[str(row["message_id"])].density)
-            mid = messages.index(str(row["message_id"]))
-            uid = users.index(str(row["user_id"]))
-            arr[mid, uid] = value
+            row_idx.append(messages.index(str(row["message_id"])))
+            col_idx.append(users.index(str(row["user_id"])))
+            data.append(value)
+            #arr[mid, uid] = value """
+    arr = csr_array(coo_array((data, (row_idx, col_idx))))
     print("results", len(users), arr.shape)
     # df = pd.DataFrame.from_dict(out,orient="index")
     print("generated dataframe")
-    return arr, messages
+    return arr, messages, users
 
 
-def compute_feature_importance(features, target, index):
-    """
-    Computes feature importance weights using the Extra Trees algorithm.
-
-    Args:
-        df (pandas.DataFrame): An DataFrame of input features.
-        target (string): Column which contains the labels.
-
-    Returns:
-        dict: A dictionary mapping feature names to importance weights.
-    """
-    # get rid of target
-    X = (features - np.nanmean(features, 0)) / np.nanstd(features, 0)
-    # print("feature",X.shape)
-    # get target
-    y = target
-    # Use simple imputer for mean to not change the importance of tree split
-    # Create an instance of the ExtraTreesRegressor algorithm
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    et = XGBRegressor(tree_method="gpu_hist", n_estimators=200, verbosity=0)
-    # et =
-    # now get wether the coefficient is negative or positive:
-    # lin = Pipeline([("scaler", StandardScaler()),("imputer", SimpleImputer(missing_values=np.nan, strategy='constant',fill_value=0)), ("lin", LinearRegression())])
-
-    # Fit the algorithm to the input data
-    et.fit(X, y)
-    # lin.fit(X, y)
-
-    # Get the feature importances from the fitted algorithm
-    # importances = np.array(list(et.named_steps["extra trees"].get_booster().get_score(importance_type="gain").values()))
-
-    explainer = shap.Explainer(et, verbosity=0)
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    shap_values = explainer.shap_values(X)
-    # Normalize based on _non NAN_ features:
-    mask = ~np.isnan(X)
-    to_combine = mask.sum(0) + 1
-    importances = np.sum(shap_values, 0) / to_combine
-
-    # print(len(importances),len(coefficient),X.columns.tolist())
-    # Create a dictionary mapping feature names to importance weights
-    # print("index",len(index))
-    # print("length importance", importances)
-    # print("length coefficient", len(coefficient))
-
-    importance_series = pd.DataFrame({"importance": importances}, index=index)
-    return importance_series
-
-
-def reweight_features(features, weights):
+def reweight_features(features, weights, noise_scale=0.0):
     # X = df.drop(target_col, axis=1)
     # print("info",features.shape,weights.shape)
-    X = (features - np.nanmean(features, 0, keepdims=True)) / np.nanstd(features, 0, keepdims=True)
-    # print(X.shape, weights.shape)
+    # X = (features - np.mean(features, 0).reshape(1,-1)) / np.std(features, 0).reshape(1,-1)
+    noise = np.random.randn(weights.shape[0]) * noise_scale
+    weights = weights + noise
     # normalizer = (X.notna().astype(float) * weights).sum(skipna=True, axis=1)
-    values = np.nansum(X * weights.reshape(1, -1), axis=1)
+    values = features @ weights
     # values = values / normalizer
     return values
 
 
 def get_subframe(arr, columns_to_filter):
-    return np.delete(arr, columns_to_filter, axis=1)
+    # return np.delete(arr, columns_to_filter, axis=1)
+    """
+    Remove the rows denoted by ``indices`` form the CSR sparse matrix ``mat``.
+    """
+    if not isinstance(arr, csr_array):
+        raise ValueError("works only for CSR format -- use .tocsr() first")
+    indices = list(columns_to_filter)
+    mask = np.ones(arr.shape[1], dtype=bool)
+    mask[indices] = False
+    return arr[:, mask]
 
 
 def sample_importance_weights(importance_weights, temperature=1.0):
@@ -135,83 +146,109 @@ def make_random_testframe(num_rows, num_cols, frac_missing):
 
 def combine_underrepresented_columns(arr, num_instances):
     # 1. get the mask
-    mask = ~np.isnan(arr)
+    mask = arr != 0
     to_combine = mask.sum(0) < num_instances
-    print("to combine", mask.sum(0))
-    print("combining", to_combine.astype(int).sum().tolist(), "many columns")
+    # print("to combine", mask.sum(0))
+    # print("combining", to_combine.astype(int).sum().tolist(), "many columns")
     if not any(to_combine):
         return arr
     # mean = np.zeros(arr.shape[0])
     # for i in to_combine.tolist():
     #    mean = np.nansum(np.stack(arr[:,i],mean),0)
     # mean = mean/len(to_combine)
-    mean = np.nanmean(arr[:, to_combine], 1, keepdims=True)
-    print(mean.shape)
+    mean = np.mean(arr[:, to_combine], 1).reshape(-1, 1)
+    # print("mean shape",mean.shape)
     dp = np.arange(len(to_combine))[to_combine]
-    print("removing unused columns")
+    # print("removing unused columns")
     arr = get_subframe(arr, dp)
-    arr = np.concatenate([arr, mean], 1)
-    print("out arr", arr.shape)
-    print(np.isnan(mean).astype(int).sum())
+    # print("subframe shape",arr.shape)
+    arr = hstack([arr, mean])
+    # print("out arr", arr.shape)
+    # print((mean==0).astype(int).sum())
     return arr
 
 
-def importance_votes(arr, alpha=100.0, to_fit=50, init_weight=None):
+def importance_votes(arr, to_fit=10, init_weight=None):
     # arr = combine_underrepresented_columns(matrix,underrepresentation_thresh)
     filtered_columns = []
     weighter = None
     if init_weight is None:
-        weighter = np.ones(arr.shape[1])  # pd.Series(1.0, index=df.drop(columns=target).columns)
+        weighter = np.ones(arr.shape[1]) / arr.shape[1]  # pd.Series(1.0, index=df.drop(columns=target).columns)
     else:
         weighter = init_weight
-    index = list(range(arr.shape[1]))
+    # print(arr.shape)
+    index = np.arange(arr.shape[1])
     # subtract 1: the last one will always have maximal reduction!
     bar = trange(to_fit)
-    for _ in bar:
+    target = np.ones(arr.shape[0])
+    for i in bar:
         index = list(filter(lambda x: x not in filtered_columns, index))
         # 0. produce target column:
         # print("step 0")
+        target_old = target
         target = reweight_features(arr, weighter)
+        error = np.mean((target - target_old) ** 2)
+        bar.set_description(f"expected error: {error}", refresh=True)
+        if error < 1e-10:
+            break
         # 1. get a subframe of interesting features
         # print("step 1")
         # subframe = get_subframe(arr, filtered_columns)
         # 2. compute feature importance
         # print("step 2")
-        importance_weights = compute_feature_importance(arr, target, index)
+        # importance_weights=None
+        # importance_weights = compute_feature_importance(arr, target, index)
+        weighter = least_squares_fit(arr, target)
         # 3. sample column
         # print("step 3")
         # new_column = sample_importance_weights(importance_weights["importance"], temperature)
         # new_column=index[new_column]
         # value = importance_weights["importance"][new_column]
         # print(weighter.shape, importance_weights["importance"].shape)
-        weighter += alpha * importance_weights["importance"].to_numpy()
+        # weighter += alpha[i] * importance_weights["importance"].to_numpy()
         # normalize to maintain the "1-voter one vote" total number of votes!
-        stepsize = np.mean(abs(importance_weights["importance"].to_numpy()))
-        bar.set_description(f"expected stepsize: {stepsize}", refresh=True)
-        weighter = weighter / sum(abs(weighter)) * len(weighter)
+        # weighter = weighter / sum(abs(weighter))
+        # stepsize = np.mean(abs(importance_weights["importance"].to_numpy()))
+        # bar.set_description(f"expected stepsize: {stepsize}", refresh=True)
         # filtered_columns.append(new_column)
     # print("new weight values", weighter)
     return reweight_features(arr, weighter), weighter
 
 
-def select_ids(arr, pick_frac, alph_range=(100, 100), folds=20, frac=0.5):
-    # votes = []
-    votes = np.zeros((folds, len(arr)))
-    alphas = np.geomspace(alph_range[0], alph_range[1], folds)
-    num_per_iter = int(len(arr) * pick_frac)
+def select_ids(arr, pick_frac, minima=(50, 500), folds=50, to_fit=200, frac=0.6):
+    """
+    selects the top-"pick_frac"% of messages from "arr" after merging all
+    users with less than "minima" votes (minima increases linearly with each iteration from min to max).
+    The method returns all messages that are within `frac` many "minima" selection
+    """
+    votes = []
+    minima = np.linspace(*minima, num=folds, dtype=int)
+    num_per_iter = int(arr.shape[0] * pick_frac)
+    writer_num = 0
+    tmp = None
     for i in trange(folds):
-        tofit = np.copy(arr)
+        tofit = combine_underrepresented_columns(arr, minima[i])
+        if tofit.shape[1] == writer_num:
+            print("already tested these writer counts, skipping and using cached value.....")
+            votes.append(tmp)
+            continue
+        writer_num = tofit.shape[1]
         # print("arr shape", arr.shape)
-        init_weight = dirichlet.rvs(np.ones(arr.shape[1]))[0]
-        out, weight = importance_votes(tofit, alpha=alphas[i], init_weight=init_weight)
-        print(i, "final weight")
-        print(weight)
+        init_weight = np.ones(tofit.shape[1]) / tofit.shape[1]
+        out, weight = importance_votes(tofit, init_weight=init_weight, to_fit=to_fit)
+        # print(i, "final weight")
+        # print(weight)
         # mask =(out>thresh)
         # out = np.arange(arr.shape[0])[mask]
         indices = np.argpartition(out, -num_per_iter)[-num_per_iter:]
-        votes[i, indices] = 1
+        tmp = np.zeros((arr.shape[0]))
+        tmp[indices] = 1
+        votes.append(tmp)
         # votes.append(indices.tolist())
+    # print(*[f"user_id: {users[idx]} {m}±{s}" for m, s, idx in zip(weights_mean, weights_std, range(len(weights_mean)))], sep="\n")
     out = []
+    votes = np.stack(votes, axis=0)
+    print("votespace", votes.shape)
     votes = np.mean(votes, 0)
     for idx, f in enumerate(votes):
         if f > frac:
@@ -229,9 +266,13 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.ERROR)
     # Generate some example data
     # df = make_random_testframe(100_000,5000,0.99)
-    df, message_ids = get_df("spam")
+    df, message_ids, users = get_df("quality")
     print("combining columns:")
-    df = combine_underrepresented_columns(df, 250)
+    # df = combine_underrepresented_columns(df, 100)
+    weights = np.ones(df.shape[-1])
+    y = reweight_features(df, weights)
+    num_per_iter = int(df.shape[0] * 0.5)
+    naive = np.argpartition(y, -num_per_iter)[-num_per_iter:]
 
     print("after preprocessing")
     # print(df)
@@ -255,7 +296,28 @@ if __name__ == "__main__":
     # print(weightings)
     with logging_redirect_tqdm():
         print("selected ids")
-    ids = select_ids(df, 0.1)
+        ids = select_ids(df, 0.5, folds=500)
+
+    #    print(res, frac)
+    conn = psycopg2.connect("host=0.0.0.0 port=5432 user=postgres password=postgres dbname=postgres")
+    # Define the SQL query
+    # , payload#>'{payload, text}' as text
+    query = "SELECT DISTINCT id as message_id, message_tree_id FROM message;"
+    print("selected", len(ids), "messages")
+    # Read the query results into a Pandas dataframe
+    df = pd.read_sql(query, con=conn)
+    out = []
+    fracs = []
+    in_naive = []
     for i, frac in ids:
         res = message_ids[i]
-        print(res, frac)
+        out.append((df.loc[df["message_id"] == res]))
+        fracs.append(frac)
+        in_naive.append(i in naive)
+    df = pd.concat(out)
+    df["fracs"] = fracs
+    df["in_naive"] = in_naive
+    print(df.shape)
+    print("differences from naive", len(in_naive) - sum(in_naive))
+    print(df)
+    df.to_csv("output.csv")
--- scripts/postprocessing/rankings.py
+++ scripts/postprocessing/rankings.py
@@ -96,13 +96,15 @@ def ranked_pairs(ranks: List[List[int]]):
     """
     tallies, names = head_to_head_votes(ranks)
     tallies = tallies - tallies.T
-    # print(tallies)
     # note: the resulting tally matrix should be skew-symmetric
     # order by strength of victory (using tideman's original method, don't think it would make a difference for us)
     sorted_majorities = []
     for i in range(len(ranks[0])):
         for j in range(len(ranks[0])):
-            if tallies[i, j] > 0:
+            # you can never prefer yourself over yourself
+            # we also have to pick one of the two choices,
+            # if the preference is exactly zero...
+            if tallies[i, j] >= 0 and i != j:
                 sorted_majorities.append((i, j, tallies[i, j]))
     # we don't explicitly deal with tied majorities here
     sorted_majorities = np.array(sorted(sorted_majorities, key=lambda x: x[2], reverse=True))
@@ -128,13 +130,36 @@ def ranked_pairs(ranks: List[List[int]]):
 
 
 if __name__ == "__main__":
-    ranks = (
+
+    ranks = """ (
         [("w", "x", "z", "y") for _ in range(1)]
         + [("w", "y", "x", "z") for _ in range(2)]
         # + [("x","y","z","w") for _ in range(4)]
-        # + [("x", "z", "w", "y") for _ in range(5)]
-        # + [("y", "w", "x", "z") for _ in range(1)]
+        + [("x", "z", "w", "y") for _ in range(5)]
+        + [("y", "w", "x", "z") for _ in range(1)]
         # [("y","z","w","x") for _ in range(1000)]
-    )
+    )"""
+    ranks = [
+        [
+            ("c5181083-d3e9-41e7-a935-83fb9fa01488"),
+            ("dcf3d179-0f34-4c15-ae21-b8feb15e422d"),
+            ("d11705af-5575-43e5-b22e-08d155fbaa62"),
+        ],
+        [
+            ("d11705af-5575-43e5-b22e-08d155fbaa62"),
+            ("c5181083-d3e9-41e7-a935-83fb9fa01488"),
+            ("dcf3d179-0f34-4c15-ae21-b8feb15e422d"),
+        ],
+        [
+            ("dcf3d179-0f34-4c15-ae21-b8feb15e422d"),
+            ("c5181083-d3e9-41e7-a935-83fb9fa01488"),
+            ("d11705af-5575-43e5-b22e-08d155fbaa62"),
+        ],
+        [
+            ("d11705af-5575-43e5-b22e-08d155fbaa62"),
+            ("c5181083-d3e9-41e7-a935-83fb9fa01488"),
+            ("dcf3d179-0f34-4c15-ae21-b8feb15e422d"),
+        ],
+    ]
     rp = ranked_pairs(ranks)
     print(rp)
