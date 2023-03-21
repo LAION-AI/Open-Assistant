e0d7883f687caa0abd0f32f900752e581658a5b2
Theodor Peifer <teddypeifer@gmail.com>
2023-03-21 17:05:32 +0100

    Updated team page to fit visually more the the rest of the pages, added a button to the call-to-action section which links to the team page. (#2126)
    Hey, as discussed here
    https://github.com/LAION-AI/Open-Assistant/issues/1703 ...
    1. I made a few cosmetical changes to the new team page (it looks now
    similar to the ToS and Privacy-Policy page):
    
    ![image](https://user-images.githubusercontent.com/25230234/226197002-a50887bf-a1e5-41f6-9202-f325e21f067e.png)
    I'm not sure why the ``team_message`` translation doesn't work but it
    was like this on the main branch already.
    The only "issue" with this design is that you can see the boxes of the
    team members streching. But I still find it visually nicer as before.
    
    2. I also added a button next to the Discord and GitHub buttons in the
    Call-To-Action section which links to the team page so it that it's more
    accessible. Let me know what you think about that:
    
    ![image](https://user-images.githubusercontent.com/25230234/226196938-9e6ff60a-84a8-4646-a5d8-97f2efd9bc51.png)
    
    ---------
    
    Co-authored-by: Yannic Kilcher <yk@users.noreply.github.com>
    Co-authored-by: theopfr <teddypeifer@gmailc.com>
    Co-authored-by: notmd <tinhmeo10@gmail.com>

--- website/src/pages/team.tsx
+++ website/src/pages/team.tsx
@@ -1,5 +1,17 @@
 export { getDefaultStaticProps as getStaticProps } from "src/lib/default_static_props";
-import { Avatar, Badge, Box, Card, CardBody, Flex, Grid, Heading, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Badge,
+  Box,
+  Card,
+  CardBody,
+  Flex,
+  Grid,
+  Heading,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { Github } from "lucide-react";
 import Head from "next/head";
 import Link from "next/link";
@@ -10,6 +22,8 @@ import { getTransparentHeaderLayout } from "src/components/Layout";
 import data from "../data/team.json";
 
 const Team = () => {
+  const cardBackgroundColor = useColorModeValue("gray.100", "gray.800");
+  const contributorBackgroundColor = useColorModeValue("gray.200", "gray.700");
   const { groups, people } = data;
   const { t } = useTranslation();
   return (
@@ -19,30 +33,30 @@ const Team = () => {
         <meta name="description" content="The team behind Open Assistant" />
       </Head>
       <Box fontFamily="Inter" p="6" className="oa-basic-theme">
-        <Box className="max-w-6xl mx-auto">
-          <Card>
-            <CardBody display="flex" flexDirection={"column"} gap={6}>
-              <Heading as="h1" size="xl">
-                {t("who_are_we")}
-              </Heading>
-              <Text>{t("team_message")}</Text>
+        <Stack className="max-w-6xl mx-auto" spacing="6" mb="6">
+          <Heading as="h1" size="xl" color="blue.500">
+            {t("who_are_we")}
+          </Heading>
+          <Text fontWeight="bold">{t("team_message")}</Text>
+          <Card bg={cardBackgroundColor}>
+            <CardBody display="flex" flexDir="column" gap={6}>
               {groups.map((group) => (
                 <React.Fragment key={group.name}>
-                  <Text as="h2" fontWeight="bold" size="lg">
+                  <Heading as="h3" size="md">
                     {group.name}
-                  </Text>
-                  <Grid gap={6} gridTemplateColumns="repeat(auto-fit, minmax(300px, 1fr))">
+                  </Heading>
+                  <Grid gap="6" gridTemplateColumns="repeat(auto-fit, minmax(300px, 1fr))">
                     {group.members.map((id) => {
                       const info = people[id] ?? {};
                       const { name, title, githubURL, imageURL } = info;
                       return (
-                        <Flex key={id} gap={2}>
+                        <Flex key={id} gap="1" bg={contributorBackgroundColor} borderRadius="md" p="2">
                           <Avatar src={imageURL} loading="lazy" name={name} />
                           <Box ml="3">
                             <Text fontWeight="bold">
                               {name}
                               {githubURL && (
-                                <Badge ml="1">
+                                <Badge ml="2" mb="0.5">
                                   <Link href={githubURL} target="_default" rel="noreferrer" title="github">
                                     <Github size={12} />
                                   </Link>
@@ -59,7 +73,7 @@ const Team = () => {
               ))}
             </CardBody>
           </Card>
-        </Box>
+        </Stack>
       </Box>
     </>
   );
