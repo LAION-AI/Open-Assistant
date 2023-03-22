7453a97319e50e1f909b7dcf9b82c689e19ca633
Yannic Kilcher <yk@users.noreply.github.com>
2023-03-22 21:30:04 +0100

    added quantization to dockerfiles

--- inference/worker/worker_full_main.sh
+++ inference/worker/worker_full_main.sh
@@ -5,13 +5,14 @@ echo -n "$HF_TOKEN" > $HOME/.cache/huggingface/token
 
 num_shards=${NUM_SHARDS:-1}
 model_id=${MODEL_ID:-"OpenAssistant/oasst-sft-1-pythia-12b"}
+quantize=${QUANTIZE:-false}
 
 export HF_HUB_ENABLE_HF_TRANSFER=
 export HF_HOME=$HOME/.cache/huggingface
 export HUGGING_FACE_HUB_TOKEN=$HF_TOKEN
 /opt/miniconda/envs/text-generation/bin/python /worker/download_model.py
 
-text-generation-launcher --model-id $model_id --num-shard $num_shards &
+text-generation-launcher --model-id $model_id --num-shard $num_shards --quantize $quantize &
 
 export INFERENCE_SERVER_URL="http://localhost:80"
 
