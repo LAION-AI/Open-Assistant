cad15c51ab92a2f1e95c3f6e1c31676de447c27c
Yannic Kilcher <yk@users.noreply.github.com>
2023-03-24 00:11:21 +0100

    added quantized models

--- oasst-shared/oasst_shared/model_configs.py
+++ oasst-shared/oasst_shared/model_configs.py
@@ -36,6 +36,12 @@ MODEL_CONFIGS = {
         max_input_length=1024,
         max_total_length=2048,
     ),
+    "OA_SFT_Pythia_12Bq": ModelConfig(
+        model_id="OpenAssistant/oasst-sft-1-pythia-12b",
+        max_input_length=1024,
+        max_total_length=2048,
+        quantized=True,
+    ),
     "OA_SFT_Llama_7B": ModelConfig(
         model_id="OpenAssistant/oasst_sft_llama_7b_mask_1000",
         max_input_length=1024,
@@ -46,20 +52,40 @@ MODEL_CONFIGS = {
         max_input_length=1024,
         max_total_length=2048,
     ),
-    "OA_SFT_Llama_30B_1000steps": ModelConfig(
+    "OA_SFT_Llama_13Bq": ModelConfig(
+        model_id="OpenAssistant/oasst_sft_llama_13b_mask_1500",
+        max_input_length=1024,
+        max_total_length=2048,
+        quantized=True,
+    ),
+    "OA_SFT_Llama_30B": ModelConfig(
         model_id="OpenAssistant/llama_30b_oasst_latcyr_1000",
         max_input_length=1024,
         max_total_length=2048,
     ),
-    "OA_SFT_Llama_30B_400steps": ModelConfig(
+    "OA_SFT_Llama_30Bq": ModelConfig(
+        model_id="OpenAssistant/llama_30b_oasst_latcyr_1000",
+        max_input_length=1024,
+        max_total_length=2048,
+        quantized=True,
+    ),
+    "OA_SFT_Llama_30B_2": ModelConfig(
         model_id="OpenAssistant/llama_30b_oasst_latcyr_400",
         max_input_length=1024,
         max_total_length=2048,
     ),
+    "OA_SFT_Llama_30Bq_2": ModelConfig(
+        model_id="OpenAssistant/llama_30b_oasst_latcyr_400",
+        max_input_length=1024,
+        max_total_length=2048,
+        quantized=True,
+    ),
 }
 
 
-def get_some_model_config_by_model_id(model_id: str, default_if_not_found: bool = True) -> ModelConfig:
+def get_some_model_config_by_model_id(
+    model_id: str, default_if_not_found: bool = True
+) -> ModelConfig:
     for model_config in MODEL_CONFIGS.values():
         if model_config.model_id == model_id:
             return model_config
