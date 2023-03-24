6420cf935eb154c518c16df8628e3535e812bbeb
Shahules786 <shahules786@gmail.com>
2023-03-24 11:44:11 +0000

    load rm model

--- model/model_eval/sampling_score.py
+++ model/model_eval/sampling_score.py
@@ -1,12 +1,14 @@
 import argparse
-import os
 import json
-from dataset import get_dataloader
-import torch
+import os
+
+import model_training.models.reward_model  # noqa: F401 (registers reward model for AutoModel loading)
 import numpy as np
 import pandas as pd
-from model.model_training.utils import get_tokenizer
-from model.model_training.models.reward_model import RewardModel
+import torch
+from eval_datasets import get_sampling_dataloader
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
 
 def load_sampling_data(path):
     """
@@ -51,40 +53,33 @@ if __name__ == "__main__":
     parser.add_argument("--model", type=str, help="Path or url of the model file")
     parser.add_argument("--batch_size", type=int, help="device", default=4)
     parser.add_argument("--device", type=str, help="device", default="cpu")
-    parser.add_argument(
-        "--save", type=bool, help="whether to save the results", default=True
-    )
+    parser.add_argument("--save", type=bool, help="whether to save the results", default=True)
 
     args = parser.parse_args().__dict__
     if args.get("device") != "cpu":
-        device = (
-            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-        )
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     else:
         device = torch.device("cpu")
 
     data = load_sampling_data(args.get("data_path"))
 
-    reward_name = args.get("model")
-    
-    ##TODO
-    rank_model = RewardModel().from_pretrained(model)
-    tokenizer = get_tokenizer(model)
-    rank_model.eval()
-    rank_model.to(device)
-    max_len = model.max_len
+    model_name = args.get("model")
 
-    dataloader = get_dataloader(data, tokenizer, max_len, args.get("batch_size"), device)
-    sampling, scores = batch_inference(rank_model, dataloader)
-    
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    model.eval()
+    model.to(device)
+
+    dataloader = get_sampling_dataloader(data, tokenizer, args.get("max_length", 512), args.get("batch_size"), device)
+    sampling, scores = batch_inference(model, dataloader)
+
     df = pd.DataFrame({"sampling": sampling, "score": scores})
     id2label = {v: k for k, v in dataloader.dataset.label2id.items()}
     df["sampling"] = df["sampling"].map(id2label)
     results = df.groupby("sampling")["score"].mean().to_dict()
-    results["mean_reward"] = df["score"].mean().astype('str')
+    results["mean_reward"] = str(df["score"].mean())
     print("RESULTS: ", results)
 
-
-    results = {"model_name":data["model_name"], "results":results, "reward_model":args.get("model")}
-    results = {"model_name":data["model_name"], "results":results}
+    results = {"model_name": data["model_name"], "results": results, "reward_model": args.get("model")}
+    results = {"model_name": data["model_name"], "results": results}
     name = "-".join(data["model_name"].split("/"))
