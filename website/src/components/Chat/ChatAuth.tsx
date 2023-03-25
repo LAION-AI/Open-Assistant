d19f5ca58e9b9c0070a242071ab30be394dd6eeb
AbdBarho <ka70911@gmail.com>
2023-03-25 12:06:45 +0100

    Get available auth providers from inference server

--- website/src/components/Chat/ChatAuth.tsx
+++ website/src/components/Chat/ChatAuth.tsx
@@ -3,37 +3,55 @@ import { Github } from "lucide-react";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { useTranslation } from "next-i18next";
-import { useMemo } from "react";
+import { memo } from "react";
 import { Discord } from "src/components/Icons/Discord";
+import { get } from "src/lib/api";
+import useSWRImmutable from "swr/immutable";
 
-export const ChatAuth = ({ inferenceHost }: { inferenceHost: string }) => {
+const icons = {
+  github: <Github size={128} />,
+  discord: <Discord size={128} />,
+};
+
+export const ChatAuth = memo(function ChatAuth({ inferenceHost }: { inferenceHost: string }) {
   const { t } = useTranslation(["chat", "common"]);
+  const { data: authProviders } = useSWRImmutable<string[]>(`${inferenceHost}/auth/providers`, get, {
+    fallbackData: [],
+  });
+
   const { data: session } = useSession();
   const isAuth = session?.inference.isAuthenticated;
+  const username = session?.user.name;
 
-  const content = useMemo(() => {
-    if (isAuth) {
-      return (
+  if (isAuth) {
+    return (
+      <Card mt={4}>
         <CardBody display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" gap={4}>
           <Text>{t("you_are_logged_in")}</Text>
           <Button>{t("common:sign_out")}</Button>
         </CardBody>
-      );
-    }
-    return (
+      </Card>
+    );
+  }
+  return (
+    <Card mt={4}>
       <CardBody display="flex" flexDirection="column" gap={4}>
         <Text>{t("login_message")}</Text>
-        <Grid justifyItems="center" gridTemplateColumns="repeat(2, minmax(150px, 1fr))" gap={12}>
-          <Link href={`${inferenceHost}/auth/login/github`}>
-            <Github size={128} />
-          </Link>
-          <Link href={`${inferenceHost}/auth/login/discord`}>
-            <Discord size={128} />
-          </Link>
+        <Grid
+          justifyItems="center"
+          gridTemplateColumns={`repeat(${authProviders.length}, minmax(150px, 1fr))`}
+          gap={12}
+        >
+          {authProviders.map((provider) => (
+            <Link
+              key={provider}
+              href={`${inferenceHost}/auth/login/${provider}` + (provider === "debug" ? `?username=${username}` : "")}
+            >
+              {icons[provider] ?? provider}
+            </Link>
+          ))}
         </Grid>
       </CardBody>
-    );
-  }, [isAuth, t, inferenceHost]);
-
-  return <Card mt={4}>{content}</Card>;
-};
+    </Card>
+  );
+});
