fbaf98d0dc2d28fe725b0e0c97df1b00c2dc3d0a
Shahules786 <shahules786@gmail.com>
2023-03-25 12:50:40 +0000

    mv funtion to utils

--- model/model_eval/sampling_score.py
+++ model/model_eval/sampling_score.py
@@ -8,28 +8,9 @@ import pandas as pd
 import torch
 from eval_datasets import get_sampling_dataloader
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from utils import load_sampling_data
 
 
-def load_sampling_data(path):
-    """
-    Load sampling data and ensure appropriate keys are present.
-    """
-
-    if os.path.exists(path):
-        data = json.load(open(path))
-    else:
-        raise FileNotFoundError(f"Sampling data {path} not found")
-
-    if "prompts" not in data.keys():
-        raise KeyError("sampling data should contain prompts key")
-
-    keys = set(data["prompts"][0].keys())
-    required_keys = set(["prompt", "results"])
-    keys = keys.intersection(required_keys)
-    if keys != required_keys:
-        raise KeyError(f"Missing keys {required_keys - keys} ")
-
-    return data
 
 
 def batch_inference(model, dataloader):
