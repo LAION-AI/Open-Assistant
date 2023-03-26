521c92777d8d80ec1f849a6eac16e6075b9330fe
Shahules786 <shahules786@gmail.com>
2023-03-26 07:30:12 +0000

    add json file writer

--- model/model_eval/utils.py
+++ model/model_eval/utils.py
@@ -1,4 +1,8 @@
 import json
+import os
+
+import numpy as np
+
 
 def load_sampling_data(path):
     """
@@ -20,3 +24,23 @@ def load_sampling_data(path):
         raise KeyError(f"Missing keys {required_keys - keys} ")
 
     return data
+
+
+def write_to_json(filename, data):
+    if not filename.endswith(".json"):
+        filename = f"{filename}.json"
+
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
+
+
+def describe_samples(samples):
+    reward_scores = []
+    for item in samples:
+        reward_scores.extend([float(output[1]) for output in item["outputs"]])
+
+    return {
+        "mean": np.mean(reward_scores).astype(str),
+        "min": np.min(reward_scores).astype(str),
+        "max": np.max(reward_scores).astype(str),
+    }
