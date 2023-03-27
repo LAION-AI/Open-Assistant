67a0a7b4136c04bdfef4b54548da7356ec99be37
notmd <tinhmeo10@gmail.com>
2023-03-27 22:43:02 +0700

    overide on client only

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -10,6 +10,7 @@ import React, { useEffect } from "react";
 import { FlagsProvider } from "react-feature-flags";
 import { getDefaultLayout, NextPageWithLayout } from "src/components/Layout";
 import flags from "src/flags";
+import { isChatEnabled } from "src/lib/chat_enabled";
 import { SWRConfig, SWRConfiguration } from "swr";
 
 import nextI18NextConfig from "../../next-i18next.config.js";
@@ -37,8 +38,10 @@ function MyApp({ Component, pageProps: { session, ...pageProps }, env, cookie }:
   }, [direction]);
 
   // expose env vars on the client
-  for (const key in env) {
-    process.env[key] = env[key];
+  if (typeof window !== "undefined") {
+    for (const key in env) {
+      process.env[key] = env[key];
+    }
   }
 
   return (
@@ -61,7 +64,7 @@ MyApp.getInitialProps = ({ ctx: { req } }: AppContext) => {
   return {
     env: {
       INFERENCE_SERVER_HOST: process.env.INFERENCE_SERVER_HOST,
-      ENABLE_CHAT: process.env.NODE_ENV === "development" || boolean(process.env.ENABLE_CHAT === "true"),
+      ENABLE_CHAT: isChatEnabled(),
       ENABLE_EMAIL_SIGNIN: boolean(process.env.ENABLE_EMAIL_SIGNIN),
       ENABLE_EMAIL_SIGNIN_CAPTCHA: boolean(process.env.ENABLE_EMAIL_SIGNIN_CAPTCHA),
       CLOUDFLARE_CAPTCHA_SITE_KEY: boolean(process.env.CLOUDFLARE_CAPTCHA_SITE_KEY),
