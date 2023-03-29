4dc06d644180213561d0f3a40f984d880e8260d0
shahues786 <shahues786@gmail.com>
2023-03-29 05:42:31 +0000

    add accuracy score

--- model/model_training/metrics.py
+++ model/model_training/metrics.py
@@ -1,29 +1,33 @@
 import numpy as np
 from scipy import stats as st
+from collections import defaultdict
 
 RM_METRICS = ["accuracy", "kendalltau", "spearmanr"]
 
 
 def reward_accuracy(eval_pred):
+
+    metrics = defaultdict(list)
     logits = eval_pred.predictions
     labels = eval_pred.label_ids
+    for b_logits,b_labels in zip(logits,labels):
+        pos_scores, neg_scores = [], []
+        b_labels = b_labels[b_labels!=-100]
+        b_logits = b_logits[b_logits!=-100]
+        for i in np.unique(b_labels):
+            logits_batch = b_logits[b_labels == i]
+            pos_scores.append(logits_batch[0])
+            neg_scores.append(logits_batch[-1])
+        pos_scores = np.array(pos_scores).reshape(-1, 1)
+        neg_scores = np.array(neg_scores).reshape(-1, 1)
 
-    pos_scores, neg_scores = [], []
-    for i in np.unique(labels):
-        logits_batch = logits[labels == i]
-        pos_scores.append(logits_batch[0])
-        neg_scores.append(logits_batch[-1])
-    pos_scores = np.array(pos_scores).reshape(-1, 1)
-    neg_scores = np.array(neg_scores).reshape(-1, 1)
+        metrics["pos_score"].append(np.mean(pos_scores))
+        metrics["neg_score"].append(np.mean(neg_scores))
+        metrics["score_diff"].append(np.mean(pos_scores - neg_scores))
+        metrics["accuracy"].append(np.mean(pos_scores > neg_scores))
 
-    metrics = {
-        "pos_score": np.mean(pos_scores),
-        "neg_score": np.mean(neg_scores),
-        "score_diff": np.mean(pos_scores - neg_scores),
-        "accuracy": np.mean(pos_scores > neg_scores),
-    }
+    metrics = {k:np.mean(v) for k,v in metrics.items()}
     return metrics
-
 
 def kendall_tau(eval_pred):
     logits = eval_pred.predictions
