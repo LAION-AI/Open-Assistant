08353955c22a3e5a9e258f28f5160fb853e14545
Yannic Kilcher <yk@users.noreply.github.com>
2023-04-02 17:39:55 +0200

    bugfix

--- inference/worker/hf_streamer.py
+++ inference/worker/hf_streamer.py
@@ -8,19 +8,22 @@ class Printer(typing.Protocol):
         ...
 
 
+def _unpack(value):
+    if len(value.shape) > 1 and value.shape[0] > 1:
+        raise ValueError("HFStreamer only supports batch size 1")
+    elif len(value.shape) > 1:
+        value = value[0]
+    return value.tolist()
+
+
 # based on HF text streamer
 class HFStreamer(transformers.generation.streamers.BaseStreamer):
-    def __init__(self, input_ids: list[int], printer: Printer):
-        self.input_ids = input_ids[::-1]
+    def __init__(self, input_ids, printer: Printer):
+        self.input_ids = _unpack(input_ids)[::-1]
         self.printer = printer
 
     def put(self, value):
-        if len(value.shape) > 1 and value.shape[0] > 1:
-            raise ValueError("HFStreamer only supports batch size 1")
-        elif len(value.shape) > 1:
-            value = value[0]
-
-        for token_id in value.tolist():
+        for token_id in _unpack(value):
             if self.input_ids:
                 input_id = self.input_ids.pop()
                 print(f"input_id: {input_id}, token_id: {token_id}, match: {input_id == token_id}")
