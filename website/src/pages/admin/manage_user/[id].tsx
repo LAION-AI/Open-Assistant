f3989250be531f9d4130878ab63ec5fe8511b45a
Vivasvan Patel <vivasvanpatel40@gmail.com>
2023-04-04 20:04:00 +0000

    stats are shown on the admin

--- website/src/pages/admin/manage_user/[id].tsx
+++ website/src/pages/admin/manage_user/[id].tsx
@@ -14,7 +14,10 @@ import {
   FormLabel,
   Input,
   Stack,
+  Flex,
   useToast,
+  Grid,
+  Text,
 } from "@chakra-ui/react";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Head from "next/head";
@@ -32,6 +35,12 @@ import prisma from "src/lib/prismadb";
 import { getFrontendUserIdForDiscordUser } from "src/lib/users";
 import { User } from "src/types/Users";
 import useSWRMutation from "swr/mutation";
+import uswSWRImmutable from "swr/immutable";
+import { useTranslation } from "next-i18next";
+import { LeaderboardEntity, LeaderboardTimeFrame } from "src/types/Leaderboard";
+import { SurveyCard } from "src/components/Survey/SurveyCard";
+import { getTypeSafei18nKey } from "src/lib/i18n";
+import { get } from "src/lib/api";
 
 interface UserForm {
   user_id: string;
@@ -44,7 +53,16 @@ interface UserForm {
 }
 
 const ManageUser = ({ user }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const { t } = useTranslation("leaderboard");
   const toast = useToast();
+
+  const { data: entries } = uswSWRImmutable<Partial<{ [time in LeaderboardTimeFrame]: LeaderboardEntity }>>(
+    "/api/user_stats?uid=" + user.id,
+    get,
+    {
+      fallbackData: {},
+    }
+  );
 
   // Trigger to let us update the user's role.  Triggers a toast when complete.
   const { trigger } = useSWRMutation("/api/admin/update_user", post, {
@@ -130,11 +148,71 @@ const ManageUser = ({ user }: InferGetServerSidePropsType<typeof getServerSidePr
               <AdminMessageTable userId={user.user_id}></AdminMessageTable>
             </CardBody>
           </Card>
+
+          <SurveyCard>
+            <Title>{t("user_stats")}</Title>
+            {[
+              LeaderboardTimeFrame.day,
+              LeaderboardTimeFrame.week,
+              LeaderboardTimeFrame.month,
+              LeaderboardTimeFrame.total,
+            ]
+              .map((key) => ({ key, values: entries[key] }))
+              .filter(({ values }) => values)
+              .map(({ key, values }) => (
+                <Box key={key} py={4}>
+                  <Title>{t(getTypeSafei18nKey(key))}</Title>
+                  <Flex w="full" wrap="wrap" alignItems="flex-start" gap={4}>
+                    <TableColumn>
+                      <Entry title={t("score")} value={values.leader_score} />
+                      <Entry title={t("rank")} value={values.rank} />
+                      <Entry title={t("prompt")} value={values.prompts} />
+                      <Entry title={t("accepted_prompts")} value={values.accepted_prompts} />
+                    </TableColumn>
+                    <TableColumn>
+                      <Entry title={t("replies_assistant")} value={values.replies_assistant} />
+                      <Entry title={t("accepted")} value={values.accepted_replies_assistant} />
+                      <Entry title={t("replies_prompter")} value={values.replies_prompter} />
+                      <Entry title={t("accepted")} value={values.accepted_replies_prompter} />
+                    </TableColumn>
+                    <TableColumn>
+                      <Entry title={t("labels_full")} value={values.labels_full} />
+                      <Entry title={t("labels_simple")} value={values.labels_simple} />
+                      <Entry title={t("rankings")} value={values.rankings_total} />
+                      <Entry title={t("reply_ranked_1")} value={values.reply_ranked_1} />
+                    </TableColumn>
+                  </Flex>
+                </Box>
+              ))}
+          </SurveyCard>
         </Stack>
       </AdminArea>
     </>
   );
 };
+
+const TableColumn = ({ children }) => {
+  return (
+    <Grid gridTemplateColumns="1fr max-content" mx={8} w="60" gap={2}>
+      {children}
+    </Grid>
+  );
+};
+
+const Entry = ({ title, value }) => {
+  return (
+    <>
+      <span className="text-start">{title}</span>
+      <span className="text-end">{value}</span>
+    </>
+  );
+};
+
+const Title = ({ children }) => (
+  <Text as="b" display="block" fontSize="2xl" py={2}>
+    {children}
+  </Text>
+);
 
 /**
  * Fetch the user's data on the server side when rendering.
--- website/src/pages/api/user_stats.ts
+++ website/src/pages/api/user_stats.ts
@@ -3,7 +3,14 @@ import { createApiClientFromUser } from "src/lib/oasst_client_factory";
 import { getBackendUserCore } from "src/lib/users";
 
 const handler = withoutRole("banned", async (req, res, token) => {
-  const user = await getBackendUserCore(token.sub);
+  let userid: string;
+  if (req.query.uid) {
+    userid = typeof req.query.uid === "string" ? req.query.uid : req.query.uid[0];
+  } else {
+    userid = token.sub;
+  }
+
+  const user = await getBackendUserCore(userid as string);
   const oasstApiClient = createApiClientFromUser(user);
   const stats = await oasstApiClient.fetch_user_stats(user);
   res.status(200).json(stats);
