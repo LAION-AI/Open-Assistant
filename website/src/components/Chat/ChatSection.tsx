0553417db21547978624f57a31436606df826fad
AbdBarho <ka70911@gmail.com>
2023-04-07 09:42:08 +0200

    Cache chat config between chats

--- website/src/components/Chat/ChatSection.tsx
+++ website/src/components/Chat/ChatSection.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { ChatConfigForm } from "src/types/Chat";
 
@@ -5,16 +6,29 @@ import { ChatConfigSummary } from "./ChatConfigSummary";
 import { useChatContext } from "./ChatContext";
 import { ChatConversation } from "./ChatConversation";
 
+const CHAT_CONFIG_KEY = "CHAT_CONFIG";
+
 export const ChatSection = ({ chatId }: { chatId: string }) => {
   const { modelInfos } = useChatContext();
 
+  const oldConfig = localStorage.getItem(CHAT_CONFIG_KEY);
   console.assert(modelInfos.length > 0, "No model config was found");
   const form = useForm<ChatConfigForm>({
-    defaultValues: {
-      ...modelInfos[0].parameter_configs[0].sampling_parameters,
-      model_config_name: modelInfos[0].name,
-    },
+    //NOTE: we should at some point validate the config, in case models were added or deleted
+    // or certain parameters disabled
+    defaultValues: oldConfig
+      ? JSON.parse(oldConfig)
+      : {
+          ...modelInfos[0].parameter_configs[0].sampling_parameters,
+          model_config_name: modelInfos[0].name,
+        },
   });
+
+  const config = form.watch();
+
+  useEffect(() => {
+    localStorage.setItem(CHAT_CONFIG_KEY, JSON.stringify(config));
+  }, [config]);
 
   return (
     <FormProvider {...form}>
