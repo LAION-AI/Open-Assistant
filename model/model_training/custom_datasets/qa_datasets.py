7f976d680582cf7d4652563c8442f256ca404d9e
tobias.pitters <tobias.pitters@gmail.com>
2023-04-07 17:34:14 +0200

    remove redundant function

--- model/model_training/custom_datasets/qa_datasets.py
+++ model/model_training/custom_datasets/qa_datasets.py
@@ -460,19 +460,6 @@ class Vicuna(Dataset):
     name = "vicuna"
 
     @staticmethod
-    def _partition_list(l: list, chunk_size: int) -> list[list]:
-        """Partition list in sublists
-
-        Args:
-            l (list): list to partition
-            chunk_size (int): length of sublists
-
-        Returns:
-            list[list]: list of sublists with length chunk_size
-        """
-        return [l[i : i + chunk_size] for i in range(0, len(l), chunk_size)]
-
-    @staticmethod
     def process_vicuna_conversations(data: list[dict[str, None | str]], input_max_length: int) -> list[str] | None:
         dialogue = []
         role = None
