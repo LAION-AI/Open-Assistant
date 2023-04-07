491765f2f67e50c4d845a6aca4f7ddb94c935272
notmd <33456881+notmd@users.noreply.github.com>
2023-04-07 22:22:58 +0700

    Use SSR for every pages (#2334)
    ~~It's ready now. `_document` only runs once serverside, so this is the
    expected behavior we want. `getInitialProps` run on both server and
    client. On the client it populates the env wrong, it uses static env
    from `.env` instead of actual env, I'm not sure why.~~
    
    Using SSR is the only way to make it work. This PR is quite similar to
    #2343 but I expose it to a global var for flexibility (accessing the env
    outside of react scope) and more performance.

--- website/src/pages/api/chat/index.ts
+++ website/src/pages/api/chat/index.ts
@@ -1,7 +1,11 @@
 import { withoutRole } from "src/lib/auth";
+import { isChatEnable } from "src/lib/isChatEnable";
 import { createInferenceClient } from "src/lib/oasst_inference_client";
 
 const handler = withoutRole("banned", async (req, res, token) => {
+  if (!isChatEnable()) {
+    return res.status(404).end();
+  }
   const client = createInferenceClient(token);
 
   let data;
