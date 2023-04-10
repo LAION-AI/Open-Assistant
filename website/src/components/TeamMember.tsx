d4ca7ed172f0b1ba33ca6b5d8ebc176a5275aeff
AbdBarho <ka70911@gmail.com>
2023-04-10 17:21:59 +0200

    Add Links to sponsors (#2444)

--- website/src/components/TeamMember.tsx
+++ website/src/components/TeamMember.tsx
@@ -5,25 +5,32 @@ import Link from "next/link";
 export interface TeamMemberProps {
   name: string;
   imageURL: string;
+  url?: string;
   githubURL?: string;
   title: string;
 }
-export function TeamMember({ name, imageURL, githubURL, title }: TeamMemberProps) {
+export function TeamMember({ name, url, imageURL, githubURL, title }: TeamMemberProps) {
   const contributorBackgroundColor = useColorModeValue("gray.200", "gray.700");
   return (
-    <Flex gap="1" bg={contributorBackgroundColor} borderRadius="md" p="2">
+    <Flex gap="3" bg={contributorBackgroundColor} borderRadius="md" p="2">
       <Avatar src={imageURL} loading="lazy" name={name} />
-      <Box ml="3">
-        <Text fontWeight="bold">
-          {name}
-          {githubURL && (
-            <Badge ml="2" mb="0.5">
-              <Link href={githubURL} target="_default" rel="noreferrer" title="github">
-                <Github size={12} />
-              </Link>
-            </Badge>
+      <Box>
+        <Flex gap={3} fontWeight="bold">
+          {url ? (
+            <Link href={url} className="underline">
+              {name}
+            </Link>
+          ) : (
+            <Text>{name}</Text>
           )}
-        </Text>
+          {githubURL && (
+            <Link href={githubURL} target="_default" rel="noreferrer" title="github">
+              <Badge mb="1">
+                <Github size={12} />
+              </Badge>
+            </Link>
+          )}
+        </Flex>
         <Text fontSize="sm">{title}</Text>
       </Box>
     </Flex>
