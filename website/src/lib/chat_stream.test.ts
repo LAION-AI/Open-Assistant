6352b94391db55041828cf8ce795bfb7d04921c6
Korbinian Pöppel <korbip@korbip.de>
2023-04-12 19:24:50 +0200

    Add more complex test involving comma, etc.

--- website/src/lib/chat_stream.test.ts
+++ website/src/lib/chat_stream.test.ts
@@ -92,4 +92,95 @@ describe("iteratorSSE", () => {
     }
     expect(idx).toBe(outputs.length);
   });
+
+  it("iteratorSSE newline", async () => {
+    const inputs = ["dat", "a: {", '"test": "\\n"}\r\n'];
+    const outputs = [{ data: '{"test": "\\n"}' }];
+    let idx = 0;
+
+    for await (const res of iteratorSSE(streamFromIt(generatorFromList(inputs)))) {
+      expect(res).toEqual(outputs[idx]);
+      idx += 1;
+    }
+    expect(idx).toBe(outputs.length);
+  });
+
+  it("iteratorSSE comma", async () => {
+    const inputs = ["dat", "a: {", '"test": ","}\r\n'];
+    const outputs = [{ data: '{"test": ","}' }];
+    let idx = 0;
+
+    for await (const res of iteratorSSE(streamFromIt(generatorFromList(inputs)))) {
+      expect(res).toEqual(outputs[idx]);
+      idx += 1;
+    }
+    expect(idx).toBe(outputs.length);
+  });
+
+  it("iteratorSSE dot", async () => {
+    const inputs = ["dat", "a: {", '"test": "."}\r\n'];
+    const outputs = [{ data: '{"test": "."}' }];
+    let idx = 0;
+
+    for await (const res of iteratorSSE(streamFromIt(generatorFromList(inputs)))) {
+      expect(outputs.length).toBeGreaterThan(idx);
+      expect(res).toEqual(outputs[idx]);
+      idx += 1;
+    }
+    expect(idx).toBe(outputs.length);
+  });
+
+  it("iteratorSSE complete text", async () => {
+    const inputs = [
+      "dat",
+      "a: {",
+      `"token": "The"}\r\n
+data: {"token": "re"}
+data: {"token": " "}
+data: {"token": "should"}
+data: {"token": " "}
+data: {"token": "be"}
+data: {"token": " "}
+data: {"token": "no"}
+data: {"token": " "}
+data: {"token": "problem"}
+data: {"token": " "}
+data: {"token": "with"}
+data: {"token": " "}
+data: {"token": "\`"}
+data: {"token": ","}
+data: {"token": "\\"}
+data: {"token": "."}\n`,
+    ];
+    const output_tokens: Array<string> = [
+      "The",
+      "re",
+      " ",
+      "should",
+      " ",
+      "be",
+      " ",
+      "no",
+      " ",
+      "problem",
+      " ",
+      "with",
+      " ",
+      "`",
+      ",",
+      "\\",
+      ".",
+    ];
+    const outputs = [];
+    for (const output_token of output_tokens) {
+      outputs.push({ data: '{"token": "' + output_token + '"}' });
+    }
+    let idx = 0;
+    for await (const res of iteratorSSE(streamFromIt(generatorFromList(inputs)))) {
+      expect(outputs.length).toBeGreaterThan(idx);
+      expect(res).toEqual(outputs[idx]);
+      idx += 1;
+    }
+    expect(idx).toBe(outputs.length);
+  });
 });
