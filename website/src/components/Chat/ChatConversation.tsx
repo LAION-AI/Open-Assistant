b12f892cfe79b4a3f0d4b2578d12a0d6120413ce
notmd <33456881+notmd@users.noreply.github.com>
2023-04-14 16:02:47 +0700

    Implement chat threading UI (#2484)
    - Allow editing a prompt (except the root one)
    - Allow regenerating a message (only last one for now)
    
    There still a lot layout shift when regenerate message, I will try to
    address them later.
    
    
    https://user-images.githubusercontent.com/33456881/231652205-da80aaa2-c18f-4068-aec0-f6640a3781ad.mp4

--- website/src/components/Chat/ChatConversation.tsx
+++ website/src/components/Chat/ChatConversation.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Flex, useBoolean } from "@chakra-ui/react";
+import { Flex, useBoolean, useToast } from "@chakra-ui/react";
 import router from "next/router";
-import { memo, useCallback, useMemo, useRef, useState } from "react";
+import { memo, useCallback, useRef, useState } from "react";
 import { useFormContext } from "react-hook-form";
 import { useMessageVote } from "src/hooks/chat/useMessageVote";
 import { get, post } from "src/lib/api";
@@ -17,8 +17,9 @@ import {
 import { mutate } from "swr";
 
 import { useChatContext } from "./ChatContext";
+import { ChatConversationTree, LAST_ASSISTANT_MESSAGE_ID } from "./ChatConversationTree";
 import { ChatForm } from "./ChatForm";
-import { ChatMessageEntry, ChatMessageEntryProps, PendingMessageEntry } from "./ChatMessageEntry";
+import { ChatMessageEntryProps, EditPromptParams, PendingMessageEntry } from "./ChatMessageEntry";
 
 interface ChatConversationProps {
   chatId: string | null; // will be null when render on chat list page (/chat)
@@ -32,35 +33,14 @@ export const ChatConversation = memo(function ChatConversation({ chatId: chatIdP
   const [queueInfo, setQueueInfo] = useState<QueueInfo | null>(null);
   const [isSending, setIsSending] = useBoolean();
 
-  // calculate the current thread as always going down the newest child in the tree
-  const currentThread: InferenceMessage[] = useMemo(() => {
-    if (!messages.length) return [];
-    // sort dates latest first
-    const sortedMessages = messages.sort((a, b) => Date.parse(b.created_at) - Date.parse(a.created_at));
-    // find the root message without parent_id
-    const root = sortedMessages.find((m) => m.parent_id === null)!;
-    const threadMessages = [root];
-    let current: InferenceMessage | null = root;
-    while (current) {
-      const next = sortedMessages.find((m) => m.parent_id === current!.id);
-      if (next) {
-        threadMessages.push(next);
-        current = next;
-      } else {
-        current = null;
-      }
-    }
-    return threadMessages;
-  }, [messages]);
-
   const { getValues: getFormValues } = useFormContext<ChatConfigFormData>();
 
   const initiate_assistant_message = useCallback(
-    async (parent_id: string, chatId: string) => {
+    async ({ parentId, chatId }: { parentId: string; chatId: string }) => {
       const { model_config_name, ...sampling_parameters } = getFormValues();
       const assistant_arg: InferencePostAssistantMessageParams = {
         chat_id: chatId,
-        parent_id,
+        parent_id: parentId,
         model_config_name,
         sampling_parameters,
       };
@@ -98,50 +78,62 @@ export const ChatConversation = memo(function ChatConversation({ chatId: chatIdP
     },
     [getFormValues, setIsSending]
   );
+  const toast = useToast();
   const send = useCallback(async () => {
     const content = inputRef.current?.value.trim();
     if (!content) {
       return;
     }
     setIsSending.on();
+    inputRef.current!.value = "";
     let currentChatId = chatIdProps;
     if (currentChatId === null) {
       const chat: ChatItem = await post(API_ROUTES.LIST_CHAT);
-      // setChatId(chat.id);
       currentChatId = chat.id;
     }
 
-    // find last VALID assistant message
-    const parent_id =
-      currentThread
-        .slice()
-        .reverse()
-        .find((m) => m.role === "assistant" && m.state === "complete")?.id ?? null;
+    const parentId = document.getElementById(LAST_ASSISTANT_MESSAGE_ID)?.dataset.id ?? null;
+    if (parentId !== null) {
+      const parent = messages.find((m) => m.id === parentId);
+      if (!parent) {
+        // we should never reach here
+        return console.error("Parent message not found", parentId);
+      }
+      if (parent!.state !== "complete") {
+        return toast({
+          title: "You are trying reply to a message that is not complete yet.",
+        });
+      }
+      // parent is exist and completed here, so we can send the message
+    }
 
     const prompter_arg: InferencePostPrompterMessageParams = {
       chat_id: currentChatId,
       content,
-      parent_id,
+      parent_id: parentId,
     };
 
     const prompter_message: InferenceMessage = await post(API_ROUTES.CREATE_PROMPTER_MESSAGE, { arg: prompter_arg });
     mutate(API_ROUTES.LIST_CHAT); // revalidte chat list after create prompter message to make sure the message already has title
     setMessages((messages) => [...messages, prompter_message]);
 
-    await initiate_assistant_message(prompter_message.id, currentChatId);
+    await initiate_assistant_message({ parentId: prompter_message.id, chatId: currentChatId });
     router.push(ROUTES.CHAT(currentChatId));
-  }, [setIsSending, chatIdProps, currentThread, initiate_assistant_message]);
+  }, [setIsSending, chatIdProps, initiate_assistant_message, messages, toast]);
 
   const sendVote = useMessageVote();
 
+  const [retryingParentId, setReytryingParentId] = useState<string | null>(null);
+
   const handleOnRetry = useCallback(
-    (messageId: string, chatId: string) => {
+    async (params: { parentId: string; chatId: string }) => {
       setIsSending.on();
-      initiate_assistant_message(messageId, chatId);
+      setReytryingParentId(params.parentId);
+      await initiate_assistant_message(params);
+      setReytryingParentId(null);
     },
     [initiate_assistant_message, setIsSending]
   );
-
   const handleOnVote: ChatMessageEntryProps["onVote"] = useCallback(
     async ({ chatId, messageId, newScore, oldScore }) => {
       // immediately set score
@@ -165,23 +157,62 @@ export const ChatConversation = memo(function ChatConversation({ chatId: chatIdP
     [sendVote]
   );
 
-  const entries = useMemo(
-    () =>
-      currentThread.map((message) => (
-        <ChatMessageEntry
-          key={message.id}
-          message={message}
-          onVote={handleOnVote}
-          onRetry={handleOnRetry}
-          isSending={isSending}
-        ></ChatMessageEntry>
-      )),
-    [handleOnVote, currentThread, handleOnRetry, isSending]
+  const handleEditPrompt = useCallback(
+    async ({ chatId, parentId, content }: EditPromptParams) => {
+      if (!content || isSending) {
+        return;
+      }
+
+      setIsSending.on();
+      setReytryingParentId(parentId);
+      const prompter_arg: InferencePostPrompterMessageParams = {
+        chat_id: chatId,
+        content,
+        parent_id: parentId,
+      };
+
+      let prompter_message: InferenceMessage | null = null;
+      const dummyMessage: InferenceMessage = {
+        id: "__dummy__",
+        ...prompter_arg,
+        created_at: new Date().toISOString(),
+        role: "prompter",
+        state: "complete",
+        score: 0,
+        reports: [],
+      };
+
+      try {
+        // push the dummy message first to avoid layout shift
+        setMessages((messages) => [...messages, dummyMessage]);
+        prompter_message = await post(API_ROUTES.CREATE_PROMPTER_MESSAGE, { arg: prompter_arg });
+        // filter out the dummy message and push the real one
+        setMessages((messages) => [...messages.filter((m) => m.id !== "__dummy__"), prompter_message!]);
+      } catch {
+        // revert on any error
+        // TODO consider to trigger notification
+        setMessages((messages) => messages.filter((m) => m.id !== "__dummy__"));
+      }
+
+      if (prompter_message) {
+        await initiate_assistant_message({ parentId: prompter_message.id, chatId: chatId });
+      }
+
+      setReytryingParentId(null);
+    },
+    [initiate_assistant_message, isSending, setIsSending]
   );
 
   return (
     <Flex flexDir="column" gap={4}>
-      {entries}
+      <ChatConversationTree
+        messages={messages}
+        onVote={handleOnVote}
+        onRetry={handleOnRetry}
+        isSending={isSending}
+        retryingParentId={retryingParentId}
+        onEditPromtp={handleEditPrompt}
+      ></ChatConversationTree>
       {isSending && streamedResponse && <PendingMessageEntry isAssistant content={streamedResponse} />}
 
       <ChatForm ref={inputRef} isSending={isSending} onSubmit={send} queueInfo={queueInfo}></ChatForm>
