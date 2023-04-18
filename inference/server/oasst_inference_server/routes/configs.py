ff0a4b27bd25ab26fd00e199624299318d9b3c18
Oliver Stanley <olivergestanley@gmail.com>
2023-04-18 18:02:47 +0100

    Add sampling presets based on community feedback

--- inference/server/oasst_inference_server/routes/configs.py
+++ inference/server/oasst_inference_server/routes/configs.py
@@ -29,7 +29,27 @@ DEFAULT_PARAMETER_CONFIGS = [
         sampling_parameters=inference.SamplingParameters(
             top_k=50,
             top_p=0.95,
-            temperature=0.9,
+            temperature=0.75,
+            repetition_penalty=1.2,
+        ),
+    ),
+    ParameterConfig(
+        name="k50-Creative",
+        description="Top-k sampling with k=50, higher temperature",
+        sampling_parameters=inference.SamplingParameters(
+            top_k=50,
+            top_p=0.95,
+            temperature=0.85,
+            repetition_penalty=1.2,
+        ),
+    ),
+    ParameterConfig(
+        name="k50-Precise",
+        description="Top-k sampling with k=50, low temperature",
+        sampling_parameters=inference.SamplingParameters(
+            top_k=50,
+            top_p=0.95,
+            temperature=0.1,
             repetition_penalty=1.2,
         ),
     ),
