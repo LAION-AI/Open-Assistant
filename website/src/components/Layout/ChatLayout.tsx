5952e4e60cc2028a914cbfc8686da4da9e9ee103
notmd <notmd1811@gmail.com>
2023-04-19 16:18:28 +0700

    remove old code

--- website/src/components/Layout/ChatLayout.tsx
+++ website/src/components/Layout/ChatLayout.tsx
@@ -1,33 +1,13 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
 import { ChatListDesktop } from "../Chat/ChatListDesktop";
 import { ChatListMobile } from "../Chat/ChatListMobile";
-import { SlimFooter } from "../Dashboard/SlimFooter";
-import { Header, HeaderLayout } from "../Header/Header";
+import { Header } from "../Header/Header";
 import { getDashBoardLayoutSidebarItem } from "../Layout";
 import { SideMenuItem } from "../SideMenu";
-import { SideMenuLayout } from "../SideMenuLayout";
 import { ToSWrapper } from "../ToSWrapper";
 
 export const getChatLayout = (page: React.ReactElement) => (
-  <HeaderLayout preLogoSlot={<ChatListMobile />}>
-    <ToSWrapper>
-      <SideMenuLayout items={getDashBoardLayoutSidebarItem()}>
-        <Flex gap={{ md: 4, lg: 6 }}>
-          <ChatListDesktop></ChatListDesktop>
-          <Box flexGrow="1">
-            <Box>{page}</Box>
-            <Box mt="10">
-              <SlimFooter />
-            </Box>
-          </Box>
-        </Flex>
-      </SideMenuLayout>
-    </ToSWrapper>
-  </HeaderLayout>
-);
-
-export const getNewChatLayout = (page: React.ReactElement) => (
   <div className="min-h-screen h-screen max-h-screen flex flex-col overflow-hidden">
     <Header fixed={false} preLogoSlot={<ChatListMobile />}></Header>
     <ToSWrapper>
