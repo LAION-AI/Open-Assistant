e42684876219ccfac2032ad0279cb138eac1120a
tobias.pitters <tobias.pitters@gmail.com>
2023-04-19 21:59:46 +0200

    update dataset entry class and add tests

--- model/model_training/custom_datasets/formatting.py
+++ model/model_training/custom_datasets/formatting.py
@@ -1,7 +1,9 @@
-from enum import Enum
+from enum import Enum, unique
 from itertools import zip_longest
+from random import shuffle
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from pydantic.fields import ModelField
 
 QA_SPECIAL_TOKENS = {
     "Question": "<|prompter|>",
@@ -26,23 +28,237 @@ def format_system_prefix(prefix, eos_token):
     )
 
 
+@unique
+class Language(str, Enum):
+    AB = "ab"  # Abkhazian
+    AA = "aa"  # Afar
+    AF = "af"  # Afrikaans
+    AK = "ak"  # Akan
+    SQ = "sq"  # Albanian
+    AM = "am"  # Amharic
+    AR = "ar"  # Arabic
+    AN = "an"  # Aragonese
+    HY = "hy"  # Armenian
+    AS = "as"  # Assamese
+    AV = "av"  # Avaric
+    AE = "ae"  # Avestan
+    AY = "ay"  # Aymara
+    AZ = "az"  # Azerbaijani
+    BM = "bm"  # Bambara
+    BA = "ba"  # Bashkir
+    EU = "eu"  # Basque
+    BE = "be"  # Belarusian
+    BN = "bn"  # Bengali
+    BH = "bh"  # Bihari languages
+    BI = "bi"  # Bislama
+    BS = "bs"  # Bosnian
+    BR = "br"  # Breton
+    BG = "bg"  # Bulgarian
+    MY = "my"  # Burmese
+    CA = "ca"  # Catalan, Valencian
+    CH = "ch"  # Chamorro
+    CE = "ce"  # Chechen
+    NY = "ny"  # Chichewa, Chewa, Nyanja
+    ZH = "zh"  # Chinese
+    CV = "cv"  # Chuvash
+    KW = "kw"  # Cornish
+    CO = "co"  # Corsican
+    CR = "cr"  # Cree
+    HR = "hr"  # Croatian
+    CS = "cs"  # Czech
+    DA = "da"  # Danish
+    DV = "dv"  # Divehi, Dhivehi, Maldivian
+    NL = "nl"  # Dutch, Flemish
+    DZ = "dz"  # Dzongkha
+    EN = "en"  # English
+    EO = "eo"  # Esperanto
+    ET = "et"  # Estonian
+    EE = "ee"  # Ewe
+    FO = "fo"  # Faroese
+    FJ = "fj"  # Fijian
+    FI = "fi"  # Finnish
+    FR = "fr"  # French
+    FF = "ff"  # Fulah
+    GL = "gl"  # Galician
+    KA = "ka"  # Georgian
+    DE = "de"  # German
+    EL = "el"  # Greek, Modern (1453-)
+    GN = "gn"  # Guarani
+    GU = "gu"  # Gujarati
+    HT = "ht"  # Haitian, Haitian Creole
+    HA = "ha"  # Hausa
+    HE = "he"  # Hebrew
+    HZ = "hz"  # Herero
+    HI = "hi"  # Hindi
+    HO = "ho"  # Hiri Motu
+    HU = "hu"  # Hungarian
+    IA = "ia"  # Interlingua (International Auxiliary Language Association)
+    ID = "id"  # Indonesian
+    IE = "ie"  # Interlingue, Occidental
+    GA = "ga"  # Irish
+    IG = "ig"  # Igbo
+    IK = "ik"  # Inupiaq
+    IO = "io"  # Ido
+    IS = "is"  # Icelandic
+    IT = "it"  # Italian
+    IU = "iu"  # Inuktitut
+    JA = "ja"  # Japanese
+    JV = "jv"  # Javanese
+    KL = "kl"  # Kalaallisut, Greenlandic
+    KN = "kn"  # Kannada
+    KR = "kr"  # Kanuri
+    KS = "ks"  # Kashmiri
+    KK = "kk"  # Kazakh
+    KM = "km"  # Central Khmer
+    KI = "ki"  # Kikuyu, Gikuyu
+    RW = "rw"  # Kinyarwanda
+    KY = "ky"  # Kirghiz, Kyrgyz
+    KV = "kv"  # Komi
+    KG = "kg"  # Kongo
+    KO = "ko"  # Korean
+    KU = "ku"  # Kurdish
+    KJ = "kj"  # Kuanyama, Kwanyama
+    LA = "la"  # Latin
+    LB = "lb"  # Luxembourgish, Letzeburgesch
+    LG = "lg"  # Ganda
+    LI = "li"  # Limburgan, Limburger, Limburgish
+    LN = "ln"  # Lingala
+    LO = "lo"  # Lao
+    LT = "lt"  # Lithuanian
+    LU = "lu"  # Luba-Katanga
+    LV = "lv"  # Latvian
+    GV = "gv"  # Manx
+    MK = "mk"  # Macedonian
+    MG = "mg"  # Malagasy
+    MS = "ms"  # Malay
+    ML = "ml"  # Malayalam
+    MT = "mt"  # Maltese
+    MI = "mi"  # Maori
+    MR = "mr"  # Marathi
+    MH = "mh"  # Marshallese
+    MN = "mn"  # Mongolian
+    NA = "na"  # Nauru
+    NV = "nv"  # Navajo, Navaho
+    ND = "nd"  # North Ndebele
+    NE = "ne"  # Nepali
+    NG = "ng"  # Ndonga
+    NB = "nb"  # Norwegian Bokm√•l
+    NN = "nn"  # Norwegian Nynorsk
+    NO = "no"  # Norwegian
+    II = "ii"  # Sichuan Yi, Nuosu
+    NR = "nr"  # South Ndebele
+    OC = "oc"  # Occitan
+    OJ = "oj"  # Ojibwa
+    CU = "cu"  # Church¬†Slavic, Old Slavonic, Church Slavonic, Old Bulgarian, Old¬†Church¬†Slavonic
+    OM = "om"  # Oromo
+    OR = "or"  # Oriya
+    OS = "os"  # Ossetian, Ossetic
+    PA = "pa"  # Panjabi, Punjabi
+    PI = "pi"  # Pali
+    FA = "fa"  # Persian
+    PL = "pl"  # Polish
+    PS = "ps"  # Pashto, Pushto
+    PT = "pt"  # Portuguese
+    QU = "qu"  # Quechua
+    RM = "rm"  # Romansh
+    RN = "rn"  # Rundi
+    RO = "ro"  # Romanian, Moldavian, Moldovan
+    RU = "ru"  # Russian
+    SA = "sa"  # Sanskrit
+    SC = "sc"  # Sardinian
+    SD = "sd"  # Sindhi
+    SE = "se"  # Northern Sami
+    SM = "sm"  # Samoan
+    SG = "sg"  # Sango
+    SR = "sr"  # Serbian
+    GD = "gd"  # Gaelic, Scottish Gaelic
+    SN = "sn"  # Shona
+    SI = "si"  # Sinhala, Sinhalese
+    SK = "sk"  # Slovak
+    SL = "sl"  # Slovenian
+    SO = "so"  # Somali
+    ST = "st"  # Southern Sotho
+    ES = "es"  # Spanish, Castilian
+    SU = "su"  # Sundanese
+    SW = "sw"  # Swahili
+    SS = "ss"  # Swati
+    SV = "sv"  # Swedish
+    TA = "ta"  # Tamil
+    TE = "te"  # Telugu
+    TG = "tg"  # Tajik
+    TH = "th"  # Thai
+    TI = "ti"  # Tigrinya
+    BO = "bo"  # Tibetan
+    TK = "tk"  # Turkmen
+    TL = "tl"  # Tagalog
+    TN = "tn"  # Tswana
+    TO = "to"  # Tonga (Tonga Islands)
+    TR = "tr"  # Turkish
+    TS = "ts"  # Tsonga
+    TT = "tt"  # Tatar
+    TW = "tw"  # Twi
+    TY = "ty"  # Tahitian
+    UG = "ug"  # Uighur, Uyghur
+    UK = "uk"  # Ukrainian
+    UR = "ur"  # Urdu
+    UZ = "uz"  # Uzbek
+    VE = "ve"  # Venda
+    VI = "vi"  # Vietnamese
+    VO = "vo"  # Volap√ºk
+    WA = "wa"  # Walloon
+    CY = "cy"  # Welsh
+    WO = "wo"  # Wolof
+    FY = "fy"  # Western Frisian
+    XH = "xh"  # Xhosa
+    YI = "yi"  # Yiddish
+    YO = "yo"  # Yoruba
+    ZA = "za"  # Zhuang, Chuang
+    ZU = "zu"  # Zulu
+
+
 class DatasetEntry(BaseModel):
     questions: list[str]
     answers: list[str]
     context: str | None
+    lang: Language | None
+    length: int | None
+    quality: float | None
+    humor: float | None
+    creativity: float | None
 
-    def get_formatted(self, mode: Mode, eos_token: str):
-        # todo: test
-        if self.context is not None:
-            first_entry = f"{QA_SPECIAL_TOKENS['System']}{self.context}{eos_token}"
-            qa_list = [first_entry]
-        else:
-            qa_list = list()
+    @validator("length")
+    def above_zero(cls, v) -> int:
+        if v is not None and v < 0:
+            raise ValueError(f"Length cannot be lower than 0. Received {v}")
+        return v
+
+    @validator("quality", "humor", "creativity")
+    def between_0_1(cls, v, field: ModelField) -> float:
+        if v is not None and not (0 <= v <= 1):
+            raise ValueError(f"Field {field.name} must be between 0 and 1. Received {v}.")
+        return v
+
+    def system_tag(self, eos_token: str) -> str | None:
+        relevant_system_infos = [
+            (k, v) for k, v in self.__dict__.items() if k not in ["questions", "answers"] and v is not None
+        ]
+        if len(relevant_system_infos) > 0:
+            shuffle(relevant_system_infos)
+            system_tag_key_values = "\n".join([f"{k}: {v}" for k, v in relevant_system_infos])
+            system_tag = f"{QA_SPECIAL_TOKENS['System']}{system_tag_key_values}\n{eos_token}"
+            return system_tag
+
+    def get_formatted(self, mode: Mode, eos_token: str) -> str | list[str]:
+        system_tag = self.system_tag(eos_token)
         if mode == Mode.rl:
-            if self.context is not None:
-                return f"{QA_SPECIAL_TOKENS['System']}{self.context}{eos_token}{QA_SPECIAL_TOKENS['Question']}{self.questions[0]}{QA_SPECIAL_TOKENS['Answer']}"
+            if system_tag is not None:
+                return f"{system_tag}{QA_SPECIAL_TOKENS['Question']}{self.questions[0]}{QA_SPECIAL_TOKENS['Answer']}"
             else:
                 return f"{QA_SPECIAL_TOKENS['Question']}{self.questions[0]}{QA_SPECIAL_TOKENS['Answer']}"
+        if system_tag is not None:
+            qa_list = [system_tag]
+        else:
+            qa_list = list()
         for q, a in zip_longest(self.questions, self.answers):
             match (q, a):
                 case (str(), str()):
@@ -57,9 +273,6 @@ class DatasetEntry(BaseModel):
                 case (None, None):
                     break
                 case (None, str()):
-                    import pdb
-
-                    pdb.set_trace()
                     raise ValueError("Received answer without getting corresponding question. Aborting")
         if mode == Mode.sft:
             return qa_list
@@ -75,7 +288,7 @@ class DatasetEntry(BaseModel):
             self: DatasetEntry class
         """
         # todo: implement
-        pass
+        NotImplementedError("Function not implemented currently.")
 
 
 def format_pairs(
