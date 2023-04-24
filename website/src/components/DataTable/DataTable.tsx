e8751227e4c681e5dc1730185c22cd38656325a5
scorbett123 <50634068+scorbett123@users.noreply.github.com>
2023-04-24 20:14:17 +0000

    Move overflow fix to UserTable, instead of being general in DataTable.

--- website/src/components/DataTable/DataTable.tsx
+++ website/src/components/DataTable/DataTable.tsx
@@ -199,7 +199,7 @@ const DataTableRow = <T,>({ row }: { row: Row<T> }) => {
       {renderCells.map((cell) => {
         const props = cell.column.columnDef.meta?.cellProps?.(cell);
         return (
-          <Td key={cell.id} {...props} colSpan={cell.span} style={{ overflow: "hidden" }}>
+          <Td key={cell.id} {...props} colSpan={cell.span}>
             {flexRender(cell.column.columnDef.cell, cell.getContext())}
           </Td>
         );
