35ee0f53b53aa09cc0e980f580f31dd5b309045f
Yannic Kilcher <yk@users.noreply.github.com>
2023-04-25 10:11:54 +0200

    Added OpenAssistant/oasst-sft-7e3-llama-30b to model configs (#2894)

--- oasst-shared/oasst_shared/model_configs.py
+++ oasst-shared/oasst_shared/model_configs.py
@@ -113,6 +113,11 @@ MODEL_CONFIGS = {
         max_total_length=1792,  # seeing OOMs on 2048 on an A100 80GB
         quantized=True,
     ),
+    "OA_SFT_Llama_30B_7e3": ModelConfig(
+        model_id="OpenAssistant/oasst-sft-7e3-llama-30b",
+        max_input_length=1024,
+        max_total_length=1792,  # seeing OOMs on 2048 on an A100 80GB
+    ),
     "Carper_RLHF_13B_1": ModelConfig(
         model_id="CarperAI/vicuna-13b-fine-tuned-rlhf",
         max_input_length=1024,
