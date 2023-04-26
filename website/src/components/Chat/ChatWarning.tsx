ca7f54b10f243df196f8e73ce3562371a75a6abf
notmd <notmd1811@gmail.com>
2023-04-26 18:20:15 +0700

    wip

--- website/src/components/Chat/ChatWarning.tsx
+++ website/src/components/Chat/ChatWarning.tsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 export const ChatWarning = () => {
   return (
-    <Box fontSize="sm" textAlign="center" mb="2">
+    <Box fontSize="sm" textAlign="center" mb="2" className="max-w-5xl mx-auto">
       This Assistant is a demonstration version that does not have internet access. It may generate incorrect or
       misleading information. It is not suitable for important use cases or for giving advice.
     </Box>
