f8d1d8c50b271f8cd0f0c46fc7a86f5e4c223a4e
Tam Nguyen <mtosity@gmail.com>
2023-04-26 05:52:54 -0500

    feat(website): support pagination for chat list (#2860)
    Issue https://github.com/LAION-AI/Open-Assistant/issues/2584
    Relate to https://github.com/LAION-AI/Open-Assistant/pull/2773
    
    Test list before ready
    - [x] Should fetch more when scrolling down
    - [x] Update title should function normally
    - [x] Hide chat should function normally
    - [x] Create a new chat should function normally
    
    Demo
    
    
    https://user-images.githubusercontent.com/40803798/233858390-019db56d-02e2-44b6-9513-9c5213019f6b.mp4
    
    ---------
    
    Co-authored-by: Andreas Köpf <andreas.koepf@provisio.com>
    Co-authored-by: notmd <notmd1811@gmail.com>

--- website/src/components/Chat/ChatListBase.tsx
+++ website/src/components/Chat/ChatListBase.tsx
@@ -5,50 +5,66 @@ import { Plus } from "lucide-react";
 import { useTranslation } from "next-i18next";
 import { memo, useCallback } from "react";
 import SimpleBar from "simplebar-react";
-import { get } from "src/lib/api";
-import { API_ROUTES } from "src/lib/routes";
 import { GetChatsResponse } from "src/types/Chat";
-import useSWR from "swr";
 
 import { ChatListItem } from "./ChatListItem";
+import { ChatWarning } from "./ChatWarning";
 import { CreateChatButton } from "./CreateChatButton";
 import { InferencePoweredBy } from "./InferencePoweredBy";
-import { ChatWarning } from "./ChatWarning";
+import { useListChatPagination } from "./useListChatPagination";
 
 export const ChatListBase = memo(function ChatListBase({
-  chats, // TODO: can we remove this?
+  initialChats, // TODO: can we remove this?
   ...props
-}: CardProps & { chats?: GetChatsResponse }) {
-  const { data: response, mutate: mutateChatResponse } = useSWR<GetChatsResponse>(API_ROUTES.LIST_CHAT, get, {
-    fallbackData: chats,
-  });
+}: CardProps & { initialChats?: GetChatsResponse }) {
+  const { loadMoreRef, responses, mutateChatResponses } = useListChatPagination(initialChats);
+  const chats = responses?.flatMap((response) => response.chats) || [];
+
   const { t } = useTranslation(["common", "chat"]);
 
   const handleUpdateTitle = useCallback(
     ({ chatId, title }: { chatId: string; title: string }) => {
-      mutateChatResponse(
-        (chatResponse) => ({
-          ...chatResponse,
-          chats: chatResponse?.chats.map((chat) => (chat.id === chatId ? { ...chat, title } : chat)) || [],
-        }),
+      mutateChatResponses(
+        (chatResponses) => [
+          ...(chatResponses?.map((chatResponse) => ({
+            ...chatResponse,
+            chats: chatResponse.chats.map((chat) => {
+              if (chat.id === chatId) {
+                return {
+                  ...chat,
+                  title,
+                };
+              }
+              return chat;
+            }),
+          })) || []),
+        ],
         false
       );
     },
-    [mutateChatResponse]
+    [mutateChatResponses]
   );
 
   const handleHide = useCallback(
     ({ chatId }: { chatId: string }) => {
-      mutateChatResponse(
-        (chatResponse) => ({
-          ...chatResponse,
-          chats: chatResponse?.chats.filter((chat) => chat.id !== chatId) || [],
-        }),
+      mutateChatResponses(
+        (chatResponses) => [
+          ...(chatResponses?.map((chatResponse) => ({
+            ...chatResponse,
+            chats: chatResponse.chats.filter((chat) => {
+              return chat.id !== chatId;
+            }),
+          })) || []),
+        ],
         false
       );
     },
-    [mutateChatResponse]
+    [mutateChatResponses]
   );
+
+  const handleCreateChat = useCallback(() => {
+    mutateChatResponses();
+  }, [mutateChatResponses]);
 
   return (
     <Box
@@ -72,6 +88,7 @@ export const ChatListBase = memo(function ChatListBase({
         borderRadius="lg"
         mx="3"
         mb="2"
+        onUpdated={handleCreateChat}
       >
         {t("create_chat")}
       </CreateChatButton>
@@ -81,9 +98,10 @@ export const ChatListBase = memo(function ChatListBase({
           contentEl: "space-y-2 mx-3 flex flex-col overflow-y-auto",
         }}
       >
-        {response?.chats.map((chat) => (
-          <ChatListItem key={chat.id} chat={chat} onUpdateTitle={handleUpdateTitle} onHide={handleHide}></ChatListItem>
+        {chats.map((chat) => (
+          <ChatListItem key={chat.id} chat={chat} onUpdateTitle={handleUpdateTitle} onHide={handleHide} />
         ))}
+        <div ref={loadMoreRef} />
       </SimpleBar>
       <ChatWarning />
       <InferencePoweredBy />
