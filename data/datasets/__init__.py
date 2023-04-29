0c3b131cc1fbe9bee2798ae0a571672963cb9285
almostEvil___ <122345469+0x22almostEvil@users.noreply.github.com>
2023-04-30 00:52:16 +0300

    Add 0x22almostEvil/multilingual-wikihow-qa-8k (#2978)
    Dataset appears to be in QA/INSTRUCTION format.
    It contains scrapped articles from WikiHow with a bit of randomized
    questions (e.g., How to `{theme}`; Write a guide on `{theme}`; etc)
    
    Source code is also available in this repo (won't include it in OA repo
    for now, because I think there's a lot to optimize) under `source`
    directory:
    https://huggingface.co/datasets/0x22almostEvil/multilingual-wikihow-qa-8k
    
    Languages (~1K entries for each language, ~8K in total):
      - en
      - ru
      - pt
      - it
      - es
      - fr
      - de
      - nl
    
    ---
    
    Resolves https://github.com/LAION-AI/Open-Assistant/issues/2965

--- data/datasets/__init__.py
+++ data/datasets/__init__.py
@@ -24,6 +24,7 @@ INSTRUCTION_DATASETS = {
     "oa_dolly_15k": "OllieStanley/oa_dolly_15k",
     "poetry_instruction": "checkai/instruction-poems",
     "oa_stackexchange": "donfu/oa-stackexchange",
+    "multilingual_wikihow_qa_8k": "0x22almostEvil/multilingual-wikihow-qa-8k",
 }
 
 SAFETY_DATASETS = {
