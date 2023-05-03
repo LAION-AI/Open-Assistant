c7f1d18a21f98e520500c82bf5eac4f571287e72
Oliver Stanley <oliver.stanley@kainos.com>
2023-05-03 15:42:41 +0100

    Remove debugs

--- inference/worker/hf_stopping.py
+++ inference/worker/hf_stopping.py
@@ -28,9 +28,4 @@ class SequenceStoppingCriteria(StoppingCriteria):
         # TODO: optimise this. Inefficient to decode whole sequence every time
         # but can't encode stop sequences as they don't always tokenize the same
         generated_text = self.tokenizer.decode(generated_ids)
-        print("CHECKING STOP CRITERIA")
-        print("Generated text:", generated_text)
-        print("Stop texts:", str(self.stop_texts))
-        print("Matches:", str([text for text in self.stop_texts if text in generated_text]))
-        print("FINISHED CHECKING STOP CRITERIA")
         return any(text in generated_text for text in self.stop_texts)
