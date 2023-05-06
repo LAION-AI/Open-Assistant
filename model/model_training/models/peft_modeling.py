11a8971b56dd55819cd8f22146e8a3e86621f783
jordiclive <jordiclive19@imperial.ac.uk>
2023-05-06 11:17:24 +0100

    fix configs.

--- model/model_training/models/peft_modeling.py
+++ model/model_training/models/peft_modeling.py
@@ -101,19 +101,19 @@ def save_adapter_model_from_ckpt(save_config: SaveLoraConfig):
     torch.save(new_embs, Path(save_config.adapter_save_path).joinpath("extra_embeddings.pt"))
 
 
-def save_adapter_model_from_ckpt(save_config: SaveLoraConfig):
-    tokenizer = get_tokenizer(save_config)
-    save_config.model_name = "decapoda-research/llama-13b-hf"
-    model = get_model(save_config, tokenizer)
-    model = peft_model(model)
-    model.load_state_dict(torch.load(save_config.torch_ckpt_path))
-    vocab_size = tokenizer.vocab_size
-    num_special_tokens = len(tokenizer.additional_special_tokens)
-
-    new_embs = model.state_dict()["base_model.model.model.embed_tokens.weight"][
-        vocab_size : vocab_size + num_special_tokens, :
-    ].clone()
-    new_embs = new_embs.to(save_config.dtype)
-    model.save_pretrained(save_config.adapter_save_path, torch_dtype=save_config.dtype)
-    tokenizer.save_pretrained(save_config.adapter_save_path)
-    torch.save(new_embs, Path(save_config.adapter_save_path).joinpath("extra_embeddings.pt"))
+# def save_adapter_model_from_ckpt(save_config: SaveLoraConfig):
+#     tokenizer = get_tokenizer(save_config)
+#     save_config.model_name = "decapoda-research/llama-13b-hf"
+#     model = get_model(save_config, tokenizer)
+#     model = peft_model(model)
+#     model.load_state_dict(torch.load(save_config.torch_ckpt_path))
+#     vocab_size = tokenizer.vocab_size
+#     num_special_tokens = len(tokenizer.additional_special_tokens)
+#
+#     new_embs = model.state_dict()["base_model.model.model.embed_tokens.weight"][
+#         vocab_size : vocab_size + num_special_tokens, :
+#     ].clone()
+#     new_embs = new_embs.to(save_config.dtype)
+#     model.save_pretrained(save_config.adapter_save_path, torch_dtype=save_config.dtype)
+#     tokenizer.save_pretrained(save_config.adapter_save_path)
+#     torch.save(new_embs, Path(save_config.adapter_save_path).joinpath("extra_embeddings.pt"))
