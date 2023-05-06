2e7a973d07c0cc418a46da499b867cbeec562cc1
root <root@Hachi>
2023-05-05 21:22:37 -0400

    Added message next to thumbs up/down button

---
+++ website/src/components/Chat/EncourageMessage.tsx
@@ -0,0 +1,10 @@
+import { Badge } from "@chakra-ui/react";
+
+// kept brief so that it doesnt distract/interfere with user experience
+export const EncourageMessage = () => {
+  return (
+    <Badge ml="1" fontFamily="Geneva" fontWeight="normal" fontSize="0.4em" background="gray.300" color="black">
+      Thoughts? Let us know!
+    </Badge>
+  );
+};
