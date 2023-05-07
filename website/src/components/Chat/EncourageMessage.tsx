56b9788b6c182eba6c5a605e356bba1177cf2bfe
notmd <notmd1811@gmail.com>
2023-05-08 03:52:33 +0700

    wip

--- website/src/components/Chat/EncourageMessage.tsx
+++ website/src/components/Chat/EncourageMessage.tsx
@@ -1,10 +1,11 @@
 import { Badge } from "@chakra-ui/react";
-
+import { useTranslation } from "next-i18next";
 // kept brief so that it doesnt distract/interfere with user experience
 export const EncourageMessage = () => {
+  const { t } = useTranslation("chat");
   return (
-    <Badge ml="1" fontFamily="Geneva" fontWeight="normal" fontSize="0.4em" background="gray.300" color="black">
-      Thoughts? Let us know!
+    <Badge fontWeight="normal" background="gray.300" color="black">
+      {t("feedback_message")}
     </Badge>
   );
 };
