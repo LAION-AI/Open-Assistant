3219bffcf72fef4e00669d63537985325fbd2299
0x22almostEvil <0x22almostEvil>
2023-05-20 12:59:13 +0300

    Add Semantics WS dataset

--- data/datasets/__init__.py
+++ data/datasets/__init__.py
@@ -31,6 +31,7 @@ INSTRUCTION_DATASETS = {
     "tatoeba_mt_qna_oa": "0x22almostEvil/tatoeba-mt-qna-oa",
     "reasoning_bg_oa": "0x22almostEvil/reasoning_bg_oa",
     "reasoning_gsm_qna_oa": "0x22almostEvil/reasoning-gsm-qna-oa",
+    "semantics_ws_qna_oa": "0x22almostEvil/semantics-ws-qna-oa",
 }
 
 SAFETY_DATASETS = {
