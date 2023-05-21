ee4253700a56ec4be9ac718e5ff88af291659106
almostEvil___ <122345469+0x22almostEvil@users.noreply.github.com>
2023-05-21 14:57:42 +0300

    Update semantics WS dataset

--- data/datasets/semantics_ws_qna_oa/data_process.py
+++ data/datasets/semantics_ws_qna_oa/data_process.py
@@ -16,30 +16,15 @@ def qna_wrapper():
         random_num = random.randint(0, 2)
 
         # extract rows' vals
-        con_type = row["Type"]
         lang = row["Language"]
+        con_type = row["Type"]
         word1 = row["Word1"]
         word2 = row["Word2"]
 
         score_percent = row["Score"]
         # 0 - yes; 1 - 50%, 2 - no
-        if con_type == "sim":
-            instruction = random_stuff.random_dict_sim_q[lang][random_num].format(word1=word1, word2=word2)
-        else:
-            instruction = random_stuff.random_dict_rel_q[lang][random_num].format(word1=word1, word2=word2)
-        if score_percent < 3.0 and con_type == "sim":
-            response = random_stuff.random_dict_sim_a[lang][2][random_num].format(word1=word1, word2=word2)
-        elif score_percent < 3.0 and con_type == "rel":
-            response = random_stuff.random_dict_rel_a[lang][2][random_num].format(word1=word1, word2=word2)
-        elif score_percent < 9 and con_type == "sim":
-            response = random_stuff.random_dict_sim_a[lang][1][random_num].format(word1=word1, word2=word2)
-        elif score_percent < 9 and con_type == "rel":
-            response = random_stuff.random_dict_rel_a[lang][1][random_num].format(word1=word1, word2=word2)
-        elif score_percent >= 9 and con_type == "sim":
-            response = random_stuff.random_dict_sim_a[lang][0][random_num].format(word1=word1, word2=word2)
-        elif score_percent >= 9 and con_type == "rel":
-            response = random_stuff.random_dict_rel_a[lang][0][random_num].format(word1=word1, word2=word2)
-
+        instruction = random_stuff.qna_random_magic(lang, word1, word2, con_type, score_percent, random_num, True)
+        response = random_stuff.qna_random_magic(lang, word1, word2, con_type, score_percent, random_num, False)
         source = "WordSim353"
         metadata = {
             "language": lang,
