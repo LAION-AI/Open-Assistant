1fe9e8a0c9f20a04862c1176911a0f713bd8a797
Mohamad Zamini <32536264+mzamini92@users.noreply.github.com>
2023-05-27 13:13:56 -0600

    Update locustfile.py
    It seems like you have a nested loop for `client.send_message("hello", self.model_config_name)`. If this loop is intended to wait until the message is sent, you can simplify it by removing the loop and directly calling `client.send_message` without the need for a loop. If you do need to wait for a response, you can use the `wait()` method provided by Locust.
    Instead of using time.sleep, you can use the wait_time attribute provided by Locust.
    If you don't need to generate a new random number for conversation_length and time_to_respond on every iteration of the chat task, you can move these assignments outside the task and generate them once before starting the user. This way, the same values will be used throughout the user's execution.
     Instead of appending the path to the module in `sys.path`, you can directly import it using a relative import.

--- inference/tests/locust/locustfile.py
+++ inference/tests/locust/locustfile.py
@@ -1,31 +1,32 @@
 import random
 import string
-import sys
-import time
 from pathlib import Path
 
 from locust import HttpUser, between, task
 
-sys.path.append(str(Path(__file__).parent.parent.parent / "text-client"))
-import text_client_utils as utils  # noqa: E402
+from ..text-client import text_client_utils as utils
 
 
 class ChatUser(HttpUser):
     wait_time = between(1, 2)
     conversation_length = random.randint(3, 20)
     time_to_respond = random.randint(3, 5)  # for the user
-    # model_config_name = "distilgpt2"
     model_config_name = "_lorem"
+
+    def on_start(self):
+        self.client = utils.DebugClient(backend_url="", http_client=self.client)
+        username = "".join(random.choice(string.ascii_lowercase) for _ in range(20))
+        self.client.login(username)
+        self.client.create_chat()
 
     @task
     def chat(self):
-        client = utils.DebugClient(backend_url="", http_client=self.client)
-        username = "".join(random.choice(string.ascii_lowercase) for _ in range(20))
-        client.login(username)
-        client.create_chat()
-
         for _ in range(self.conversation_length):
-            for _ in client.send_message("hello", self.model_config_name):
-                pass
+            self.client.send_message("hello", self.model_config_name)
+            self.wait()
 
-            time.sleep(self.time_to_respond)
+    def wait(self):
+        self.client.wait()
+        self.wait_time()
+
+
