69a3c4723762c4c202ed756f22ab8f5bc8bf53e7
Mohamad Zamini <32536264+mzamini92@users.noreply.github.com>
2023-05-27 13:34:52 -0600

    Update interface.py
    Pydantic models automatically infer the type of fields based on default values. You can simplify the type annotations by removing the explicit type declarations where they can be inferred correctly.
    Also, I've added docstrings to the classes and methods to provide better documentation for each component.

--- inference/worker/interface.py
+++ inference/worker/interface.py
@@ -1,24 +1,36 @@
-from typing import Literal
-
+from typing import List, Optional, Union
 import pydantic
-from oasst_shared.schemas import inference
+from oasst_shared import schemas
 
 
 class GenerateStreamParameters(pydantic.BaseModel):
+    """
+    Parameters for generating a stream.
+    """
+
     max_new_tokens: int = 1024
     do_sample: bool = True
-    top_k: int | None = None
-    top_p: float | None = None
-    typical_p: float | None = None
-    temperature: float | None = None
-    repetition_penalty: float | None = None
-    seed: int | None = None
-    stop: list[str] = []
+    top_k: Optional[int] = None
+    top_p: Optional[float] = None
+    typical_p: Optional[float] = None
+    temperature: Optional[float] = None
+    repetition_penalty: Optional[float] = None
+    seed: Optional[int] = None
+    stop: List[str] = []
     details: bool = True
-    plugins: list[inference.PluginEntry] = pydantic.Field(default_factory=list[inference.PluginEntry])
+    plugins: List[schemas.inference.PluginEntry] = pydantic.Field(default_factory=list)
 
     @staticmethod
-    def from_work_parameters(params: inference.WorkParameters) -> "GenerateStreamParameters":
+    def from_work_parameters(params: schemas.inference.WorkParameters) -> "GenerateStreamParameters":
+        """
+        Create GenerateStreamParameters instance from WorkParameters instance.
+
+        Args:
+            params: WorkParameters instance.
+
+        Returns:
+            GenerateStreamParameters instance.
+        """
         return GenerateStreamParameters(
             max_new_tokens=params.sampling_parameters.max_new_tokens,
             do_sample=params.do_sample,
@@ -33,20 +45,43 @@ class GenerateStreamParameters(pydantic.BaseModel):
 
 
 class GenerateStreamRequest(pydantic.BaseModel):
+    """
+    Request to generate a stream.
+    """
+
     inputs: str
     parameters: GenerateStreamParameters
 
 
 class Token(pydantic.BaseModel):
+    """
+    Token information.
+    """
+
     text: str
-    logprob: float | None
+    logprob: Optional[float] = None
     id: int
 
     def __len__(self) -> int:
+        """
+        Return the length of the token text.
+
+        Returns:
+            Length of the token text.
+        """
         return len(self.text)
 
-    def to_token_response(self, request_id: str) -> inference.TokenResponse:
-        return inference.TokenResponse(
+    def to_token_response(self, request_id: str) -> schemas.inference.TokenResponse:
+        """
+        Convert Token instance to TokenResponse instance.
+
+        Args:
+            request_id: Request ID.
+
+        Returns:
+            TokenResponse instance.
+        """
+        return schemas.inference.TokenResponse(
             request_id=request_id,
             text=self.text,
             log_prob=self.logprob,
@@ -55,21 +90,41 @@ class Token(pydantic.BaseModel):
 
 
 class StreamDetails(pydantic.BaseModel):
+    """
+    Details of the generated stream.
+    """
+
     generated_tokens: int
-    seed: int | None
-    finish_reason: Literal["length", "eos_token", "stop_sequence"]
+    seed: Optional[int] = None
+    finish_reason: Optional[Literal["length", "eos_token", "stop_sequence"]] = None
 
 
 class GenerateStreamResponse(pydantic.BaseModel):
-    token: Token | None
-    generated_text: str | None
-    details: StreamDetails | None
-    error: str | None
+    """
+    Response from generating a stream.
+    """
+
+    token: Optional[Token] = None
+    generated_text: Optional[str] = None
+    details: Optional[StreamDetails] = None
+    error: Optional[str] = None
 
     @property
     def is_end(self) -> bool:
+        """
+        Check if the generation is complete.
+
+        Returns:
+            True if generation is complete, False otherwise.
+        """
         return self.generated_text is not None
 
     @property
     def is_error(self) -> bool:
+        """
+        Check if the response contains an error.
+
+        Returns:
+            True if the response contains an error, False otherwise.
+        """
         return self.error is not None
