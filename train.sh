a31a47ecfebb419bb433577ff7c14ed148f3bb85
jordiclive <jordiclive19@imperial.ac.uk>
2023-05-31 21:16:02 +0100

    no message

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@ cd /home/ubuntu/Open-Assistant
 export TRANSFORMERS_CACHE=/home/ubuntu/transformers_cache
 
 cd /home/ubuntu/Open-Assistant/model/model_training
-deepspeed --include=localhost:0,1 --master_port 61500 /home/ubuntu/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst-top1 falcon-7b --cache_dir /home/ubuntu/Open-Assistant/data_cache --output_dir /home/ubuntu/Open-Assistant/output_dir 2>&1 --deepspeed | tee debug_falcon.txt
+deepspeed --include=localhost:0,1 --master_port 61500 /home/ubuntu/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst-top1 falcon-7b --cache_dir /home/ubuntu/data_cache --output_dir /home/ubuntu/Open-Assistant/output_dir 2>&1 --deepspeed | tee debug_falcon.txt
