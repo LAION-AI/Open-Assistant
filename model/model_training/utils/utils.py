e40ca002229b8ef7ff6c04904f9313d9e13cafbc
theblackcat102 <theblackcat102@github.com>
2023-05-31 02:34:23 +0100

    [fix] make precommit happy

--- model/model_training/utils/utils.py
+++ model/model_training/utils/utils.py
@@ -16,10 +16,9 @@ from model_training.models import freeze_top_n_layers, get_specific_model
 from model_training.models.patching import patch_model
 from model_training.models.reward_model import GPTNeoXRewardModel
 from sklearn.model_selection import train_test_split
+from tokenizers import pre_tokenizers
 from torch.utils.data import ConcatDataset, Dataset, Subset
 from torch.utils.data.distributed import DistributedSampler
-
-from tokenizers import pre_tokenizers
 
 from .losses import CrossEntropyLoss, PolyLoss, RMCLSLoss, RMLoss
 
