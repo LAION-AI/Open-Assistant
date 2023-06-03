a7be8a347f73c7e0f54c663bfc99dbe22f5be2ed
Owen Law <owenlaw222@gmail.com>
2023-06-03 10:16:21 -0400

    Use uuid v7

--- backend/oasst_backend/models/message_revision.py
+++ backend/oasst_backend/models/message_revision.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from typing import Optional
-from uuid import UUID, uuid4
+from uuid import UUID
 
 import sqlalchemy as sa
 import sqlalchemy.dialects.postgresql as pg
 from sqlmodel import Field, SQLModel
+from uuid_extensions import uuid7
 
 from .payload_column_type import PayloadContainer, payload_column_type
 
@@ -12,11 +13,7 @@ from .payload_column_type import PayloadContainer, payload_column_type
 class MessageRevision(SQLModel, table=True):
     __tablename__ = "message_revision"
 
-    id: UUID = Field(
-        sa_column=sa.Column(
-            pg.UUID(as_uuid=True), primary_key=True, default=uuid4, server_default=sa.text("gen_random_uuid()")
-        )
-    )
+    id: UUID = Field(sa_column=sa.Column(pg.UUID(as_uuid=True), primary_key=True, default=uuid7))
 
     payload: Optional[PayloadContainer] = Field(
         sa_column=sa.Column(payload_column_type(PayloadContainer), nullable=True)
