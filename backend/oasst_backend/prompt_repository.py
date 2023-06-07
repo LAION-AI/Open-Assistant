6ad31d30fa8397f98a5cd0fd3c191a4c7d1c1e38
Owen Law <owenlaw222@gmail.com>
2023-06-07 18:23:04 -0400

    Add uuid7 sorting comment

--- backend/oasst_backend/prompt_repository.py
+++ backend/oasst_backend/prompt_repository.py
@@ -801,6 +801,7 @@ class PromptRepository:
         return query.all()
 
     def fetch_message_revision_history(self, message_id: UUID) -> list[MessageRevision]:
+        # the revisions are sorted by time using the uuid7 id
         revisions: list[MessageRevision] = sorted(
             self.db.query(MessageRevision).filter(MessageRevision.message_id == message_id).all(),
             key=lambda revision: revision.id.int >> 80,
