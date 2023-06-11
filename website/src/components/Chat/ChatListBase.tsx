cd4718f6d1a6784b2dc1a097939ef8e4d20487c8
Joon_Thinkpad <mjypark1212@gmail.com>
2023-06-11 22:01:31 +0800

    Modify: useMemo Hook on chat Ids

--- website/src/components/Chat/ChatListBase.tsx
+++ website/src/components/Chat/ChatListBase.tsx
@@ -3,7 +3,7 @@ import "simplebar-react/dist/simplebar.min.css";
 import { Box, CardProps, Flex } from "@chakra-ui/react";
 import { Plus } from "lucide-react";
 import { useTranslation } from "next-i18next";
-import { memo, useCallback, useState } from "react";
+import { memo, useCallback, useMemo, useState } from "react";
 import SimpleBar from "simplebar-react";
 
 import { ChatListItem } from "./ChatListItem";
@@ -21,6 +21,7 @@ export const ChatListBase = memo(function ChatListBase({
   const [view, setView] = useState<ChatListViewSelection>("visible");
   const { loadMoreRef, responses, mutateChatResponses } = useListChatPagination(view);
   const chats = responses?.flatMap((response) => response.chats) || [];
+  const chatIds = useMemo(() => chats.map((chat) => chat.id), [chats]);
 
   const { t } = useTranslation(["common", "chat"]);
 
@@ -96,7 +97,7 @@ export const ChatListBase = memo(function ChatListBase({
         {allowViews && (
           <ChatViewSelection w={["full", "auto"]} onChange={(e) => setView(e.target.value as ChatListViewSelection)} />
         )}
-        <HideAllChatButton chatIds={chats.map((chat) => chat.id)} onHide={removeItemFromList}></HideAllChatButton>
+        <HideAllChatButton chatIds={chatIds} onHide={removeItemFromList}></HideAllChatButton>
       </Flex>
       <SimpleBar style={{ padding: "8px", height: "100%", minHeight: chats.length && minHeight ? minHeight : "0" }}>
         {chats.map((chat) => (
