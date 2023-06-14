3a9a7380a4974685af791bf24ee1ea2922047681
Oliver Stanley <olivergestanley@gmail.com>
2023-06-14 14:44:05 +0100

    Update inference.py

--- oasst-shared/oasst_shared/schemas/inference.py
+++ oasst-shared/oasst_shared/schemas/inference.py
@@ -47,6 +47,7 @@ class WorkerHardwareInfo(pydantic.BaseModel):
         except Exception:
             # Workaround for psutil.cpu_freq() throwing exception on some hardware
             # or sometimes returning `None`. Hardware affected includes Apple Silicon
+            # https://github.com/giampaolo/psutil/issues/1892
             data["cpu_freq_max"] = 0
             data["cpu_freq_min"] = 0
         data["mem_total"] = psutil.virtual_memory().total
