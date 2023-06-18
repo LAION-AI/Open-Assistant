21a8391d831dd4d1d137916aa7b91e981f4fa0a4
AbdBarho <ka70911@gmail.com>
2023-06-18 09:12:30 +0200

    Add linked accounts to account page (#3489)
    Does not look the best, but we should add buttons for users to unlink
    their accounts (or link more)

--- website/src/pages/account/index.tsx
+++ website/src/pages/account/index.tsx
@@ -2,30 +2,36 @@ import { Divider, Flex, Grid, Icon, Text } from "@chakra-ui/react";
 import Head from "next/head";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { Fragment } from "react";
 export { getStaticProps } from "src/lib/defaultServerSideProps";
+import { Discord, Google } from "@icons-pack/react-simple-icons";
 import { Pencil } from "lucide-react";
 import { useTranslation } from "next-i18next";
 import { UserStats } from "src/components/Account/UserStats";
 import { XPBar } from "src/components/Account/XPBar";
 import { SurveyCard } from "src/components/Survey/SurveyCard";
 import { get } from "src/lib/api";
+import { ExternalProvider, UserAccountResponse } from "src/types/Account";
 import { LeaderboardEntity, LeaderboardTimeFrame } from "src/types/Leaderboard";
-import uswSWRImmutable from "swr/immutable";
+import useSWRImmutable from "swr/immutable";
 
 export default function Account() {
   const { t } = useTranslation(["leaderboard", "account"]);
   const { data: session } = useSession();
-  const { data: stats } = uswSWRImmutable<Partial<{ [time in LeaderboardTimeFrame]: LeaderboardEntity }>>(
+  const { data: userAccounts } = useSWRImmutable<UserAccountResponse>("/api/account", get, { keepPreviousData: true });
+  const { data: stats } = useSWRImmutable<Partial<{ [time in LeaderboardTimeFrame]: LeaderboardEntity }>>(
     "/api/user_stats",
     get,
     {
       fallbackData: {},
     }
   );
-  if (!session) {
+
+  if (!session || !userAccounts) {
     return;
   }
+
+  const { emailIsVerified, accounts } = userAccounts;
 
   return (
     <>
@@ -43,7 +49,7 @@ export default function Account() {
             <Divider />
             <Grid gridTemplateColumns="repeat(2, max-content)" alignItems="center" gap={6} py={4}>
               <Text as="b">{t("username")}</Text>
-              <Flex gap={2} style={{ overflow: "hidden" }}>
+              <Flex gap={2}>
                 {session.user.name ?? t("no_username")}
                 <Link href="/account/edit">
                   <Icon boxSize={5} as={Pencil} size="1em" />
@@ -51,6 +57,19 @@ export default function Account() {
               </Flex>
               <Text as="b">Email</Text>
               <Text>{session.user.email ?? t("no_email")}</Text>
+              {accounts.length > 0 && (
+                <>
+                  <Text as="b" className="col-span-2">
+                    {t("account:linked_accounts")}
+                  </Text>
+                  {accounts.map(({ provider, providerAccountId }) => (
+                    <Fragment key={provider + providerAccountId}>
+                      <ProviderIcon provider={provider} />
+                      <Text>{providerAccountId}</Text>
+                    </Fragment>
+                  ))}
+                </>
+              )}
             </Grid>
             <Divider my={4} />
             <XPBar />
@@ -70,3 +89,13 @@ const Title = ({ children }) => (
     {children}
   </Text>
 );
+
+const ProviderIcon = ({ provider }: { provider: ExternalProvider }): JSX.Element => {
+  if (provider === "discord") {
+    return <Discord />;
+  }
+  if (provider === "google") {
+    return <Google />;
+  }
+  return <div>❌</div>;
+};
