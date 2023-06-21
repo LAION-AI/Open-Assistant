4f1b1ebd19ddc95bc53dcd7af5aa68213e8e7032
CertifiedJoon <61619422+CertifiedJoon@users.noreply.github.com>
2023-06-21 17:55:02 +0800

    Add: option to view plaintext chat message. Issue/2508 (#3496)
    Fixes: #2508 
    
    Rendered by suspense
    <img width="537" alt="image"
    src="https://github.com/LAION-AI/Open-Assistant/assets/61619422/b259d0ad-44bb-4d80-9590-8a7fb9964c66">
    
    Plain text
    <img width="587" alt="image"
    src="https://github.com/LAION-AI/Open-Assistant/assets/61619422/723600ec-cca2-4716-8abf-fde3ea2e63db">
    
    I am not sure whether using memo on isPlainText has any performance
    gain. I need your opinions.
    
    ---------
    
    Co-authored-by: notmd <notmd1811@gmail.com>

---
+++ website/src/components/icons/MarkdownOff.tsx
@@ -0,0 +1,26 @@
+import { LucideProps } from "lucide-react";
+
+export const MarkdownOffIcon = (props: LucideProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-markdown-off"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M9 5h10a2 2 0 0 1 2 2v10"></path>
+      <path d="M19 19h-14a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 1.85 -2"></path>
+      <path d="M7 15v-6l2 2l1 -1m1 1v4"></path>
+      <path d="M17.5 13.5l.5 -.5m-2 -1v-3"></path>
+      <path d="M3 3l18 18"></path>
+    </svg>
+  );
+};
