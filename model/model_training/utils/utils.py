83d69ab4c48f77626cc8f3431857539601a57fa3
Toan Ho <toanbku@gmail.com>
2023-07-10 15:26:54 +0700

    chore: remove duplicate

--- model/model_training/utils/utils.py
+++ model/model_training/utils/utils.py
@@ -174,7 +174,6 @@ class TokenizerConfig(NamedTuple):
 TOKENIZER_CONFIGS = {
     "galactica": TokenizerConfig(special_tokens=SpecialTokens("<pad>", "</s>")),
     "vietcuna": TokenizerConfig(special_tokens=SpecialTokens("<pad>", "</s>", "<s>")),
-    "vietcuna-7b": TokenizerConfig(special_tokens=SpecialTokens("<pad>", "</s>", "<s>")),
     "llama-7b": TokenizerConfig(special_tokens=SpecialTokens("<pad>", "</s>", "<s>")),
     "GPT-JT": TokenizerConfig(special_tokens=SpecialTokens(sep_token="<|extratoken_100|>")),
     "codegen": TokenizerConfig(special_tokens=SpecialTokens("<|endoftext|>", sep_token="<|endoftext|>")),
